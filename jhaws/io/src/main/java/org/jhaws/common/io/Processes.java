0dadb07cce831ab3c8945d5f5a81b774abcc512d
jdlandsh <Jurgen.DeLandsheer@UGent.be>
2015-03-27 13:03:40 +0100

    *

--- jhaws/io/src/main/java/org/jhaws/common/io/Processes.java
+++ jhaws/io/src/main/java/org/jhaws/common/io/Processes.java
@@ -1,8 +1,11 @@
 package org.jhaws.common.io;
 
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Spliterators;
 import java.util.function.Consumer;
 import java.util.stream.StreamSupport;
@@ -45,21 +48,40 @@ public class Processes {
 		public String toString() {
 			return this.getText();
 		}
+	}
 
+	public static class Out implements Consumer<String> {
+		@Override
+		public void accept(String t) {
+			System.out.println(t);
+		}
 	}
 
 	@SafeVarargs
 	public static void callProcess(List<String> command, FilePath dir, Consumer<String>... consumers) throws IOException {
-		ProcessBuilder pb = new ProcessBuilder(command);
-		pb.directory(dir.checkExists().checkDirectory().toFile());
-		pb.redirectErrorStream(true);
-		Process p = pb.start();
+		callProcess(new HashMap<>(), command, dir, consumers);
+	}
+
+	@SafeVarargs
+	public static void callProcess(Map<String, String> env, List<String> command, FilePath dir, Consumer<String>... consumers) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(command);
+		if (env != null) {
+			builder.environment().putAll(env);
+		}
+		if (dir != null) {
+			builder.directory(dir.checkExists().checkDirectory().toFile());
+		}
+		builder.redirectInput(Redirect.INHERIT);
+		builder.redirectOutput(Redirect.INHERIT); // Redirect.to(file)
+		builder.redirectError(Redirect.INHERIT); // Redirect.to(file)
+		Process process = builder.start();
 		Consumer<String> consumer = consumers[0];
 		for (int i = 1; i < consumers.length; i++) {
 			consumer = consumer.andThen(consumers[i]);
 		}
-		try (LineIterator lineIterator = new LineIterator(p.getInputStream())) {
+		try (LineIterator lineIterator = new LineIterator(process.getInputStream())) {
 			StreamSupport.stream(Spliterators.spliteratorUnknownSize(lineIterator, 0), false).filter(StringUtils::isNotBlank).forEach(consumer);
 		}
+		process.destroy();
 	}
 }
