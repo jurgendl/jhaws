d771e54a1f7f21d34df45268008efa87fa853dd1
jurgendl <jurgen.de.landsheer@gmail.com>
2015-09-23 18:12:22 +0200

    propertydescriptors

--- jhaws/lang/src/main/java/org/jhaws/common/lang/PropertyDescriptorsBean.java
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/PropertyDescriptorsBean.java
@@ -13,9 +13,14 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Spliterators;
 import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.StreamSupport;
 
 public class PropertyDescriptorsBean {
     private static final String SPLIT = "_";
@@ -28,38 +33,99 @@ public class PropertyDescriptorsBean {
 
     private final Map<String, Method> is = new HashMap<>();
 
+    public static class BufferFunction<T, R> implements Function<T, R> {
+        private final Function<T, R> delegate;
+
+        private T initialValue;
+
+        private R returnValue;
+
+        public BufferFunction(Function<T, R> delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public R apply(T initialValue) {
+            if (returnValue == null || initialValue != this.initialValue)
+                returnValue = delegate.apply(this.initialValue = initialValue);
+            return returnValue;
+        }
+    }
+
+    public static class Jumper<T> implements Iterator<T> {
+        private final T initial;
+
+        private final Function<T, T> nextItem;
+
+        private final Predicate<T> accept;
+
+        private T current = null;
+
+        public Jumper(T initial, Function<T, T> nextItem) {
+            this(initial, nextItem, t -> true);
+        }
+
+        public Jumper(T initial, Function<T, T> nextItem, Predicate<T> accept) {
+            Objects.nonNull(initial);
+            this.initial = initial;
+            this.nextItem = new BufferFunction<>(nextItem);
+            this.accept = accept;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (current == null) {
+                return accept.test(initial);
+            }
+            T next = nextItem.apply(current);
+            return next != null && accept.test(next);
+        }
+
+        @Override
+        public T next() {
+            if (current == null) {
+                return current = initial;
+            }
+            T next = nextItem.apply(current);
+            return current = next;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("remove");
+        }
+    }
+
     public PropertyDescriptorsBean(Class<?> clazz) {
         Predicate<Method> mf = ((Predicate<Method>) m -> isPublic(m.getModifiers())).and(m -> !isStatic(m.getModifiers()))
                 .and(m -> !isAbstract(m.getModifiers()));
-        Class<?> current = clazz;
-        while (!current.equals(Object.class) && current != null) {
-            get.putAll(stream(current.getDeclaredMethods())//
-                    .parallel()
-                    .filter(mf)
-                    .filter(m -> m.getName().matches("^get.+"))
-                    .filter(m -> !Void.TYPE.isAssignableFrom(m.getReturnType()))
-                    .filter(m -> m.getParameterTypes().length == 0)
-                    .filter(m -> !get.containsKey(getKey(m)))
-                    .collect(toMap(this::getKey, identity())));
-            is.putAll(stream(current.getDeclaredMethods())//
-                    .parallel()
-                    .filter(mf)
-                    .filter(m -> m.getName().matches("^is.+"))
-                    .filter(m -> !Void.TYPE.isAssignableFrom(m.getReturnType()))
-                    .filter(m -> m.getParameterTypes().length == 0)
-                    .filter(m -> !is.containsKey(isKey(m)))
-                    .collect(toMap(this::isKey, identity())));
-            set.putAll(stream(current.getDeclaredMethods())//
-                    .parallel()
-                    .filter(mf)
-                    .filter(m -> m.getName().matches("^set.+"))
-                    .filter(m -> Void.TYPE.isAssignableFrom(m.getReturnType()))
-                    .filter(m -> m.getParameterTypes().length == 1)
-                    .filter(m -> !set.containsKey(setKey(m)))
-                    .collect(toMap(this::setKey, identity())));
-            current = current.getSuperclass();
-        }
-
+        StreamSupport.stream(Spliterators.spliteratorUnknownSize(new Jumper<Class<?>>(clazz, Class::getSuperclass, c -> !Object.class.equals(c)), 0),
+                false).forEach(current -> {
+                    get.putAll(stream(current.getDeclaredMethods())//
+                            .parallel()
+                            .filter(mf)
+                            .filter(m -> m.getName().matches("^get.+"))
+                            .filter(m -> !Void.TYPE.isAssignableFrom(m.getReturnType()))
+                            .filter(m -> m.getParameterTypes().length == 0)
+                            .filter(m -> !get.containsKey(getKey(m)))
+                            .collect(toMap(this::getKey, identity())));
+                    is.putAll(stream(current.getDeclaredMethods())//
+                            .parallel()
+                            .filter(mf)
+                            .filter(m -> m.getName().matches("^is.+"))
+                            .filter(m -> !Void.TYPE.isAssignableFrom(m.getReturnType()))
+                            .filter(m -> m.getParameterTypes().length == 0)
+                            .filter(m -> !is.containsKey(isKey(m)))
+                            .collect(toMap(this::isKey, identity())));
+                    set.putAll(stream(current.getDeclaredMethods())//
+                            .parallel()
+                            .filter(mf)
+                            .filter(m -> m.getName().matches("^set.+"))
+                            .filter(m -> Void.TYPE.isAssignableFrom(m.getReturnType()))
+                            .filter(m -> m.getParameterTypes().length == 1)
+                            .filter(m -> !set.containsKey(setKey(m)))
+                            .collect(toMap(this::setKey, identity())));
+                });
         this.propertyDescriptors = unmodifiableMap(of(get, is)//
                 .map(Map::entrySet)//
                 .flatMap(Collection::stream)
