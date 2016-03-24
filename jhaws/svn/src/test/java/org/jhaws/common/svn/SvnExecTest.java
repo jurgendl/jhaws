35a00a92be30c8ca0b5d398310996309123ba72e
Jurgen DL <jurgen.de.landsheer@gmail.com>
2016-03-24 20:03:46 +0100

    *

--- jhaws/svn/src/test/java/org/jhaws/common/svn/SvnExecTest.java
+++ jhaws/svn/src/test/java/org/jhaws/common/svn/SvnExecTest.java
@@ -6,6 +6,7 @@ import org.jhaws.common.svn.xml.ChangeList;
 import org.jhaws.common.svn.xml.Entry;
 import org.jhaws.common.svn.xml.EntryList;
 import org.jhaws.common.svn.xml.LogEntry;
+import org.jhaws.common.svn.xml.SvnInfo;
 import org.jhaws.common.svn.xml.SvnLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,16 +16,26 @@ public class SvnExecTest {
 
 	public static void main(String[] args) {
 		try {
-			File projectdir = new File("C:/java/workspaces/GISMO/komodo");
-			System.out.println(SvnExec.svn_info(projectdir));
+			File projectdir = new File(args[0]);
+			System.out.println("=========================");
+			SvnInfo svn_info = SvnExec.svn_info(projectdir);
+			System.out.println(svn_info);
+			System.out.println("=========================");
 			SvnLog svn_log = SvnExec.svn_log(projectdir, "10000", 20);
 			for (LogEntry le : svn_log)
 				System.out.println(le);
+			System.out.println("=========================");
 			for (ChangeList cl : SvnExec.svn_status(projectdir))
 				System.out.println(cl);
-			for (EntryList el : SvnExec.svn_ls(projectdir, "svn://savannah.ugent.be/komodo/branches/"))
+			System.out.println("=========================");
+			for (EntryList el : SvnExec.svn_ls(projectdir, svn_info.getEntry().getUrl().replaceAll("trunk", "branches")))
 				for (Entry e : el)
 					System.out.println(e);
+			System.out.println("=========================");
+			for (EntryList el : SvnExec.svn_ls_tag(projectdir))
+				for (Entry e : el)
+					System.out.println(e);
+			System.out.println("=========================");
 		} catch (Exception ex) {
 			logger.error("{}", ex);
 		}
