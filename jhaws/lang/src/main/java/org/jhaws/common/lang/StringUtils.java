16ad33e9ae939b5f053b8c267af6f7a699b3b845
Jurgen DL <jurgen.de.landsheer@gmail.com>
2016-06-22 17:04:32 +0200

    *

--- jhaws/lang/src/main/java/org/jhaws/common/lang/StringUtils.java
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/StringUtils.java
@@ -88,7 +88,7 @@ public interface StringUtils {
 	}
 
 	public static String splitCamelCase(String s) {
-		return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
+		return s == null ? null : s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
 	}
 
 	public static String sortable(String string) {
