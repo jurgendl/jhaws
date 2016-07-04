93765eba3f33355524c0a68890295a1ae5f89e74
Jurgen DL <jurgen.de.landsheer@gmail.com>
2016-07-04 15:15:29 +0200

    *

---
+++ jhaws/web/src/main/java/org/jhaws/common/web/servlets/SpringStreamingServlet.java
@@ -0,0 +1,43 @@
+package org.jhaws.common.web.servlets;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SpringStreamingServlet extends StreamingServlet {
+	private static final long serialVersionUID = 4914047812048630486L;
+
+	protected transient WebApplicationContext webApplicationContext;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		webApplicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+	}
+
+	protected Map<String, StreamingService> streamingServices;
+
+	protected Map<String, StreamingService> getStreamingServicesService() {
+		if (streamingServices == null) {
+			streamingServices = webApplicationContext.getBeansOfType(StreamingService.class);
+		}
+		return this.streamingServices;
+	}
+
+	@Override
+	protected StreamingSource getStream(String requestedFile) throws UnsupportedEncodingException, IOException {
+		for (StreamingService streamingService : getStreamingServicesService().values()) {
+			if (requestedFile.startsWith(streamingService.getPath())) {
+				String relative = requestedFile.replaceFirst(streamingService.getPath(), "");
+				return streamingService.getStream(relative);
+			}
+		}
+		throw new IOException(requestedFile);
+	}
+}
---
+++ jhaws/web/src/main/java/org/jhaws/common/web/servlets/StreamingService.java
@@ -0,0 +1,9 @@
+package org.jhaws.common.web.servlets;
+
+import java.io.IOException;
+
+public interface StreamingService {
+	public String getPath();
+
+	public StreamingSource getStream(String relative) throws IOException;
+}
