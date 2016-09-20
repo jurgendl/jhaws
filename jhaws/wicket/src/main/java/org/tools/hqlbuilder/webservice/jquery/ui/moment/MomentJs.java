464a8f40d163b1967afb339e4c90e4229fde5d5a
jurgendl <jurgen.de.landsheer@gmail.com>
2016-09-20 11:37:10 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/moment/MomentJs.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/moment/MomentJs.java
@@ -7,4 +7,6 @@ import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
  */
 public class MomentJs {
 	public static JavaScriptResourceReference JS = new JavaScriptResourceReference(MomentJs.class, "moment.js");
+
+	public static JavaScriptResourceReference JS_LOCALE = new JavaScriptResourceReference(MomentJs.class, "moment-with-locales.js");
 }
