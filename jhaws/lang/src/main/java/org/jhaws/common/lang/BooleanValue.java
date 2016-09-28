f9e9c898bb5aa373c5b13ecff93eee2fe671df7a
Jurgen DL <jurgen.de.landsheer@gmail.com>
2016-09-28 12:56:54 +0200

    *

---
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/BooleanValue.java
@@ -0,0 +1,21 @@
+package org.jhaws.common.lang;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class BooleanValue extends Value<Boolean> {
+	private static final long serialVersionUID = -1530814975734843133L;
+
+	public BooleanValue() {
+		super(Boolean.FALSE);
+	}
+
+	public BooleanValue(Boolean value) {
+		super(value);
+	}
+
+	public BooleanValue negate() {
+		set(!Boolean.TRUE.equals(get()));
+		return this;
+	}
+}
