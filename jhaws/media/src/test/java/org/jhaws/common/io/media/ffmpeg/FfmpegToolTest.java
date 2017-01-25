1d08952e8cbb2ddcd7e010d48705abe3a9cfc4f5
jurgendl <jurgen.de.landsheer@gmail.com>
2017-01-25 18:18:03 +0100

    *

--- jhaws/media/src/test/java/org/jhaws/common/io/media/ffmpeg/FfmpegToolTest.java
+++ jhaws/media/src/test/java/org/jhaws/common/io/media/ffmpeg/FfmpegToolTest.java
@@ -1,17 +1,26 @@
 package org.jhaws.common.io.media.ffmpeg;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.jhaws.common.io.FilePath;
 import org.jhaws.common.io.media.ffmpeg.FfmpegTool.RemuxCfg;
 import org.jhaws.common.io.media.ffmpeg.FfmpegTool.RemuxDefaultsCfg;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FfmpegToolTest {
+    static FfmpegTool t;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        FilePath f = new FilePath(System.getenv("FFMPEG"));
+        t = new FfmpegTool(f);
+    }
+
     @Test
     public void test1() {
         try {
-            FilePath f = new FilePath(System.getenv("FFMPEG"));
-            if (f.isDirectory()) f = f.child("ffmpeg.exe");
-            FfmpegTool t = new FfmpegTool(f);
             System.out.println(t.getHwAccel());
             FilePath input = FilePath.createDefaultTempFile("mp4");
             input.write(FfmpegTool.class.getClassLoader().getResourceAsStream("hevc.mp4"));
@@ -20,8 +29,53 @@ public class FfmpegToolTest {
             def.setTwopass(true);
             RemuxCfg cfg = t.remux(def, null, input, output, null);
             cfg.commands.forEach(System.out::println);
-        } catch (Exception ex) {
+        } catch (RuntimeException ex) {
             ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    @Test
+    public void test2() {
+        try {
+            // runtime config VM arguments -Dsld=
+            FilePath h = new FilePath(System.getProperty("sld"));
+            FilePath sourcedir = h.child("sources").createDirectoryIfNotExists();
+            FilePath tmp = h.child("tmp");
+            tmp.deleteAllIfExists();
+            tmp.createDirectoryIfNotExists();
+            List<FilePath> sources = sourcedir.list().stream().sorted().collect(Collectors.toList());
+            int nr = sources.size();
+            if (nr == 0) return;
+            int repeat = 3;
+            int index = 0;
+            int sindex = 0;
+            for (int i = 0; i < nr * repeat; i++) {
+                for (int j = 0; j < repeat; j++) {
+                    String ii = "";
+                    if (index < 1000) {
+                        ii = "0" + ii;
+                    }
+                    if (index < 100) {
+                        ii = "0" + ii;
+                    }
+                    if (index < 10) {
+                        ii = "0" + ii;
+                    }
+                    ii = ii + index;
+                    sources.get(sindex).copyTo(tmp.child("imgs_" + ii + ".png"));
+                }
+                index++;
+                sindex++;
+                if (sindex >= nr) {
+                    sindex = 0;
+                }
+            }
+            t.slideshow(null, nr, tmp.getAbsolutePath() + "/images_%04d.png", h.child("output").createDirectoryIfNotExists().child("out.mp4"),
+                    System.out::println);
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+            throw ex;
         }
     }
 }
