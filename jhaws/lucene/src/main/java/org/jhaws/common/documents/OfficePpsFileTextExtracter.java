e080ecae937dcde617cd46b4dcf02b5d187ae2a6
jurgendl <jurgen.de.landsheer@gmail.com>
2017-06-12 18:02:38 +0200

    *

---
+++ jhaws/lucene/src/main/java/org/jhaws/common/documents/OfficePpsFileTextExtracter.java
@@ -0,0 +1,20 @@
+package org.jhaws.common.documents;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jhaws.common.io.FilePath;
+
+public class OfficePpsFileTextExtracter implements FileTextExtracter {
+    @Override
+    public List<String> accepts() {
+        return Arrays.asList("pps");
+    }
+
+    @Override
+    public void extract(InputStream inputFile, FilePath target) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+}
