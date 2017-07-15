8989cc8528cef95d93d54dc9c5c210ffb581ec8e
jurgendl <jurgen.de.landsheer@gmail.com>
2017-07-15 15:38:34 +0200

    *

---
+++ jhaws/swing/src/main/java/org/swingeasy/tmp/Img2Pdf.java
@@ -0,0 +1,40 @@
+package org.swingeasy.tmp;
+
+import static org.jhaws.common.lang.CollectionUtils8.sort;
+
+import java.util.List;
+
+import org.jhaws.common.io.FilePath;
+import org.jhaws.common.io.FilePath.Comparators.LastModifiedTimeComparator;
+
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Img2Pdf {
+	public static void main(String[] args) {
+		try {
+			createPDF(new FilePath(args[0]), new FilePath(args[1]));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	static public void createPDF(FilePath trg, FilePath dir) throws Exception {
+		FilePath pdffile = new FilePath(dir, trg.getShortFileName() + ".pdf");
+		List<FilePath> images = trg.listFiles();
+		images = sort(true, images, new LastModifiedTimeComparator());
+		com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+		PdfWriter writer = PdfWriter.getInstance(document, pdffile.newBufferedOutputStream());
+		document.open();
+		for (FilePath img : images) {
+			Image iimg = Image.getInstance(img.toUrl());
+			Rectangle pageSize = new Rectangle(iimg.getWidth(), iimg.getHeight());
+			document.setPageSize(pageSize);
+			document.newPage();
+			iimg.setAbsolutePosition(0f, 0f);
+			document.add(iimg);
+		}
+		document.close();
+	}
+}
---
+++ jhaws/swing/src/main/java/org/swingeasy/tmp/Pdf2Img.java
@@ -0,0 +1,80 @@
+package org.swingeasy.tmp;
+
+import java.awt.image.RenderedImage;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.function.Consumer;
+
+import javax.imageio.ImageIO;
+
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.jhaws.common.io.FilePath;
+import org.jhaws.common.lang.IntegerValue;
+
+// http://stackoverflow.com/questions/7007917/how-to-extract-images-from-a-pdf-with-itext-in-the-correct-order
+public class Pdf2Img {
+	public static void main(String[] args) {
+		try {
+			IntegerValue iv = new IntegerValue();
+			FilePath pdf = new FilePath(args[0]).checkExists();
+			FilePath dir = new FilePath(args[1]).createDirectoryIfNotExists();
+			// PDFBox
+			try (PDDocument document = PDDocument.load(pdf.toFile())) {
+				getImagesFromPDF(document, img -> {
+					if (img.getWidth() > 100 && img.getHeight() > 100) {
+						BufferedOutputStream out = dir.child(pdf.getShortFileName() + "." + iv.add() + ".jpg").newBufferedOutputStream();
+						try {
+							ImageIO.write(img, "jpg", out);
+						} catch (IOException ex) {
+							throw new UncheckedIOException(ex);
+						}
+					}
+				});
+				// PDPageTree pageTree = document.getDocumentCatalog().getPages();
+				// Iterator<PDPage> iter = pageTree.iterator();
+				// int i = 0;
+				// while (iter.hasNext()) {
+				// PDPage page = iter.next();
+				// PDResources resources = page.getResources();
+				// for (COSName xObjectName : resources.getXObjectNames()) {
+				// PDXObject xObject = resources.getXObject(xObjectName);
+				// if (xObject instanceof PDImageXObject) {
+				// PDImageXObject pdImageXObject = PDImageXObject.class.cast(xObject);
+				// if (pdImageXObject.getWidth() > 25 && pdImageXObject.getHeight() > 25) {
+				// BufferedOutputStream out = dir.child(pdf.getShortFileName() + "." + (i++) + ".jpg").newBufferedOutputStream();
+				// IOUtils.copy(pdImageXObject.createInputStream(), out);
+				// }
+				// }
+				// }
+				// }
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace(System.out);
+		}
+	}
+
+	static public void getImagesFromPDF(PDDocument document, Consumer<RenderedImage> whatToDo) throws IOException {
+		for (PDPage page : document.getPages()) {
+			getImagesFromResources(page.getResources(), whatToDo);
+		}
+	}
+
+	static private void getImagesFromResources(PDResources resources, Consumer<RenderedImage> whatToDo) throws IOException {
+		for (COSName xObjectName : resources.getXObjectNames()) {
+			PDXObject xObject = resources.getXObject(xObjectName);
+			if (xObject instanceof PDFormXObject) {
+				getImagesFromResources(((PDFormXObject) xObject).getResources(), whatToDo);
+			} else if (xObject instanceof PDImageXObject) {
+				System.out.println(xObject);
+				whatToDo.accept(((PDImageXObject) xObject).getImage());
+			}
+		}
+	}
+}
