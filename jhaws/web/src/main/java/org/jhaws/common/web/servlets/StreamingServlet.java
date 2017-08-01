c51b9e7e65674ba697da34d9208e5de69cb90b69
jurgendl <jurgen.de.landsheer@gmail.com>
2017-08-01 13:55:41 +0200

    *

--- jhaws/web/src/main/java/org/jhaws/common/web/servlets/StreamingServlet.java
+++ jhaws/web/src/main/java/org/jhaws/common/web/servlets/StreamingServlet.java
@@ -22,8 +22,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.jhaws.common.lang.StringUtils;
 
 /**
- * A file servlet supporting resume of downloads and client-side caching and GZIP of text content. This servlet can also be used for images, client-side caching would become more
- * efficient. This servlet can also be used for text files, GZIP would decrease network bandwidth.
+ * A file servlet supporting resume of downloads and client-side caching and
+ * GZIP of text content. This servlet can also be used for images, client-side
+ * caching would become more efficient. This servlet can also be used for text
+ * files, GZIP would decrease network bandwidth.
  *
  * @author BalusC
  * @link http://balusc.blogspot.com/2009/02/fileservlet-supporting-resume-and.html
@@ -61,15 +63,18 @@ public abstract class StreamingServlet extends HttpServlet {
 
 	}
 
-	// Properties ---------------------------------------------------------------------------------
+	// Properties
+	// ---------------------------------------------------------------------------------
 
 	private static final long serialVersionUID = 2517932408034610799L;
 
-	// Actions ------------------------------------------------------------------------------------
+	// Actions
+	// ------------------------------------------------------------------------------------
 
 	protected static final int _DEFAULT_BUFFER_SIZE = 2 << 12; // ..bytes = 8KB.
 
-	protected static final long _DEFAULT_EXPIRE_TIME = 60l * 60l * 24l * 7l; // ..ms = 1 week.
+	// ..ms = 1 week.
+	protected static final long _DEFAULT_EXPIRE_TIME = 60l * 60l * 24l * 7l;
 
 	protected static final String MULTIPART_BOUNDARY = "MULTIPART_BYTERANGES";
 
@@ -101,11 +106,13 @@ public abstract class StreamingServlet extends HttpServlet {
 	protected boolean accepts(String acceptHeader, String toAccept) {
 		String[] acceptValues = acceptHeader.split("\\s*(,|;)\\s*");
 		Arrays.sort(acceptValues);
-		return Arrays.binarySearch(acceptValues, toAccept) > -1 || Arrays.binarySearch(acceptValues, toAccept.replaceAll("/.*$", "/*")) > -1
+		return Arrays.binarySearch(acceptValues, toAccept) > -1
+				|| Arrays.binarySearch(acceptValues, toAccept.replaceAll("/.*$", "/*")) > -1
 				|| Arrays.binarySearch(acceptValues, "*/*") > -1;
 	}
 
-	// Helpers (can be refactored to public utility class) ----------------------------------------
+	// Helpers (can be refactored to public utility class)
+	// ----------------------------------------
 
 	/**
 	 * Close the given resource.
@@ -118,8 +125,10 @@ public abstract class StreamingServlet extends HttpServlet {
 			try {
 				resource.close();
 			} catch (IOException ignore) {
-				// Ignore IOException. If you want to handle this anyway, it might be useful to know
-				// that this will generally only be thrown when the client aborted the request.
+				// Ignore IOException. If you want to handle this anyway, it
+				// might be useful to know
+				// that this will generally only be thrown when the client
+				// aborted the request.
 			}
 		}
 	}
@@ -176,23 +185,27 @@ public abstract class StreamingServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest, HttpServletResponse).
 	 */
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Process request with content.
 		processRequest(request, response, true);
 	}
 
 	/**
-	 * Process HEAD request. This returns the same headers as GET request, but without content.
+	 * Process HEAD request. This returns the same headers as GET request, but
+	 * without content.
 	 *
 	 * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse).
 	 */
 	@Override
-	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doHead(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Process request without content.
 		processRequest(request, response, false);
 	}
 
-	// Inner classes ------------------------------------------------------------------------------
+	// Inner classes
+	// ------------------------------------------------------------------------------
 
 	protected int getBufferSize() {
 		return StreamingServlet._DEFAULT_BUFFER_SIZE;
@@ -237,18 +250,22 @@ public abstract class StreamingServlet extends HttpServlet {
 	 * @param response
 	 *            The response to be created.
 	 * @param content
-	 *            Whether the request body should be written (GET) or not (HEAD).
+	 *            Whether the request body should be written (GET) or not
+	 *            (HEAD).
 	 * @throws IOException
 	 *             If something fails at I/O level.
 	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response, boolean content) throws IOException {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response, boolean content)
+			throws IOException {
 		// Get requested file by path info.
 		String requestedFile = request.getPathInfo();
+		requestedFile = request.getRequestURI().replaceFirst(request.getContextPath(), "").replaceFirst("/stream", "");
 
 		// Check if file is actually supplied to the request URL.
 		if (requestedFile == null) {
 			// Do your thing if the file is not supplied to the request URL.
-			// Throw an exception, or send 404, or show default/warning page, or just ignore it.
+			// Throw an exception, or send 404, or show default/warning page, or
+			// just ignore it.
 			response.sendError(HttpServletResponse.SC_NOT_FOUND, requestedFile);
 			return;
 		}
@@ -259,9 +276,11 @@ public abstract class StreamingServlet extends HttpServlet {
 			relativeFileName = relativeFileName.substring(1);
 		}
 		// filename without relative path
-		String shortFileName = relativeFileName.contains("/") ? relativeFileName.substring(relativeFileName.lastIndexOf('/') + 1) : relativeFileName;
+		String shortFileName = relativeFileName.contains("/")
+				? relativeFileName.substring(relativeFileName.lastIndexOf('/') + 1) : relativeFileName;
 
-		// URL-decode the file name (might contain spaces and on) and prepare file object.
+		// URL-decode the file name (might contain spaces and on) and prepare
+		// file object.
 		StreamingSource file;
 		try {
 			file = getStream(relativeFileName);
@@ -273,7 +292,8 @@ public abstract class StreamingServlet extends HttpServlet {
 		// Check if file actually exists in filesystem.
 		if (!file.exists()) {
 			// Do your thing if the file appears to be non-existing.
-			// Throw an exception, or send 404, or show default/warning page, or just ignore it.
+			// Throw an exception, or send 404, or show default/warning page, or
+			// just ignore it.
 			response.sendError(HttpServletResponse.SC_NOT_FOUND, relativeFileName);
 			return;
 		}
@@ -284,28 +304,34 @@ public abstract class StreamingServlet extends HttpServlet {
 		String eTag = requestedFile.replace('/', '$').replace(',', '$') + "_" + length + "_" + lastModified;
 		long expires = System.currentTimeMillis() + getExpireTime();
 
-		// Validate request headers for caching ---------------------------------------------------
+		// Validate request headers for caching
+		// ---------------------------------------------------
 
-		// If-None-Match header should contain "*" or ETag. If so, then return 304.
+		// If-None-Match header should contain "*" or ETag. If so, then return
+		// 304.
 		String ifNoneMatch = request.getHeader("If-None-Match");
 		if (ifNoneMatch != null && matches(ifNoneMatch, eTag)) {
 			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
 			response.setHeader("ETag", eTag); // Required in 304.
-			response.setDateHeader("Expires", expires); // Postpone cache with 1 week.
+			response.setDateHeader("Expires", expires); // Postpone cache with 1
+														// week.
 			return;
 		}
 
-		// If-Modified-Since header should be greater than LastModified. If so, then return 304.
+		// If-Modified-Since header should be greater than LastModified. If so,
+		// then return 304.
 		// This header is ignored if any If-None-Match header is specified.
 		long ifModifiedSince = request.getDateHeader("If-Modified-Since");
 		if (ifNoneMatch == null && ifModifiedSince != -1 && ifModifiedSince + 1000 > lastModified) {
 			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
 			response.setHeader("ETag", eTag); // Required in 304.
-			response.setDateHeader("Expires", expires); // Postpone cache with 1 week.
+			response.setDateHeader("Expires", expires); // Postpone cache with 1
+														// week.
 			return;
 		}
 
-		// Validate request headers for resume ----------------------------------------------------
+		// Validate request headers for resume
+		// ----------------------------------------------------
 
 		// If-Match header should contain "*" or ETag. If not, then return 412.
 		String ifMatch = request.getHeader("If-Match");
@@ -314,14 +340,16 @@ public abstract class StreamingServlet extends HttpServlet {
 			return;
 		}
 
-		// If-Unmodified-Since header should be greater than LastModified. If not, then return 412.
+		// If-Unmodified-Since header should be greater than LastModified. If
+		// not, then return 412.
 		long ifUnmodifiedSince = request.getDateHeader("If-Unmodified-Since");
 		if (ifUnmodifiedSince != -1 && ifUnmodifiedSince + 1000 <= lastModified) {
 			response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED);
 			return;
 		}
 
-		// Validate and process range -------------------------------------------------------------
+		// Validate and process range
+		// -------------------------------------------------------------
 
 		// Prepare some variables. The full Range represents the complete file.
 		Range full = new Range(0, length - 1, length);
@@ -331,19 +359,26 @@ public abstract class StreamingServlet extends HttpServlet {
 		String range = request.getHeader("Range");
 		if (range != null) {
 
-			// Range header should match format "bytes=n-n,n-n,n-n...". If not, then return 416.
+			// Range header should match format "bytes=n-n,n-n,n-n...". If not,
+			// then return 416.
 			if (!range.matches("^bytes=\\d*-\\d*(,\\d*-\\d*)*$")) {
-				response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
+				response.setHeader("Content-Range", "bytes */" + length); // Required
+																			// in
+																			// 416.
 				response.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE);
 				return;
 			}
 
-			// If-Range header should either match ETag or be greater then LastModified. If not,
+			// If-Range header should either match ETag or be greater then
+			// LastModified. If not,
 			// then return full file.
 			String ifRange = request.getHeader("If-Range");
 			if (ifRange != null && !ifRange.equals(eTag)) {
 				try {
-					long ifRangeTime = request.getDateHeader("If-Range"); // Throws IAE if invalid.
+					long ifRangeTime = request.getDateHeader("If-Range"); // Throws
+																			// IAE
+																			// if
+																			// invalid.
 					if (ifRangeTime != -1 && ifRangeTime + 1000 < lastModified) {
 						ranges.add(full);
 					}
@@ -352,11 +387,14 @@ public abstract class StreamingServlet extends HttpServlet {
 				}
 			}
 
-			// If any valid If-Range header, then process each part of byte range.
+			// If any valid If-Range header, then process each part of byte
+			// range.
 			if (ranges.isEmpty()) {
 				for (String part : range.substring(6).split(",")) {
-					// Assuming a file with length of 100, the following examples returns bytes at:
-					// 50-80 (50 to 80), 40- (40 to length=100), -20 (length-20=80 to length=100).
+					// Assuming a file with length of 100, the following
+					// examples returns bytes at:
+					// 50-80 (50 to 80), 40- (40 to length=100), -20
+					// (length-20=80 to length=100).
 					long start = sublong(part, 0, part.indexOf("-"));
 					long end = sublong(part, part.indexOf("-") + 1, part.length());
 
@@ -367,9 +405,12 @@ public abstract class StreamingServlet extends HttpServlet {
 						end = length - 1;
 					}
 
-					// Check if Range is syntactically valid. If not, then return 416.
+					// Check if Range is syntactically valid. If not, then
+					// return 416.
 					if (start > end) {
-						response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
+						response.setHeader("Content-Range", "bytes */" + length); // Required
+																					// in
+																					// 416.
 						response.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE);
 						return;
 					}
@@ -380,30 +421,37 @@ public abstract class StreamingServlet extends HttpServlet {
 			}
 		}
 
-		// Prepare and initialize response --------------------------------------------------------
+		// Prepare and initialize response
+		// --------------------------------------------------------
 
-		// Get content type by file name and set default GZIP support and content disposition.
+		// Get content type by file name and set default GZIP support and
+		// content disposition.
 		String contentType = getServletContext().getMimeType(shortFileName);
 		boolean acceptsGzip = false;
 		String disposition = "inline";
 
 		// If content type is unknown, then set the default value.
-		// For all content types, see: http://www.w3schools.com/media/media_mimeref.asp
+		// For all content types, see:
+		// http://www.w3schools.com/media/media_mimeref.asp
 		// To add new content types, add new mime-mapping entry in web.xml.
 		if (contentType == null) {
 			contentType = "application/octet-stream";
 		}
 
-		// If content type is text, then determine whether GZIP content encoding is supported by
-		// the browser and expand content type with the one and right character encoding.
+		// If content type is text, then determine whether GZIP content encoding
+		// is supported by
+		// the browser and expand content type with the one and right character
+		// encoding.
 		if (contentType.startsWith("text")) {
 			String acceptEncoding = request.getHeader("Accept-Encoding");
 			acceptsGzip = acceptEncoding != null && accepts(acceptEncoding, "gzip");
 			contentType += ";charset=" + StringUtils.UTF8;
 		}
 
-		// Else, expect for images, determine content disposition. If content type is supported by
-		// the browser, then set to inline, else attachment which will pop a 'save as' dialogue.
+		// Else, expect for images, determine content disposition. If content
+		// type is supported by
+		// the browser, then set to inline, else attachment which will pop a
+		// 'save as' dialogue.
 		else if (!contentType.startsWith("image")) {
 			String accept = request.getHeader("Accept");
 			disposition = accept != null && accepts(accept, contentType) ? "inline" : "attachment";
@@ -418,7 +466,8 @@ public abstract class StreamingServlet extends HttpServlet {
 		response.setDateHeader("Last-Modified", lastModified);
 		response.setDateHeader("Expires", expires);
 
-		// Send requested file (part(s)) to client ------------------------------------------------
+		// Send requested file (part(s)) to client
+		// ------------------------------------------------
 
 		// Prepare streams.
 		SeekableByteChannel input = null;
@@ -441,13 +490,16 @@ public abstract class StreamingServlet extends HttpServlet {
 						response.setHeader("Content-Encoding", "gzip");
 						output = new GZIPOutputStream(output, getBufferSize());
 					} else {
-						// Content length is not directly predictable in case of GZIP.
-						// So only add it if there is no means of GZIP, else browser will hang.
+						// Content length is not directly predictable in case of
+						// GZIP.
+						// So only add it if there is no means of GZIP, else
+						// browser will hang.
 						response.setHeader("Content-Length", String.valueOf(r.length));
 					}
 
 					// Copy full range.
-					// System.out.println("A " + ((r.end - r.start) + "//" + r.length));
+					// System.out.println("A " + ((r.end - r.start) + "//" +
+					// r.length));
 					copy(input, output, r.start, r.length);
 				}
 
@@ -462,7 +514,8 @@ public abstract class StreamingServlet extends HttpServlet {
 
 				if (content) {
 					// Copy single part range.
-					// System.out.println("B " + ((r.end - r.start) + "//" + r.length));
+					// System.out.println("B " + ((r.end - r.start) + "//" +
+					// r.length));
 					copy(input, output, r.start, r.length);
 				}
 
@@ -473,19 +526,22 @@ public abstract class StreamingServlet extends HttpServlet {
 				response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT); // 206.
 
 				if (content) {
-					// Cast back to ServletOutputStream to get the easy println methods.
+					// Cast back to ServletOutputStream to get the easy println
+					// methods.
 					ServletOutputStream sos = (ServletOutputStream) output;
 
 					// Copy multi part range.
 					for (Range r : ranges) {
-						// Add multipart boundary and header fields for every range.
+						// Add multipart boundary and header fields for every
+						// range.
 						sos.println();
 						sos.println("--" + StreamingServlet.MULTIPART_BOUNDARY);
 						sos.println("Content-Type: " + contentType);
 						sos.println("Content-Range: bytes " + r.start + "-" + r.end + "/" + r.total);
 
 						// Copy single part range of multi part range.
-						// System.out.println("C " + ((r.end - r.start) + "//" + r.length));
+						// System.out.println("C " + ((r.end - r.start) + "//" +
+						// r.length));
 						copy(input, output, r.start, r.length);
 					}
 
@@ -502,7 +558,9 @@ public abstract class StreamingServlet extends HttpServlet {
 	}
 
 	/**
-	 * Returns a substring of the given string value from the given begin index to the given end index as a long. If the substring is empty, then -1 will be returned
+	 * Returns a substring of the given string value from the given begin index
+	 * to the given end index as a long. If the substring is empty, then -1 will
+	 * be returned
 	 *
 	 * @param value
 	 *            The string value to return a substring as long for.
@@ -510,7 +568,8 @@ public abstract class StreamingServlet extends HttpServlet {
 	 *            The begin index of the substring to be returned as long.
 	 * @param endIndex
 	 *            The end index of the substring to be returned as long.
-	 * @return A substring of the given string value as long or -1 if substring is empty.
+	 * @return A substring of the given string value as long or -1 if substring
+	 *         is empty.
 	 */
 	protected long sublong(String value, int beginIndex, int endIndex) {
 		String substring = value.substring(beginIndex, endIndex);
