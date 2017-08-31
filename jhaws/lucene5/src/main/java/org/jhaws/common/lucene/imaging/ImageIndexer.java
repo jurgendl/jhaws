da6b1bd94d67b5999972c3848ab910e872a2f382
jurgendl <jurgen.de.landsheer@gmail.com>
2017-09-01 01:46:54 +0200

    *

--- jhaws/lucene5/src/main/java/org/jhaws/common/lucene/imaging/ImageIndexer.java
+++ jhaws/lucene5/src/main/java/org/jhaws/common/lucene/imaging/ImageIndexer.java
@@ -3,6 +3,7 @@ package org.jhaws.common.lucene.imaging;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -36,6 +37,7 @@ import net.semanticmetadata.lire.utils.FileUtils;
 // https://blog.mayflower.de/1755-Image-similarity-search-with-LIRE.html
 // http://blog.thedigitalgroup.com/rajendras/2015/06/19/lire-lucene-image-retrieval/
 // https://github.com/aoldemeier/image-similarity-with-lire/blob/master/src/main/java/de/mayflower/samplecode/SimilaritySearchWithLIRE/SimilaritySearchWithLIRE.java
+// https://www.researchgate.net/publication/221573372_Lire_lucene_image_retrieval_an_extensible_java_CBIR_library
 public class ImageIndexer {
 	public static void main(String[] args) {
 		new ImageIndexer().findDuplicates(new FilePath(args[0]), new FilePath(args[1]), new FilePath(args[2]),
@@ -50,9 +52,9 @@ public class ImageIndexer {
 				images = Collections.<String>emptyList();
 			GlobalDocumentBuilder globalDocumentBuilder = new GlobalDocumentBuilder(false, HashingMode.None, false);
 			globalDocumentBuilder.addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.CEDD.class);
-			globalDocumentBuilder.addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.FCTH.class);
-			globalDocumentBuilder
-					.addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.ColorLayout.class);
+			// globalDocumentBuilder.addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.FCTH.class);
+			// globalDocumentBuilder
+			// .addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.ColorLayout.class);
 			// globalDocumentBuilder
 			// .addExtractor(net.semanticmetadata.lire.imageanalysis.features.global.EdgeHistogram.class);
 			// globalDocumentBuilder
@@ -89,7 +91,7 @@ public class ImageIndexer {
 			System.out.println();
 			IndexReader ir = DirectoryReader.open(FSDirectory.open(index.getPath()));
 			ImageSearcher searcher = new GenericFastImageSearcher(100, CEDD.class, true, ir);
-			BufferedWriter out = report.newBufferedWriter();
+			BufferedWriter out = report.newBufferedWriter(Charset.forName("utf8"));
 			// List<String> done = new ArrayList<>();
 			x = 0;
 			for (Iterator<String> it = images.iterator(); it.hasNext();) {
@@ -115,7 +117,7 @@ public class ImageIndexer {
 						continue;
 					}
 					System.out.println(score + ": \t" + fileName);
-					out.write(imageFilePath + "\t" + score + "\t" + fileName + "\n");
+					out.write(imageFilePath + "\t" + score + "\t" + fileName + "\r\n");
 				}
 				System.out.println();
 				out.flush();
