0bab14b099971c8b58ef7a629e72985e986a7d74
jurgendl <jurgen.de.landsheer@gmail.com>
2017-10-07 19:53:58 +0200

    *

--- jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedCollection.java
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedCollection.java
@@ -2,9 +2,22 @@ package org.jhaws.common.lang;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public interface EnhancedCollection<T> extends Collection<T> {
-    default Optional<T> optional() {
-        return isEmpty() ? Optional.empty() : Optional.of(iterator().next());
-    }
+	default Optional<T> optional() {
+		return isEmpty() ? Optional.empty() : Optional.of(iterator().next());
+	}
+
+	default T singleton(T defaultValue) {
+		return optional().orElse(defaultValue);
+	}
+
+	default T singleton(Supplier<T> defaultValue) {
+		return optional().orElseGet(defaultValue);
+	}
+
+	default T singleton() {
+		return optional().orElse(null);
+	}
 }
