62bf684c1147fbaa0ee93bb5c4b4f4cf0dbc6f62
jurgendl <jurgen.de.landsheer@gmail.com>
2017-10-12 22:37:22 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/weloveicons/fontawesome/FontAwesome.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/weloveicons/fontawesome/FontAwesome.java
@@ -3,6 +3,7 @@ package org.tools.hqlbuilder.webservice.jquery.ui.weloveicons.fontawesome;
 import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
 
 // http://fontawesome.io/get-started/
+// http://fontawesome.io/icons/
 public class FontAwesome {
-    public static CssResourceReference CSS = new CssResourceReference(FontAwesome.class, "css/font-awesome.css");
+	public static CssResourceReference CSS = new CssResourceReference(FontAwesome.class, "css/font-awesome.css");
 }
