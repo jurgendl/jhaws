0b3b4f99c599f32c898600b61767f887b3ce6f26
jurgendl <jurgen.de.landsheer@gmail.com>
2017-12-16 21:35:07 +0100

    *

---
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/FeedbackPanel.java
@@ -0,0 +1,14 @@
+package org.tools.hqlbuilder.webservice.wicket.forms.bootstrap;
+
+import org.apache.wicket.feedback.IFeedbackMessageFilter;
+
+@SuppressWarnings("serial")
+public class FeedbackPanel extends org.apache.wicket.markup.html.panel.FeedbackPanel {
+	public FeedbackPanel(String id, IFeedbackMessageFilter filter) {
+		super(id, filter);
+	}
+
+	public FeedbackPanel(String id) {
+		super(id);
+	}
+}
--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/FormPanel.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/FormPanel.java
@@ -26,128 +26,150 @@ import org.tools.hqlbuilder.webservice.wicket.forms.common.TagItTextFieldSetting
 
 @SuppressWarnings("serial")
 public class FormPanel<T extends Serializable> extends FormPanelParent<T> {
-    public FormPanel(String id, FormActions<T> formActions, FormSettings formSettings) {
-        super(id, formActions, formSettings);
-        bootstrap = true;
-    }
+	public FormPanel(String id, FormActions<T> formActions, FormSettings formSettings) {
+		super(id, formActions, formSettings);
+		bootstrap = true;
+	}
 
-    protected String getFeedbackCss(org.apache.wicket.feedback.FeedbackMessage message) {
-        return "alert alert-" + getFeedbackCssInternal(message) + " mb-1";
-    }
+	protected String getFeedbackCss(org.apache.wicket.feedback.FeedbackMessage message) {
+		return "alert alert-" + getFeedbackCssInternal(message) + " mb-1";
+	}
 
-    protected String getFeedbackCssInternal(org.apache.wicket.feedback.FeedbackMessage message) {
-        if (FeedbackMessage.DEBUG == message.getLevel()) {
-            return "debug";
-        }
-        if (FeedbackMessage.INFO == message.getLevel()) {
-            return "info";
-        }
-        if (FeedbackMessage.SUCCESS == message.getLevel()) {
-            return "success";
-        }
-        if (FeedbackMessage.WARNING == message.getLevel()) {
-            return "warning";
-        }
-        if (FeedbackMessage.ERROR == message.getLevel()) {
-            return "error";
-        }
-        if (FeedbackMessage.FATAL == message.getLevel()) {
-            return "error";
-        }
-        return "primary";
-    }
+	protected org.apache.wicket.markup.html.panel.FeedbackPanel newFeedbackPanel(String string) {
+		org.tools.hqlbuilder.webservice.wicket.forms.bootstrap.FeedbackPanel feedbackPanel = new org.tools.hqlbuilder.webservice.wicket.forms.bootstrap.FeedbackPanel(
+				string) {
+			protected String getCSSClass(org.apache.wicket.feedback.FeedbackMessage message) {
+				String feedbackCss = getFeedbackCss(message);
+				return feedbackCss == null ? super.getCSSClass(message) : feedbackCss;
+			}
+		};
+		feedbackPanel.setEscapeModelStrings(false);
+		return feedbackPanel;
+	}
 
-    public <PropertyType, ComponentType extends FormComponent<PropertyType>, ElementSettings extends AbstractFormElementSettings<ElementSettings>, RowPanel extends DefaultFormRowPanel<PropertyType, ComponentType, ElementSettings>> RowPanel addDefaultRow(
-            RowPanel rowpanel) {
-        return this.addRow(rowpanel);
-    }
+	protected String getFeedbackCssInternal(org.apache.wicket.feedback.FeedbackMessage message) {
+		if (FeedbackMessage.DEBUG == message.getLevel()) {
+			return "debug";
+		}
+		if (FeedbackMessage.INFO == message.getLevel()) {
+			return "info";
+		}
+		if (FeedbackMessage.SUCCESS == message.getLevel()) {
+			return "success";
+		}
+		if (FeedbackMessage.WARNING == message.getLevel()) {
+			return "warning";
+		}
+		if (FeedbackMessage.ERROR == message.getLevel()) {
+			return "danger";
+		}
+		if (FeedbackMessage.FATAL == message.getLevel()) {
+			return "error";
+		}
+		return "primary";
+	}
 
-    protected MarkupContainer newEmptyPanel() {
-        return new EmptyFormPanel();
-    }
+	public <PropertyType, ComponentType extends FormComponent<PropertyType>, ElementSettings extends AbstractFormElementSettings<ElementSettings>, RowPanel extends DefaultFormRowPanel<PropertyType, ComponentType, ElementSettings>> RowPanel addDefaultRow(
+			RowPanel rowpanel) {
+		return this.addRow(rowpanel);
+	}
 
-    public <F extends Serializable> TextFieldPanel<F> addTextField(F propertyPath, FormElementSettings componentSettings) {
-        return this.addDefaultRow(new TextFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
-    }
+	protected MarkupContainer newEmptyPanel() {
+		return new EmptyFormPanel();
+	}
 
-    public CheckBoxPanel addCheckBox(Boolean propertyPath, CheckBoxSettings componentSettings) {
-        return this.addDefaultRow(new CheckBoxPanel(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
-    }
+	public <F extends Serializable> TextFieldPanel<F> addTextField(F propertyPath,
+			FormElementSettings componentSettings) {
+		return this.addDefaultRow(
+				new TextFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
+	}
 
-    public <F extends Serializable> RadioButtonsPanel<F> addRadioButtons(F propertyPath, FormElementSettings componentSettings,
-            IModel<List<F>> choices, IChoiceRenderer<F> renderer) {
-        return this.addDefaultRow(
-                new RadioButtonsPanel<F>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, choices, renderer));
-    }
+	public CheckBoxPanel addCheckBox(Boolean propertyPath, CheckBoxSettings componentSettings) {
+		return this.addDefaultRow(
+				new CheckBoxPanel(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
+	}
 
-    public <F extends Serializable> DropDownPanel<F> addDropDown(F propertyPath, DropDownSettings componentSettings, IOptionRenderer<F> renderer,
-            IModel<List<F>> choices) {
-        return this
-                .addDefaultRow(new DropDownPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer, choices));
-    }
+	public <F extends Serializable> RadioButtonsPanel<F> addRadioButtons(F propertyPath,
+			FormElementSettings componentSettings, IModel<List<F>> choices, IChoiceRenderer<F> renderer) {
+		return this.addDefaultRow(new RadioButtonsPanel<F>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, choices, renderer));
+	}
 
-    public <F extends Serializable> DropDownPanel<F> addDropDown(F propertyPath, DropDownSettings componentSettings, IOptionRenderer<F> renderer,
-            IModel<List<F>>[] choices, IModel<String>[] groupLabels) {
-        return this.addDefaultRow(
-                new DropDownPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer, choices, groupLabels));
-    }
+	public <F extends Serializable> DropDownPanel<F> addDropDown(F propertyPath, DropDownSettings componentSettings,
+			IOptionRenderer<F> renderer, IModel<List<F>> choices) {
+		return this.addDefaultRow(new DropDownPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices));
+	}
 
-    public <F extends Serializable> ListPanel<F> addList(F propertyPath, ListSettings componentSettings, IOptionRenderer<F> renderer,
-            IModel<List<F>> choices) {
-        return this.addDefaultRow(new ListPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer, choices));
-    }
+	public <F extends Serializable> DropDownPanel<F> addDropDown(F propertyPath, DropDownSettings componentSettings,
+			IOptionRenderer<F> renderer, IModel<List<F>>[] choices, IModel<String>[] groupLabels) {
+		return this.addDefaultRow(new DropDownPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices, groupLabels));
+	}
 
-    public <F extends Serializable> ListPanel<F> addList(F propertyPath, ListSettings componentSettings, IOptionRenderer<F> renderer,
-            IModel<List<F>>[] choices, IModel<String>[] groupLabels) {
-        return this.addDefaultRow(
-                new ListPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer, choices, groupLabels));
-    }
+	public <F extends Serializable> ListPanel<F> addList(F propertyPath, ListSettings componentSettings,
+			IOptionRenderer<F> renderer, IModel<List<F>> choices) {
+		return this.addDefaultRow(new ListPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices));
+	}
 
-    public <I extends Serializable> MultiSelectPanel<I> addMultiSelect(//
-            List<I> propertyPath, //
-            MultiSelectSettings componentSettings, //
-            IOptionRenderer<I> renderer, //
-            IModel<List<I>> choices//
-    ) {
-        return this.addDefaultRow(
-                new MultiSelectPanel<I>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer, choices));
-    }
+	public <F extends Serializable> ListPanel<F> addList(F propertyPath, ListSettings componentSettings,
+			IOptionRenderer<F> renderer, IModel<List<F>>[] choices, IModel<String>[] groupLabels) {
+		return this.addDefaultRow(new ListPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices, groupLabels));
+	}
 
-    public <I extends Serializable> MultiSelectPanel<I> addMultiSelect(//
-            List<I> propertyPath, //
-            MultiSelectSettings componentSettings, //
-            IOptionRenderer<I> renderer, //
-            IModel<List<I>>[] choices, //
-            IModel<String>[] groupLabels//
-    ) {
-        return this.addDefaultRow(new MultiSelectPanel<I>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, renderer,
-                choices, groupLabels));
-    }
+	public <I extends Serializable> MultiSelectPanel<I> addMultiSelect(//
+			List<I> propertyPath, //
+			MultiSelectSettings componentSettings, //
+			IOptionRenderer<I> renderer, //
+			IModel<List<I>> choices//
+	) {
+		return this.addDefaultRow(new MultiSelectPanel<I>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices));
+	}
 
-    public <N extends Number & Comparable<N>> NumberFieldPanel<N> addNumberField(N propertyPath, NumberFieldSettings<N> componentSettings) {
-        return this.addDefaultRow(new NumberFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
-    }
+	public <I extends Serializable> MultiSelectPanel<I> addMultiSelect(//
+			List<I> propertyPath, //
+			MultiSelectSettings componentSettings, //
+			IOptionRenderer<I> renderer, //
+			IModel<List<I>>[] choices, //
+			IModel<String>[] groupLabels//
+	) {
+		return this.addDefaultRow(new MultiSelectPanel<I>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, renderer, choices, groupLabels));
+	}
 
-    public <N extends Number & Comparable<N>> NumberTextFieldPanel<N> addNumberTextField(N propertyPath, NumberFieldSettings<N> componentSettings) {
-        return this.addDefaultRow(new NumberTextFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
-    }
+	public <N extends Number & Comparable<N>> NumberFieldPanel<N> addNumberField(N propertyPath,
+			NumberFieldSettings<N> componentSettings) {
+		return this.addDefaultRow(
+				new NumberFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings));
+	}
 
-    public DatePickerPanel<Date> addDatePicker(Date propertyPath, DatePickerSettings componentSettings) {
-        return this.addDatePicker(propertyPath, componentSettings, (Converter<Date, Date>) null);
-    }
+	public <N extends Number & Comparable<N>> NumberTextFieldPanel<N> addNumberTextField(N propertyPath,
+			NumberFieldSettings<N> componentSettings) {
+		return this.addDefaultRow(new NumberTextFieldPanel<>(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings));
+	}
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public <F extends Serializable> DatePickerPanel<F> addDatePicker(F propertyPath, DatePickerSettings componentSettings,
-            Converter<F, Date> dateConverter) {
-        return this.addDefaultRow(
-                (DatePickerPanel) new DatePickerPanel<>(this.getFormModel(), propertyPath, dateConverter, this.getFormSettings(), componentSettings));
-    }
+	public DatePickerPanel<Date> addDatePicker(Date propertyPath, DatePickerSettings componentSettings) {
+		return this.addDatePicker(propertyPath, componentSettings, (Converter<Date, Date>) null);
+	}
 
-    public <F extends Serializable> HiddenFieldPanel<F> addHidden(F propertyPath) {
-        return this.addDefaultRow(new HiddenFieldPanel<F>(this.getFormModel(), propertyPath));
-    }
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public <F extends Serializable> DatePickerPanel<F> addDatePicker(F propertyPath,
+			DatePickerSettings componentSettings, Converter<F, Date> dateConverter) {
+		return this.addDefaultRow((DatePickerPanel) new DatePickerPanel<>(this.getFormModel(), propertyPath,
+				dateConverter, this.getFormSettings(), componentSettings));
+	}
 
-    public TagItTextFieldPanel addTagItTextFieldPanel(String propertyPath, TagItTextFieldSettings componentSettings, IModel<List<String>> choices) {
-        return this.addDefaultRow(new TagItTextFieldPanel(this.getFormModel(), propertyPath, this.getFormSettings(), componentSettings, choices));
-    }
+	public <F extends Serializable> HiddenFieldPanel<F> addHidden(F propertyPath) {
+		return this.addDefaultRow(new HiddenFieldPanel<F>(this.getFormModel(), propertyPath));
+	}
+
+	public TagItTextFieldPanel addTagItTextFieldPanel(String propertyPath, TagItTextFieldSettings componentSettings,
+			IModel<List<String>> choices) {
+		return this.addDefaultRow(new TagItTextFieldPanel(this.getFormModel(), propertyPath, this.getFormSettings(),
+				componentSettings, choices));
+	}
 }
