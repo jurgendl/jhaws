f568262e09c3b2865633d21695b7a5009960a773
jurgendl <jurgen.de.landsheer@gmail.com>
2017-12-18 14:46:30 +0100

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/spin/Spin.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/spin/Spin.java
@@ -1,23 +1,23 @@
 package org.tools.hqlbuilder.webservice.jquery.ui.spin;
 
+import org.tools.hqlbuilder.webservice.jquery.ui.jquery.JQuery;
 import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
 import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
-import org.tools.hqlbuilder.webservice.wicket.WicketApplication;
 
+// 3.1.0
+// https://github.com/fgnass/spin.js/releases
+// http://spin.js.org
 /**
- * add <span id="spinnercontainer"><span id="spinner" style="width:160px;height:160px;"/></span> to your page and give it a place and width/height, use startSpinner() to start and
- * stopSpinner() to stop
+ * add <span id="spinnercontainer"><span id="spinner" style="width:160px;height:160px;"/></span> to your page and give it a place and width/height,
+ * use startSpinner() to start and stopSpinner() to stop
  *
- * @see http ://fgnass.github.io/spin.js/#?lines=13&length=14&width=5&radius=19&corners=1.0&rotate=0&trail=60&speed=1.0&direction=1&shadow=on&hwaccel=on
+ * @see http://spin.js.org/#?lines=20&length=0&width=4&radius=20&scale=1.3&corners=1&opacity=0&rotate=0&direction=1&speed=0.8&trail=32&color=%23000000&fadeColor=transparent&top=50&left=50&shadow=false
  */
 public class Spin {
-	public static CssResourceReference SPIN_CSS = new CssResourceReference(Spin.class, "spin.css");
+    public static CssResourceReference SPIN_CSS = new CssResourceReference(Spin.class, "spin.css");
 
-	public static JavaScriptResourceReference SPIN_JS = new JavaScriptResourceReference(Spin.class, "spin.js");
+    public static JavaScriptResourceReference SPIN_JS = new JavaScriptResourceReference(Spin.class, "spin.js");
 
-	public static JavaScriptResourceReference SPIN_JQUERY_JS = new JavaScriptResourceReference(Spin.class, "jquery.spin.js").addJavaScriptResourceReferenceDependency(Spin.SPIN_JS)
-			.addJavaScriptResourceReferenceDependency(WicketApplication.get().getJavaScriptLibrarySettings().getJQueryReference());
-
-	public static JavaScriptResourceReference SPIN_FACTORY_JS = new JavaScriptResourceReference(Spin.class, "spin.factory.js")
-			.addJavaScriptResourceReferenceDependency(Spin.SPIN_JQUERY_JS);
+    public static JavaScriptResourceReference SPIN_FACTORY_JS = new JavaScriptResourceReference(Spin.class, "spin.factory.js")
+            .addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference()).addJavaScriptResourceReferenceDependency(Spin.SPIN_JS);
 }
--- jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/jquery/ui/spin/spin.factory.js
+++ jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/jquery/ui/spin/spin.factory.js
@@ -1,24 +1,30 @@
+//import {Spinner} from 'spin.js';
 var opts = {
-  lines: 16, // The number of lines to draw
-  length: 16, // The length of each line
+  lines: 20, // The number of lines to draw
+  length: 0, // The length of each line
   width: 4, // The line thickness
   radius: 20, // The radius of the inner circle
+  scale: 1.3, // Scales overall size of the spinner
   corners: 1, // Corner roundness (0..1)
+  color: '#000000', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  opacity: 0, // Opacity of the lines
   rotate: 0, // The rotation offset
   direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#000', // #rgb or #rrggbb or array of colors
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: true, // Whether to render a shadow
-  hwaccel: true, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
+  speed: 0.8, // Rounds per second
+  trail: 32, // Afterglow percentage
+  fps: 20, // Frames per second when using setTimeout() as a fallback in IE 9
   zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
   top: '50%', // Top position relative to parent
-  left: '50%' // Left position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: false, // Box-shadow for the lines
+  position: 'absolute' // Element positioning
 };
 var spinnerInstance;
 function startSpinner() {
 	if(spinnerInstance) {
+		//
 	} else {
 		console.log('start spinner');
 		try {
