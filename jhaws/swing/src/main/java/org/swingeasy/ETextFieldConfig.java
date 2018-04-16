aa289804c433fa179b624503c77ed7a00c276b05
jurgendl <jurgen.de.landsheer@gmail.com>
2018-04-16 19:45:16 +0200

    *

--- jhaws/swing/src/main/java/org/swingeasy/ETextFieldConfig.java
+++ jhaws/swing/src/main/java/org/swingeasy/ETextFieldConfig.java
@@ -7,125 +7,125 @@ import java.util.function.Predicate;
  * @author Jurgen
  */
 public class ETextFieldConfig extends EComponentConfig<ETextFieldConfig> {
-    protected int columns = 0;
+	protected int columns = 0;
 
-    protected boolean enabled = true;
+	protected boolean enabled = true;
 
-    protected boolean selectAllOnFocus = false;
+	protected boolean selectAllOnFocus = false;
 
-    protected String text = null;
+	protected String text = null;
 
-    protected Function<String, String> filterInput = null;
+	protected Function<String, String> filterInput = null;
 
-    protected Predicate<String> textValidator = null;
+	protected Predicate<String> textValidator = null;
 
-    public ETextFieldConfig() {
-        super();
-    }
+	public ETextFieldConfig() {
+		super();
+	}
 
-    public ETextFieldConfig(boolean enabled) {
-        this.enabled = enabled;
-    }
+	public ETextFieldConfig(boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public ETextFieldConfig(boolean enabled, int columns) {
-        this.enabled = enabled;
-        this.columns = columns;
-    }
+	public ETextFieldConfig(boolean enabled, int columns) {
+		this.enabled = enabled;
+		this.columns = columns;
+	}
 
-    public ETextFieldConfig(boolean enabled, String text) {
-        this.enabled = enabled;
-        this.text = text;
-    }
+	public ETextFieldConfig(boolean enabled, String text) {
+		this.enabled = enabled;
+		this.text = text;
+	}
 
-    public ETextFieldConfig(int columns) {
-        this.columns = columns;
-    }
+	public ETextFieldConfig(int columns) {
+		this.columns = columns;
+	}
 
-    public ETextFieldConfig(String text) {
-        this.text = text;
-    }
+	public ETextFieldConfig(String text) {
+		this.text = text;
+	}
 
-    public int getColumns() {
-        return columns;
-    }
+	public int getColumns() {
+		return columns;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public boolean isEnabled() {
-        return enabled;
-    }
+	public boolean isEnabled() {
+		return enabled;
+	}
 
-    public boolean isSelectAllOnFocus() {
-        return selectAllOnFocus;
-    }
+	public boolean isSelectAllOnFocus() {
+		return selectAllOnFocus;
+	}
 
-    public ETextFieldConfig setColumns(int columns) {
-        lockCheck();
-        this.columns = columns;
-        return this;
-    }
+	public ETextFieldConfig setColumns(int columns) {
+		lockCheck();
+		this.columns = columns;
+		return this;
+	}
 
-    public ETextFieldConfig setEnabled(boolean enabled) {
-        lockCheck();
-        this.enabled = enabled;
-        return this;
-    }
+	public ETextFieldConfig setEnabled(boolean enabled) {
+		lockCheck();
+		this.enabled = enabled;
+		return this;
+	}
 
-    public ETextFieldConfig setSelectAllOnFocus(boolean selectAllOnFocus) {
-        lockCheck();
-        this.selectAllOnFocus = selectAllOnFocus;
-        return this;
-    }
+	public ETextFieldConfig setSelectAllOnFocus(boolean selectAllOnFocus) {
+		lockCheck();
+		this.selectAllOnFocus = selectAllOnFocus;
+		return this;
+	}
 
-    public ETextFieldConfig setText(String text) {
-        lockCheck();
-        this.text = text;
-        return this;
-    }
+	public ETextFieldConfig setText(String text) {
+		lockCheck();
+		this.text = text;
+		return this;
+	}
 
-    public Function<String, String> getFilterInput() {
-        return this.filterInput;
-    }
+	public Function<String, String> getFilterInput() {
+		return this.filterInput;
+	}
 
-    public ETextFieldConfig setFilterInput(Function<String, String> filterInput) {
-        lockCheck();
-        this.filterInput = filterInput;
-        return this;
-    }
+	public ETextFieldConfig setFilterInput(Function<String, String> filterInput) {
+		lockCheck();
+		this.filterInput = filterInput;
+		return this;
+	}
 
-    public Predicate<String> getTextValidator() {
-        return this.textValidator;
-    }
+	public Predicate<String> getTextValidator() {
+		return this.textValidator;
+	}
 
-    public ETextFieldConfig setTextValidator(Predicate<String> textValidator) {
-        lockCheck();
-        this.textValidator = textValidator;
-        return this;
-    }
+	public ETextFieldConfig setTextValidator(Predicate<String> textValidator) {
+		lockCheck();
+		this.textValidator = textValidator;
+		return this;
+	}
 
-    public ETextFieldConfig removeRegexFromInput(String regexFilter) {
-        lockCheck();
-        this.filterInput = s -> s.replaceAll(regexFilter, "");
-        return this;
-    }
+	public ETextFieldConfig removeRegexFromInput(String regexFilter) {
+		lockCheck();
+		this.filterInput = s -> s.replaceAll(regexFilter, "");
+		return this;
+	}
 
-    public ETextFieldConfig regexValidateText(String regexFilter) {
-        lockCheck();
-        this.textValidator = s -> s == null || s.equals("") || s.matches(regexFilter);
-        return this;
-    }
+	public ETextFieldConfig regexValidateText(String regexFilter) {
+		lockCheck();
+		this.textValidator = s -> s == null || s.equals("") || s.matches(regexFilter);
+		return this;
+	}
 
-    public ETextFieldConfig filterInput(Function<String, String> filterInput) {
-        lockCheck();
-        this.filterInput = filterInput;
-        return this;
-    }
+	public ETextFieldConfig filterInput(Function<String, String> _filterInput) {
+		lockCheck();
+		this.filterInput = _filterInput;
+		return this;
+	}
 
-    public ETextFieldConfig validateText(Predicate<String> textValidator) {
-        lockCheck();
-        this.textValidator = textValidator;
-        return this;
-    }
+	public ETextFieldConfig validateText(Predicate<String> _textValidator) {
+		lockCheck();
+		this.textValidator = _textValidator;
+		return this;
+	}
 }
--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/WebHelper.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/WebHelper.java
@@ -21,212 +21,226 @@ import ch.lambdaj.Lambda;
 import ch.lambdaj.function.argument.Argument;
 
 public class WebHelper {
-    public static <T> T proxy(Class<T> type) {
-        if (Modifier.isFinal(type.getModifiers())) {
-            throw new IllegalArgumentException(type + " cannot be final");
-        }
-        return Lambda.on(type);
-    }
+	public static <T> T proxy(Class<T> type) {
+		if (Modifier.isFinal(type.getModifiers())) {
+			throw new IllegalArgumentException(type + " cannot be final");
+		}
+		return Lambda.on(type);
+	}
 
-    public static <A> Class<A> type(A arg) {
-        return Lambda.argument(arg).getReturnType();
-    }
+	public static <A> Class<A> type(A arg) {
+		return Lambda.argument(arg).getReturnType();
+	}
 
-    /**
-     * welke generic type implementatie heeft een class implementatie (werkt ook op anonymous inner classes)
-     *
-     * @param object de implementatie class (this waar opgeroepen)
-     * @param classOrInterfaceWithGenericType class of interface waardat de generic type naam op gedeclareerd staat
-     * @param genericTypeIndex de index binnen de &lt; en &gt; waar de generic type naam op gedeclareerd staat; als er maar 1 is wordt die zowiezo
-     *            genomen (maw index 0)
-     * @param requiredGenericType vereiste class (alternatief voor index)
-     *
-     * @return de effectieve implementatie
-     *
-     * @throws IllegalArgumentException wanneer index niet gevonden wordt of geen implementatie van class c
-     */
-    @SuppressWarnings("unchecked")
-    private static <P, G extends P> Class<G> _getImplementation(Object object, Class<?> classOrInterfaceWithGenericType, int genericTypeIndex,
-            Class<P> requiredGenericType) throws IllegalArgumentException {
-        if (object == null || classOrInterfaceWithGenericType == null) {
-            throw new NullPointerException();
-        }
+	/**
+	 * welke generic type implementatie heeft een class implementatie (werkt ook op
+	 * anonymous inner classes)
+	 *
+	 * @param object
+	 *            de implementatie class (this waar opgeroepen)
+	 * @param classOrInterfaceWithGenericType
+	 *            class of interface waardat de generic type naam op gedeclareerd
+	 *            staat
+	 * @param genericTypeIndex
+	 *            de index binnen de &lt; en &gt; waar de generic type naam op
+	 *            gedeclareerd staat; als er maar 1 is wordt die zowiezo genomen
+	 *            (maw index 0)
+	 * @param requiredGenericType
+	 *            vereiste class (alternatief voor index)
+	 *
+	 * @return de effectieve implementatie
+	 *
+	 * @throws IllegalArgumentException
+	 *             wanneer index niet gevonden wordt of geen implementatie van class
+	 *             c
+	 */
+	@SuppressWarnings("unchecked")
+	private static <P, G extends P> Class<G> _getImplementation(Object object, Class<?> classOrInterfaceWithGenericType,
+			int genericTypeIndex, Class<P> requiredGenericType) throws IllegalArgumentException {
+		if (object == null || classOrInterfaceWithGenericType == null) {
+			throw new NullPointerException();
+		}
 
-        Class<?> clazz = object instanceof Class ? Class.class.cast(object) : object.getClass();
-        ParameterizedType parameterizedType = null;
+		Class<?> clazz = object instanceof Class ? Class.class.cast(object) : object.getClass();
+		ParameterizedType parameterizedType = null;
 
-        while (parameterizedType == null && clazz != null) {
-            for (Type type : clazz.getGenericInterfaces()) {
-                if (type instanceof ParameterizedType && ParameterizedType.class.cast(type).getRawType().equals(classOrInterfaceWithGenericType)) {
-                    parameterizedType = ParameterizedType.class.cast(type);
-                }
-            }
+		while (parameterizedType == null && clazz != null) {
+			for (Type type : clazz.getGenericInterfaces()) {
+				if (type instanceof ParameterizedType
+						&& ParameterizedType.class.cast(type).getRawType().equals(classOrInterfaceWithGenericType)) {
+					parameterizedType = ParameterizedType.class.cast(type);
+				}
+			}
 
-            if (parameterizedType == null) {
-                Type type = clazz.getGenericSuperclass();
-                if (type instanceof ParameterizedType && ParameterizedType.class.cast(type).getRawType().equals(classOrInterfaceWithGenericType)) {
-                    parameterizedType = ParameterizedType.class.cast(clazz.getGenericSuperclass());
-                }
-            }
+			if (parameterizedType == null) {
+				Type type = clazz.getGenericSuperclass();
+				if (type instanceof ParameterizedType
+						&& ParameterizedType.class.cast(type).getRawType().equals(classOrInterfaceWithGenericType)) {
+					parameterizedType = ParameterizedType.class.cast(clazz.getGenericSuperclass());
+				}
+			}
 
-            if (parameterizedType == null) {
-                clazz = clazz.getSuperclass();
-            }
-        }
+			if (parameterizedType == null) {
+				clazz = clazz.getSuperclass();
+			}
+		}
 
-        if (parameterizedType == null) {
-            throw new IllegalArgumentException("no implementation");
-        }
+		if (parameterizedType == null) {
+			throw new IllegalArgumentException("no implementation");
+		}
 
-        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+		Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
 
-        if (genericTypeIndex == -1 && actualTypeArguments.length == 1) {
-            return (Class<G>) actualTypeArguments[0];
-        }
+		if (genericTypeIndex == -1 && actualTypeArguments.length == 1) {
+			return (Class<G>) actualTypeArguments[0];
+		}
 
-        if (genericTypeIndex == -1) {
-            if (requiredGenericType == null) {
-                throw new IllegalArgumentException("requiredGenericType", new NullPointerException());
-            }
+		if (genericTypeIndex == -1) {
+			if (requiredGenericType == null) {
+				throw new IllegalArgumentException("requiredGenericType", new NullPointerException());
+			}
 
-            for (Type actualTypeArgument : actualTypeArguments) {
-                if (requiredGenericType.isAssignableFrom((Class<?>) actualTypeArgument)) {
-                    return (Class<G>) actualTypeArgument;
-                }
-            }
-            throw new IllegalArgumentException(Arrays.toString(actualTypeArguments) + "<>" + requiredGenericType);
-        }
+			for (Type actualTypeArgument : actualTypeArguments) {
+				if (requiredGenericType.isAssignableFrom((Class<?>) actualTypeArgument)) {
+					return (Class<G>) actualTypeArgument;
+				}
+			}
+			throw new IllegalArgumentException(Arrays.toString(actualTypeArguments) + "<>" + requiredGenericType);
+		}
 
-        try {
-            Type type = actualTypeArguments[genericTypeIndex];
-            if (type instanceof ParameterizedType) {
-                return (Class<G>) ((ParameterizedType) type).getRawType();
-            }
-            return (Class<G>) type;
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            throw new IllegalArgumentException(Arrays.toString(actualTypeArguments), ex);
-        }
-    }
+		try {
+			Type type = actualTypeArguments[genericTypeIndex];
+			if (type instanceof ParameterizedType) {
+				return (Class<G>) ((ParameterizedType) type).getRawType();
+			}
+			return (Class<G>) type;
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			throw new IllegalArgumentException(Arrays.toString(actualTypeArguments), ex);
+		}
+	}
 
-    /**
-     * @see #getImplementation(Object, Class, int) met index=-1
-     */
-    public static <G> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType) throws IllegalArgumentException {
-        return _getImplementation(object, classOrInterfaceWithGenericType, -1, null);
-    }
+	/**
+	 * @see #getImplementation(Object, Class, int) met index=-1
+	 */
+	public static <G> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType)
+			throws IllegalArgumentException {
+		return _getImplementation(object, classOrInterfaceWithGenericType, -1, null);
+	}
 
-    /**
-     * @see #getImplementation(Object, Class, int)
-     */
-    public static <G> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType, int genericTypeIndex)
-            throws IllegalArgumentException {
-        return _getImplementation(object, classOrInterfaceWithGenericType, genericTypeIndex, null);
-    }
+	/**
+	 * @see #getImplementation(Object, Class, int)
+	 */
+	public static <G> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType,
+			int genericTypeIndex) throws IllegalArgumentException {
+		return _getImplementation(object, classOrInterfaceWithGenericType, genericTypeIndex, null);
+	}
 
-    /**
-     * @see #getImplementation(Object, Class, int)
-     */
-    public static <P, G extends P> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType, Class<P> requiredGenericType)
-            throws IllegalArgumentException {
-        return _getImplementation(object, classOrInterfaceWithGenericType, -1, requiredGenericType);
-    }
+	/**
+	 * @see #getImplementation(Object, Class, int)
+	 */
+	public static <P, G extends P> Class<G> getImplementation(Object object, Class<?> classOrInterfaceWithGenericType,
+			Class<P> requiredGenericType) throws IllegalArgumentException {
+		return _getImplementation(object, classOrInterfaceWithGenericType, -1, requiredGenericType);
+	}
 
-    public static <A> String name(A arg) {
-        return arg(arg).getInkvokedPropertyName();
-    }
+	public static <A> String name(A arg) {
+		return arg(arg).getInkvokedPropertyName();
+	}
 
-    public static <A> Argument<A> arg(A arg) {
-        return Lambda.argument(arg);
-    }
+	public static <A> Argument<A> arg(A arg) {
+		return Lambda.argument(arg);
+	}
 
-    public static void run(EnhancedRunnable runnable) {
-        run(null, runnable, Exception::printStackTrace);
-    }
+	public static void run(EnhancedRunnable runnable) {
+		run(null, runnable, Exception::printStackTrace);
+	}
 
-    public static void run(Long sleep, EnhancedRunnable runnable, Consumer<Exception> exceptionHandler) {
-        Thread thread = new Thread(() -> {
-            boolean eternal = sleep != null;
-            try {
-                do {
-                    runnable.run();
-                    if (sleep != null) {
-                        sleep(sleep);
-                    }
-                } while (eternal);
-            } catch (Exception ex) {
-                if (exceptionHandler != null) {
-                    exceptionHandler.accept(ex);
-                }
-            }
-        });
-        thread.setDaemon(true);
-        thread.start();
-    }
+	public static void run(Long sleep, EnhancedRunnable runnable, Consumer<Exception> exceptionHandler) {
+		Thread thread = new Thread(() -> {
+			boolean eternal = sleep != null;
+			try {
+				do {
+					runnable.run();
+					if (sleep != null) {
+						sleep(sleep);
+					}
+				} while (eternal);
+			} catch (Exception ex) {
+				if (exceptionHandler != null) {
+					exceptionHandler.accept(ex);
+				}
+			}
+		});
+		thread.setDaemon(true);
+		thread.start();
+	}
 
-    public static void sleep(long ms) {
-        try {
-            Thread.sleep(ms);
-        } catch (InterruptedException ex) {
-            //
-        }
-    }
+	public static void sleep(long ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException ex) {
+			//
+		}
+	}
 
-    public static <T> T create(Class<T> modelType) {
-        return BeanUtils.instantiate(modelType);
-    }
+	public static <T> T create(Class<T> modelType) {
+		return BeanUtils.instantiateClass(modelType);
+	}
 
-    public static <T> IModel<T> model(Class<T> modelType) {
-        return model(create(modelType));
-    }
+	public static <T> IModel<T> model(Class<T> modelType) {
+		return model(create(modelType));
+	}
 
-    public static <T> IModel<T> model(T model) {
-        return new CompoundPropertyModel<>(model);
-    }
+	public static <T> IModel<T> model(T model) {
+		return new CompoundPropertyModel<>(model);
+	}
 
-    public static <C extends Component> C show(C component) {
-        if (component == null) return component;
-        component.setOutputMarkupPlaceholderTag(true);
-        component.setRenderBodyOnly(false);
-        component.setOutputMarkupId(true);
-        return component;
-    }
+	public static <C extends Component> C show(C component) {
+		if (component == null)
+			return component;
+		component.setOutputMarkupPlaceholderTag(true);
+		component.setRenderBodyOnly(false);
+		component.setOutputMarkupId(true);
+		return component;
+	}
 
-    public static <C extends Component> C hide(C component) {
-        if (component == null) return component;
-        component.setOutputMarkupPlaceholderTag(false);
-        component.setRenderBodyOnly(true);
-        component.setOutputMarkupId(false);
-        return component;
-    }
+	public static <C extends Component> C hide(C component) {
+		if (component == null)
+			return component;
+		component.setOutputMarkupPlaceholderTag(false);
+		component.setRenderBodyOnly(true);
+		component.setOutputMarkupId(false);
+		return component;
+	}
 
-    public static void untag(ComponentTag tag, String tagId) {
-        tag(tag, tagId, null);
-    }
+	public static void untag(ComponentTag tag, String tagId) {
+		tag(tag, tagId, null);
+	}
 
-    public static void tag(ComponentTag tag, String tagId, Object value) {
-        if (value == null || (value instanceof String && StringUtils.isBlank(String.class.cast(value)))) {
-            tag.getAttributes().remove(tagId);
-        } else {
-            tag.getAttributes().put(tagId, value);
-        }
-    }
+	public static void tag(ComponentTag tag, String tagId, Object value) {
+		if (value == null || (value instanceof String && StringUtils.isBlank(String.class.cast(value)))) {
+			tag.getAttributes().remove(tagId);
+		} else {
+			tag.getAttributes().put(tagId, value);
+		}
+	}
 
-    public static void classAdd(ComponentTag tag, String clazz) {
-        tag.getAttributes().put("class", tag.getAttributes().getString("class") + " " + clazz.toString());
-    }
+	public static void classAdd(ComponentTag tag, String clazz) {
+		tag.getAttributes().put("class", tag.getAttributes().getString("class") + " " + clazz.toString());
+	}
 
-    public static PageParameters clone(PageParameters pageParameters, String... ids) {
-        PageParameters pp = new PageParameters();
-        for (String id : ids) {
-            if (StringUtils.isNotBlank(pageParameters.get(id).toOptionalString())) {
-                pp.add(id, pageParameters.get(id).toOptionalString());
-            }
-        }
-        return pp;
-    }
+	public static PageParameters clone(PageParameters pageParameters, String... ids) {
+		PageParameters pp = new PageParameters();
+		for (String id : ids) {
+			if (StringUtils.isNotBlank(pageParameters.get(id).toOptionalString())) {
+				pp.add(id, pageParameters.get(id).toOptionalString());
+			}
+		}
+		return pp;
+	}
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static IModel<List<String>> model(String... options) {
-        return (IModel) Model.of(Arrays.asList(options));
-    }
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static IModel<List<String>> model(String... options) {
+		return (IModel) Model.of(Arrays.asList(options));
+	}
 }
