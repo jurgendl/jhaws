3af26c279eb90e53098d826710f348026b0a3a04
jurgendl <jurgen.de.landsheer@gmail.com>
2018-05-04 16:44:40 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/RatingFieldPanel2.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/RatingFieldPanel2.java
@@ -17,50 +17,52 @@ import org.tools.hqlbuilder.webservice.wicket.forms.common.RatingFieldSettings;
  */
 @SuppressWarnings("serial")
 public class RatingFieldPanel2 extends DefaultFormRowPanel<Integer, NumberTextField<Integer>, RatingFieldSettings> {
-    public RatingFieldPanel2(IModel<?> model, Integer propertyPath, FormSettings formSettings, RatingFieldSettings componentSettings) {
-        super(model, propertyPath, formSettings, componentSettings);
-    }
+	public RatingFieldPanel2(IModel<?> model, Integer propertyPath, FormSettings formSettings,
+			RatingFieldSettings componentSettings) {
+		super(model, propertyPath, formSettings, componentSettings);
+	}
 
-    @Override
-    protected NumberTextField<Integer> createComponent(IModel<Integer> model, Class<Integer> valueType) {
-        return new NumberTextField<Integer>(VALUE, model, valueType) {
-            @Override
-            protected void onComponentTag(ComponentTag tag) {
-                super.onComponentTag(tag);
-                onFormComponentTag(tag);
-                RatingFieldSettings settings = getComponentSettings();
-                if (getComponentSettings().isReadOnly()) {
-                    tag(tag, "data-readonly", "");
-                }
-                if (Boolean.TRUE.equals(getComponentSettings().getInline())) {
-                    tag(tag, "data-inline", "");
-                }
-                tag(tag, "data-icon-lib", "fa");
-                tag(tag, "data-active-icon", "fa-star text-primary");
-                tag(tag, "data-inactive-icon", "fa-star-o text-secondary");
-                if (Boolean.TRUE.equals(getComponentSettings().getClearable())) {
-                    tag(tag, "data-clearable-icon", "fa-remove text-danger");
-                    tag(tag, "data-clearable", ".");
-                    tag(tag, "data-clearable-remain", "true");
-                }
-                tag(tag, "min", 1);
-                tag(tag, "max", settings.getMaximum());
-                tag(tag, "data-min", 1);
-                tag(tag, "data-max", settings.getMaximum());
-                tag(tag, "step", 1);
-            }
-        };
-    }
+	@Override
+	protected NumberTextField<Integer> createComponent(IModel<Integer> model, Class<Integer> valueType) {
+		return new NumberTextField<Integer>(VALUE, model, valueType) {
+			@Override
+			protected void onComponentTag(ComponentTag tag) {
+				super.onComponentTag(tag);
+				onFormComponentTag(tag);
+				RatingFieldSettings settings = getComponentSettings();
+				if (getComponentSettings().isReadOnly()) {
+					tag(tag, "data-readonly", "");
+				}
+				if (Boolean.TRUE.equals(getComponentSettings().getInline())) {
+					tag(tag, "data-inline", "");
+				}
+				tag(tag, "data-icon-lib", "");
+				tag(tag, "data-active-icon", "fas fa-star text-primary");
+				tag(tag, "data-inactive-icon", "far fa-star text-secondary");
+				if (Boolean.TRUE.equals(getComponentSettings().getClearable())) {
+					tag(tag, "data-clearable-icon", "fas fa-times-circle text-danger");
+					tag(tag, "data-clearable", ".");
+					tag(tag, "data-clearable-remain", "true");
+				}
+				tag(tag, "min", 1);
+				tag(tag, "max", settings.getMaximum());
+				tag(tag, "data-min", 1);
+				tag(tag, "data-max", settings.getMaximum());
+				tag(tag, "step", 1);
+			}
+		};
+	}
 
-    @Override
-    protected void setupPlaceholder(ComponentTag tag) {
-        //
-    }
+	@Override
+	protected void setupPlaceholder(ComponentTag tag) {
+		//
+	}
 
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        super.renderHead(response);
-        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(getClass(), "bootstrap-rating-input.js")
-                .addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference())));
-    }
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(getClass(), "bootstrap-rating-input.js")
+						.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference())));
+	}
 }
