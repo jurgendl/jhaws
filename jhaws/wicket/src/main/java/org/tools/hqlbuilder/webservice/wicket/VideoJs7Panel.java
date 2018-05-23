11c1eff59a046f2137cee6fab5d7b890cc70d463
jurgendl <jurgen.de.landsheer@gmail.com>
2018-05-24 01:35:03 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/VideoJs7Panel.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/VideoJs7Panel.java
@@ -16,72 +16,68 @@ import org.tools.hqlbuilder.webservice.libraries.videojs.VideoJs7;
 // http://bgbaden-frauen.ac.at/~infwurst/Hagmann/videojs/VideoJS%20setup%20guide%20to%20scale%20for%20responsive%20design%20on%20all%20browsers%20&%20mobile.html
 @SuppressWarnings("serial")
 public class VideoJs7Panel extends Panel {
-    @SuppressWarnings("unused")
-    private VideoJs7PanelConfig config;
+	@SuppressWarnings("unused")
+	private VideoJs7PanelConfig config;
 
-    public VideoJs7Panel(String id, VideoJs7PanelConfig config) {
-        super(id);
-        this.config = config;
-        add(createContainer(config));
-    }
+	public VideoJs7Panel(String id, VideoJs7PanelConfig config) {
+		super(id);
+		this.config = config;
+		add(createContainer(config));
+	}
 
-    protected WebMarkupContainer createContainer(VideoJs7PanelConfig _config) {
-        WebMarkupContainer container = new WebMarkupContainer("container");
-        container.add(createPlayer(_config));
-        container.add(createActions(_config));
-        return container;
-    }
+	protected WebMarkupContainer createContainer(VideoJs7PanelConfig _config) {
+		WebMarkupContainer container = new WebMarkupContainer("container");
+		container.add(createPlayer(_config));
+		container.add(createActions(_config));
+		return container;
+	}
 
-    protected WebMarkupContainer createPlayer(VideoJs7PanelConfig _config) {
-        boolean size = _config.getW() != null && _config.getH() != null && _config.getW() > 0 && _config.getH() > 0;
+	protected WebMarkupContainer createPlayer(VideoJs7PanelConfig _config) {
+		boolean size = _config.getW() != null && _config.getH() != null && _config.getW() > 0 && _config.getH() > 0;
 
-        WebMarkupContainer videocontainer = new WebMarkupContainer("videocontainer");
+		WebMarkupContainer videocontainer = new WebMarkupContainer("videocontainer");
 
-        WebMarkupContainer video = new WebMarkupContainer("video");
-        videocontainer.add(video);
+		WebMarkupContainer video = new WebMarkupContainer("video");
+		videocontainer.add(video);
 
-        RepeatingView sourceRepeater = new RepeatingView("source");
-        video.add(sourceRepeater);
-        _config.getSources().forEach(_source -> {
-            WebMarkupContainer source = new WebMarkupContainer(sourceRepeater.newChildId());
-            sourceRepeater.add(source);
-            source.add(new AttributeModifier("src", _source.getUrl()));
-            if (StringUtils.isNotBlank(_source.getMimetype())) {
-                source.add(new AttributeModifier("type", _source.getMimetype()));
-            }
-        });
-        video.add(new AttributeModifier("preload", "none")); // metadata
-        if (StringUtils.isNotBlank(_config.getSplashUrl())) {
-            video.add(new AttributeModifier("poster", _config.getSplashUrl()));
-        }
-        if (Boolean.TRUE.equals(_config.getLoop())) {
-            video.add(new AttributeModifier("loop", "true"));
-        }
-        if (size) {
-            video.add(new AttributeModifier("aspectRatio", _config.getW() + ":" + _config.getH()));
-            video.add(new AttributeModifier("width", _config.getW()));
-            video.add(new AttributeModifier("height", _config.getH()));
-        } else {
-            video.add(new AttributeModifier("width", "auto"));
-            video.add(new AttributeModifier("height", "auto"));
-        }
-        video.add(new AttributeModifier("fluid", "true"));
-        video.add(new AttributeModifier("muted", "true"));
-        video.add(AttributeModifier.remove("controls"));
+		RepeatingView sourceRepeater = new RepeatingView("source");
+		video.add(sourceRepeater);
+		_config.getSources().forEach(_source -> {
+			WebMarkupContainer source = new WebMarkupContainer(sourceRepeater.newChildId());
+			sourceRepeater.add(source);
+			source.add(new AttributeModifier("src", _source.getUrl()));
+			if (StringUtils.isNotBlank(_source.getMimetype())) {
+				source.add(new AttributeModifier("type", _source.getMimetype()));
+			}
+		});
+		video.add(new AttributeModifier("preload", "none")); // metadata
+		if (StringUtils.isNotBlank(_config.getSplashUrl())) {
+			video.add(new AttributeModifier("poster", _config.getSplashUrl()));
+		}
+		if (Boolean.TRUE.equals(_config.getLoop())) {
+			video.add(new AttributeModifier("loop", "true"));
+		}
+		if (size) {
+			video.add(new AttributeModifier("aspectRatio", _config.getW() + ":" + _config.getH()));
+			video.add(new AttributeModifier("width", _config.getW()));
+			video.add(new AttributeModifier("height", _config.getH()));
+		}
+		video.add(new AttributeModifier("fluid", "true"));
+		video.add(new AttributeModifier("muted", "true"));
 
-        return videocontainer;
-    }
+		return videocontainer;
+	}
 
-    protected WebMarkupContainer createActions(VideoJs7PanelConfig _config) {
-        WebMarkupContainer actions = new WebMarkupContainer("actions");
-        return actions;
-    }
+	protected WebMarkupContainer createActions(VideoJs7PanelConfig _config) {
+		WebMarkupContainer actions = new WebMarkupContainer("actions");
+		return actions;
+	}
 
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        super.renderHead(response);
-        response.render(CssHeaderItem.forReference(VideoJs7.CSS));
-        response.render(JavaScriptHeaderItem.forReference(VideoJs7.JS));
-        response.render(JavaScriptHeaderItem.forReference(VideoJs7.JS_PREVENT_MULTIPLE));
-    }
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(CssHeaderItem.forReference(VideoJs7.CSS));
+		response.render(JavaScriptHeaderItem.forReference(VideoJs7.JS));
+		// response.render(JavaScriptHeaderItem.forReference(VideoJs7.JS_PREVENT_MULTIPLE));
+	}
 }
