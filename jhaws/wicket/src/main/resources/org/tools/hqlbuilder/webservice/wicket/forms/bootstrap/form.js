77cbf38cfe3ce22dc0ac52cfc763dd68de23fcb8
jurgendl <jurgen.de.landsheer@gmail.com>
2018-10-08 16:19:25 +0200

    *

--- jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/form.js
+++ jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/forms/bootstrap/form.js
@@ -1,3 +1,4 @@
+/* activate bootstrap validation */
 (function() {
   'use strict';
   window.addEventListener('load', function() {
@@ -15,3 +16,20 @@
     });
   }, false);
 })();
+
+// activate with $('form').preventDoubleSubmission();
+//jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+  // Keep chainability
+  return this;
+};
