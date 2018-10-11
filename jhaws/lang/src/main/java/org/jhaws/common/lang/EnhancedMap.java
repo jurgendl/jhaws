c9c0f105759c7666677edd3895be46bf7aee8530
jurgendl <jurgen.de.landsheer@gmail.com>
2018-10-11 12:44:14 +0200

    *

--- jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedMap.java
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedMap.java
@@ -2,6 +2,7 @@ package org.jhaws.common.lang;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public interface EnhancedMap<K, V> extends Map<K, V> {
@@ -13,11 +14,23 @@ public interface EnhancedMap<K, V> extends Map<K, V> {
         return keySet().stream();
     }
 
+    default void forEachKey(Consumer<K> action) {
+        streamKeys().forEach(action);
+    }
+
     default Stream<V> streamValues() {
         return values().stream();
     }
 
+    default void forEachValue(Consumer<V> action) {
+        streamValues().forEach(action);
+    }
+
     default Stream<Entry<K, V>> stream() {
         return entrySet().stream();
+    }
+
+    default void forEach(Consumer<? super Entry<K, V>> action) {
+        entrySet().forEach(action);
     }
 }
