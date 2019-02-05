3ea482eed8d103a2b632375fa53e6c690f784ceb
jurgendl <jurgen.de.landsheer@gmail.com>
2019-02-05 14:59:36 +0100

    *

--- jhaws/net/src/test/java/org/jhaws/common/net/client/StreamHttpClientTest.java
+++ jhaws/net/src/test/java/org/jhaws/common/net/client/StreamHttpClientTest.java
@@ -26,44 +26,51 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
 import org.jhaws.common.io.FilePath;
 import org.jhaws.common.net.resteasy.MultipartFormDataOutputEntity;
 import org.jhaws.common.net.resteasy.client.RestEasyClient;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class StreamHttpClientTest {
-    private TestRestServer server;
+    private static TestRestServer server;
 
-    private HTTPClient hc;
+    private static HTTPClient hc;
 
-    private StreamingResource testResource;
+    private static StreamingResource testResource;
 
     @SuppressWarnings("unused")
-    private StreamingResourceI proxy;
+    private static StreamingResourceI proxy;
 
-    private FilePath fp = new FilePath("pom.xml");
+    private static FilePath file;
 
-    private UriBuilder getBase() {
-        return UriBuilder.fromPath(server.baseUri()).path(StreamingResourceI.PATH);
-    }
-
-    @Before
-    public void before() throws Exception {
+    static {
         try {
+            file = new FilePath("pom.xml");
             hc = new HTTPClient();
             testResource = new StreamingResource();
-            testResource.data.put(fp.getFileNameString(), fp.readAllBytes());
-            testResource.len.put(fp.getFileNameString(), fp.getFileSize());
+            testResource.data.put(file.getFileNameString(), file.readAllBytes());
+            testResource.len.put(file.getFileNameString(), file.getFileSize());
             server = TestRestServer.create(testResource);
-            proxy = new RestEasyClient<>(server.baseUri(), StreamingResourceI.class).proxy();
+            proxy = new RestEasyClient<>(getBaseExclResourcePath().build(), StreamingResourceI.class).proxy();
         } catch (Exception ex) {
             ex.printStackTrace();
-            throw ex;
+            file = null;
+            hc = null;
+            testResource = null;
+            server = null;
+            proxy = null;
         }
     }
 
-    @After
-    public void afterClass() throws Exception {
+    private static UriBuilder getBaseInclResourcePath() {
+        return getBaseExclResourcePath().path(StreamingResourceI.PATH);
+    }
+
+    private static UriBuilder getBaseExclResourcePath() {
+        return UriBuilder.fromPath(server.baseUri());
+    }
+
+    @AfterClass
+    static public void afterClass() throws Exception {
         try {
             server.close();
         } catch (Exception ex) {
@@ -77,14 +84,11 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testUploadForm() {
+    public void test_UploadForm() {
         try {
-            String url = getBase().build().toASCIIString() + StreamingResource.UPLOAD_FORM;
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(url);
             MultipartFormDataOutput mdo = new MultipartFormDataOutput();
-            mdo.addFormData("attachment", fp.newBufferedInputStream(), MediaType.APPLICATION_OCTET_STREAM_TYPE, fp.getFileNameString());
-            Response r = target.request().post(MultipartFormDataOutputEntity.entity(mdo));
+            mdo.addFormData("attachment", file.newBufferedInputStream(), MediaType.APPLICATION_OCTET_STREAM_TYPE, file.getFileNameString());
+            Response r = target().path(StreamingResource.UPLOAD_FORM).request().post(MultipartFormDataOutputEntity.entity(mdo));
             Assert.assertEquals(200, r.getStatus());
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -93,7 +97,7 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testListProxy() {
+    public void proxy_List() {
         try {
             ResteasyClient client = client();
             ResteasyWebTarget target = client.target(server.baseUri());
@@ -106,16 +110,13 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testList() {
+    public void test_List() {
         try {
-            String url = getBase().build().toASCIIString() + StreamingResource.LIST;
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(url);
-            String response = target.request().get(String.class);
+            String response = target().path(StreamingResource.LIST).request().get(String.class);
             System.out.println(response);
             JsonArray a = Json.createReader(new InputStreamReader(new ByteArrayInputStream(response.getBytes()))).read().asJsonArray();
             Assert.assertEquals(1, a.size());
-            Assert.assertTrue(JsonString.class.cast(a.get(0)).getString().contains(fp.getFileNameString()));
+            Assert.assertTrue(JsonString.class.cast(a.get(0)).getString().contains(file.getFileNameString()));
         } catch (Exception ex) {
             ex.printStackTrace();
             Assert.fail();
@@ -123,11 +124,9 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadGet() {
+    public void test_DownloadGet() {
         try {
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(getBase().build().toASCIIString());
-            Builder request = target.path(StreamingResource.DOWNLOAD_GET).queryParam("file", fp.getFileNameString()).request();
+            Builder request = target().path(StreamingResource.DOWNLOAD_GET).queryParam("file", file.getFileNameString()).request();
             Invocation get = request.buildGet();
             Response response = get.invoke();
             Assert.assertEquals(200, response.getStatus());
@@ -140,12 +139,12 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadGetProxy() {
+    public void proxy_DownloadGet() {
         try {
             ResteasyClient client = client();
             ResteasyWebTarget target = client.target(server.baseUri());
             StreamingResourceI simple = target.proxy(StreamingResourceI.class);
-            Response response = simple.downloadGet(fp.getFileNameString());
+            Response response = simple.downloadGet(file.getFileNameString());
             Assert.assertEquals(200, response.getStatus());
             InputStream readEntity = response.readEntity(InputStream.class);
             System.out.println(new String(IOUtils.readFully(readEntity, response.getLength())));
@@ -156,13 +155,11 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadForm() {
+    public void test_DownloadForm() {
         try {
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(getBase().build().toASCIIString());
-            Builder request = target.path(StreamingResource.DOWNLOAD_FORM).request();
+            Builder request = target().path(StreamingResource.DOWNLOAD_FORM).request();
             MultivaluedMap<String, String> formData = new MultivaluedHashMap<>();
-            formData.add("file", fp.getFileNameString());
+            formData.add("file", file.getFileNameString());
             Invocation post = request.buildPost(Entity.form(formData));
             Response response = post.invoke();
             Assert.assertEquals(200, response.getStatus());
@@ -175,12 +172,12 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadFormProxy() {
+    public void proxy_DownloadForm() {
         try {
             ResteasyClient client = client();
             ResteasyWebTarget target = client.target(server.baseUri());
             StreamingResourceI simple = target.proxy(StreamingResourceI.class);
-            Response response = simple.downloadForm(fp.getFileNameString());
+            Response response = simple.downloadForm(file.getFileNameString());
             Assert.assertEquals(200, response.getStatus());
             InputStream readEntity = response.readEntity(InputStream.class);
             System.out.println(new String(IOUtils.readFully(readEntity, response.getLength())));
@@ -191,11 +188,9 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadFile() {
+    public void test_DownloadStream() {
         try {
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(getBase().build().toASCIIString());
-            Builder request = target.path(StreamingResource.DOWNLOAD_STREAM).queryParam("file", fp.getFileNameString()).request();
+            Builder request = target().path(StreamingResource.DOWNLOAD_STREAM).queryParam("file", file.getFileNameString()).request();
             Invocation get = request.buildGet();
             Response response = get.invoke();
             Assert.assertEquals(200, response.getStatus());
@@ -207,20 +202,27 @@ public class StreamHttpClientTest {
         }
     }
 
-    ResteasyClient client;
+    private ResteasyClient client;
 
     private ResteasyClient client() {
         if (client == null) client = new ResteasyClientBuilder().httpEngine(new ApacheHttpClient43Engine(HttpClientBuilder.create().build())).build();
         return client;
     }
 
+    private ResteasyWebTarget target;
+
+    private ResteasyWebTarget target() {
+        if (target == null) target = client().target(getBaseInclResourcePath());
+        return target;
+    }
+
     @Test
-    public void testUploadBin() {
+    public void proxy_UploadStream() {
         try {
             ResteasyClient client = client();
             ResteasyWebTarget target = client.target(server.baseUri());
             StreamingResourceI simple = target.proxy(StreamingResourceI.class);
-            simple.uploadStream(fp.getFileNameString(), fp.newBufferedInputStream());
+            simple.uploadStream(file.getFileNameString(), file.newBufferedInputStream());
         } catch (Exception ex) {
             ex.printStackTrace();
             Assert.fail();
@@ -228,11 +230,9 @@ public class StreamHttpClientTest {
     }
 
     @Test
-    public void testDownloadFileAlt() {
+    public void test_DownloadStreamInResponse() {
         try {
-            ResteasyClient client = client();
-            ResteasyWebTarget target = client.target(getBase().build().toASCIIString());
-            Builder request = target.path(StreamingResource.DOWNLOAD_STREAM_IN_RESPONSE).queryParam("file", fp.getFileNameString()).request();
+            Builder request = target().path(StreamingResource.DOWNLOAD_STREAM_IN_RESPONSE).queryParam("file", file.getFileNameString()).request();
             Invocation get = request.buildGet();
             Response response = get.invoke();
             Assert.assertEquals(200, response.getStatus());
