241b4a1362be3b9047733f89245ed0a3faf13ad8
jurgendl <jurgen.de.landsheer@gmail.com>
2019-03-13 11:21:01 +0100

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/less/LessResourceReference.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/less/LessResourceReference.java
@@ -32,8 +32,7 @@ import ro.isdc.wro.model.resource.processor.impl.css.JawrCssMinifierProcessor;
 // ((npm install less))
 // npm install -g less
 /**
- * response.render(CssHeaderItem.forReference(new
- * lessResourceReference(WicketCSSRoot.class, "table.less")));
+ * response.render(CssHeaderItem.forReference(new lessResourceReference(WicketCSSRoot.class, "table.less")));
  *
  * @see http://less-lang.com/
  * @see https://code.google.com/p/wro4j/
@@ -42,184 +41,181 @@ import ro.isdc.wro.model.resource.processor.impl.css.JawrCssMinifierProcessor;
  */
 @SuppressWarnings("serial")
 public class LessResourceReference extends StreamResourceReference implements IResourceStream {
-	protected static final Logger logger = LoggerFactory.getLogger(SassResourceReference.class);
+    protected static final Logger logger = LoggerFactory.getLogger(SassResourceReference.class);
 
-	protected final String charset = "utf-8";
+    protected final String charset = "utf-8";
 
-	protected final String contentType = "text/css";
+    protected final String contentType = "text/css";
 
-	protected transient Bytes length = null;
+    protected transient Bytes length = null;
 
-	protected transient String css = "";
+    protected transient String css = "";
 
-	protected transient Time lastModified = null;
+    protected transient Time lastModified = null;
 
-	protected transient ResourcePreProcessor lessCssProcessor;
+    protected transient ResourcePreProcessor lessCssProcessor;
 
-	protected transient ResourcePreProcessor cssCompressorProcessor;
+    protected transient ResourcePreProcessor cssCompressorProcessor;
 
-	public LessResourceReference(Class<?> scope, String name) {
-		super(scope, name);
-	}
+    public LessResourceReference(Class<?> scope, String name) {
+        super(scope, name);
+    }
 
-	@Override
-	public void close() throws IOException {
-		//
-	}
+    @Override
+    public void close() throws IOException {
+        //
+    }
 
-	@Override
-	public String getContentType() {
-		return this.contentType;
-	}
+    @Override
+    public String getContentType() {
+        return this.contentType;
+    }
 
-	public ResourcePreProcessor getCssCompressorProcessor() {
-		if (this.cssCompressorProcessor == null) {
-			this.cssCompressorProcessor = new JawrCssMinifierProcessor();
-		}
-		return this.cssCompressorProcessor;
-	}
+    public ResourcePreProcessor getCssCompressorProcessor() {
+        if (this.cssCompressorProcessor == null) {
+            this.cssCompressorProcessor = new JawrCssMinifierProcessor();
+        }
+        return this.cssCompressorProcessor;
+    }
 
-	@Override
-	public InputStream getInputStream() throws ResourceStreamNotFoundException {
-		try {
-			boolean rebuild = false;
-			String fullPath = this.getResourcePath() + '/' + this.getLessName();
-			if (this.css == null) {
-				logger.info("building " + fullPath + " because is new");
-				rebuild = true;
-			} else if (this.lastModified == null) {
-				logger.info("building " + fullPath + " because out of date");
-				rebuild = true;
-			} else if (WebApplication.get().usesDevelopmentConfig()) {
-				try {
-					if (this.lastModified.getMilliseconds() < this.getClass().getClassLoader().getResource(fullPath)
-							.openConnection().getLastModified()) {
-						logger.info("building " + fullPath + " because template out of date");
-						rebuild = true;
-					}
-				} catch (IOException ex1) {
-					logger.info("building " + fullPath + " because of exception: " + ex1);
-					rebuild = true;
-				}
-			}
-			if (rebuild) {
-				try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-					this.getLessCssProcessor().process(null, new InputStreamReader(this.read()),
-							new OutputStreamWriter(out));
-					byte[] byteArray = out.toByteArray();
-					this.length = Bytes.bytes(byteArray.length);
-					this.css = new String(byteArray, this.charset);
-					// System.out.println(new String( this.css));
-				} catch (IOException ex) {
-					throw new ResourceStreamNotFoundException(ex);
-				}
-				if (getCssCompressorProcessor() != null) {
-					try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-						getCssCompressorProcessor().process(null,
-								new InputStreamReader(new ByteArrayInputStream(this.css.getBytes("utf-8"))),
-								new OutputStreamWriter(out));
-						byte[] byteArray = out.toByteArray();
-						this.length = Bytes.bytes(byteArray.length);
-						this.css = new String(byteArray, this.charset);
-						// System.out.println(new String( this.css));
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-				this.lastModified = Time.now();
-			}
-			return new ByteArrayInputStream(this.css.getBytes(this.charset));
-		} catch (UnsupportedEncodingException ex) {
-			throw new ResourceStreamNotFoundException(ex);
-		}
-	}
+    @Override
+    public InputStream getInputStream() throws ResourceStreamNotFoundException {
+        try {
+            boolean rebuild = false;
+            String fullPath = this.getResourcePath() + '/' + this.getLessName();
+            if (this.css == null) {
+                logger.info("building " + fullPath + " because is new");
+                rebuild = true;
+            } else if (this.lastModified == null) {
+                logger.info("building " + fullPath + " because out of date");
+                rebuild = true;
+            } else if (WebApplication.get().usesDevelopmentConfig()) {
+                try {
+                    if (this.lastModified
+                            .getMilliseconds() < this.getClass().getClassLoader().getResource(fullPath).openConnection().getLastModified()) {
+                        logger.info("building " + fullPath + " because template out of date");
+                        rebuild = true;
+                    }
+                } catch (IOException ex1) {
+                    logger.info("building " + fullPath + " because of exception: " + ex1);
+                    rebuild = true;
+                }
+            }
+            if (rebuild) {
+                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    this.getLessCssProcessor().process(null, new InputStreamReader(this.read()), new OutputStreamWriter(out));
+                    byte[] byteArray = out.toByteArray();
+                    this.length = Bytes.bytes(byteArray.length);
+                    this.css = new String(byteArray, this.charset);
+                    // System.out.println(new String( this.css));
+                } catch (IOException ex) {
+                    throw new ResourceStreamNotFoundException(ex);
+                }
+                if (getCssCompressorProcessor() != null) {
+                    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                        getCssCompressorProcessor().process(null, new InputStreamReader(new ByteArrayInputStream(this.css.getBytes("utf-8"))),
+                                new OutputStreamWriter(out));
+                        byte[] byteArray = out.toByteArray();
+                        this.length = Bytes.bytes(byteArray.length);
+                        this.css = new String(byteArray, this.charset);
+                        // System.out.println(new String( this.css));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                this.lastModified = Time.now();
+            }
+            return new ByteArrayInputStream(this.css.getBytes(this.charset));
+        } catch (UnsupportedEncodingException ex) {
+            throw new ResourceStreamNotFoundException(ex);
+        }
+    }
 
-	@Override
-	public PackageResource getResource() {
-		return super.getResource();
-	}
+    @Override
+    public PackageResource getResource() {
+        return super.getResource();
+    }
 
-	protected String getResourcePath() {
-		return this.getScope().getPackage().getName().replace('.', '/');
-	}
+    protected String getResourcePath() {
+        return this.getScope().getPackage().getName().replace('.', '/');
+    }
 
-	@Override
-	public IResourceStream getResourceStream() {
-		return this;
-	}
+    @Override
+    public IResourceStream getResourceStream() {
+        return this;
+    }
 
-	public ResourcePreProcessor getLessCssProcessor() {
-		if (lessCssProcessor == null) {
-			NodeLessCssProcessor nodeLessCssProcessor = new NodeLessCssProcessor();
-			this.lessCssProcessor = nodeLessCssProcessor.isSupported() ? nodeLessCssProcessor
-					: new RhinoLessCssProcessor();
-		}
-		return this.lessCssProcessor;
-	}
+    public ResourcePreProcessor getLessCssProcessor() {
+        if (lessCssProcessor == null) {
+            NodeLessCssProcessor nodeLessCssProcessor = new NodeLessCssProcessor();
+            this.lessCssProcessor = nodeLessCssProcessor.isSupported() ? nodeLessCssProcessor : new RhinoLessCssProcessor();
+        }
+        return this.lessCssProcessor;
+    }
 
-	protected String getLessName() {
-		return this.getName().replace(".css", ".less");
-	}
+    protected String getLessName() {
+        return this.getName().replace(".css", ".less");
+    }
 
-	@Override
-	public Time lastModifiedTime() {
-		if (this.lastModified == null) {
-			try {
-				this.getInputStream();
-			} catch (ResourceStreamNotFoundException ex) {
-				throw new RuntimeException(ex);
-			}
-		}
-		logger.trace(this.getLessName() + " - sending lastModifiedTime: " + this.lastModified);
-		return this.lastModified;
-	}
+    @Override
+    public Time lastModifiedTime() {
+        if (this.lastModified == null) {
+            try {
+                this.getInputStream();
+            } catch (ResourceStreamNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        logger.trace(this.getLessName() + " - sending lastModifiedTime: " + this.lastModified);
+        return this.lastModified;
+    }
 
-	@Override
-	public Bytes length() {
-		return this.length;
-	}
+    @Override
+    public Bytes length() {
+        return this.length;
+    }
 
-	protected InputStream read() throws IOException {
-		return this.getClass().getClassLoader().getResourceAsStream(this.getResourcePath() + '/' + this.getLessName());
-	}
+    protected InputStream read() throws IOException {
+        return this.getClass().getClassLoader().getResourceAsStream(this.getResourcePath() + '/' + this.getLessName());
+    }
 
-	public void setCssCompressorProcessor(ResourcePreProcessor cssCompressorProcessor) {
-		this.cssCompressorProcessor = cssCompressorProcessor;
-	}
+    public void setCssCompressorProcessor(ResourcePreProcessor cssCompressorProcessor) {
+        this.cssCompressorProcessor = cssCompressorProcessor;
+    }
 
-	@Override
-	public void setLocale(Locale locale) {
-		//
-	}
+    @Override
+    public void setLocale(Locale locale) {
+        //
+    }
 
-	@Override
-	public void setStyle(String style) {
-		//
-	}
+    @Override
+    public void setStyle(String style) {
+        //
+    }
 
-	@Override
-	public void setVariation(String variation) {
-		//
-	}
+    @Override
+    public void setVariation(String variation) {
+        //
+    }
 
-	public static void main(String[] args) {
-		try {
-			// ro.isdc.wro.extensions.processor.css.LessCssProcessor
-			// ro.isdc.wro.extensions.processor.css.RhinoLessCssProcessor
-			// ro.isdc.wro.extensions.processor.css.NodeLessCssProcessor
-			NodeLessCssProcessor less = new NodeLessCssProcessor();
-			InputStream in = SassResourceReference.class.getClassLoader()
-					.getResourceAsStream("org/tools/hqlbuilder/webservice/bootstrap4/social/brand.less");
-			ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
-			IOUtils.copy(in, outtmp);
-			System.out.println(new String(outtmp.toByteArray()));
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			less.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
-					new BufferedWriter(new OutputStreamWriter(out)));
-			out.close();
-			System.out.println(new String(out.toByteArray()));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            // ro.isdc.wro.extensions.processor.css.LessCssProcessor
+            // ro.isdc.wro.extensions.processor.css.RhinoLessCssProcessor
+            // ro.isdc.wro.extensions.processor.css.NodeLessCssProcessor
+            NodeLessCssProcessor less = new NodeLessCssProcessor();
+            InputStream in = SassResourceReference.class.getClassLoader()
+                    .getResourceAsStream("org/tools/hqlbuilder/webservice/bootstrap4/social/brand.less");
+            ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
+            IOUtils.copy(in, outtmp);
+            // System.out.println(new String(outtmp.toByteArray()));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            less.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
+                    new BufferedWriter(new OutputStreamWriter(out)));
+            out.close();
+            System.out.println(new String(out.toByteArray()));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/sass/SassResourceReference.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/sass/SassResourceReference.java
@@ -27,186 +27,185 @@ import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.JawrCssMinifierProcessor;
 
 /**
- * response.render(CssHeaderItem.forReference(new
- * SassResourceReference(WicketCSSRoot.class, "table.sass")));
+ * response.render(CssHeaderItem.forReference(new SassResourceReference(WicketCSSRoot.class, "table.sass")));
  *
  * @see http://sass-lang.com/
  * @see https://code.google.com/p/wro4j/
  */
 @SuppressWarnings("serial")
 public class SassResourceReference extends StreamResourceReference implements IResourceStream {
-	protected static final Logger logger = LoggerFactory.getLogger(SassResourceReference.class);
+    protected static final Logger logger = LoggerFactory.getLogger(SassResourceReference.class);
 
-	protected final String charset = "utf-8";
+    protected final String charset = "utf-8";
 
-	protected final String contentType = "text/css";
+    protected final String contentType = "text/css";
 
-	protected transient Bytes length = null;
+    protected transient Bytes length = null;
 
-	protected transient String css = "";
+    protected transient String css = "";
 
-	protected transient Time lastModified = null;
+    protected transient Time lastModified = null;
 
-	protected transient ResourcePreProcessor sassCssProcessor;
+    protected transient ResourcePreProcessor sassCssProcessor;
 
-	protected transient ResourcePreProcessor cssCompressorProcessor;
+    protected transient ResourcePreProcessor cssCompressorProcessor;
 
-	public SassResourceReference(Class<?> scope, String name) {
-		super(scope, name);
-	}
+    public SassResourceReference(Class<?> scope, String name) {
+        super(scope, name);
+    }
 
-	@Override
-	public void close() throws IOException {
-		//
-	}
+    @Override
+    public void close() throws IOException {
+        //
+    }
 
-	@Override
-	public String getContentType() {
-		return this.contentType;
-	}
+    @Override
+    public String getContentType() {
+        return this.contentType;
+    }
 
-	public ResourcePreProcessor getCssCompressorProcessor() {
-		if (this.cssCompressorProcessor == null) {
-			this.cssCompressorProcessor = new JawrCssMinifierProcessor();
-		}
-		return this.cssCompressorProcessor;
-	}
+    public ResourcePreProcessor getCssCompressorProcessor() {
+        if (this.cssCompressorProcessor == null) {
+            this.cssCompressorProcessor = new JawrCssMinifierProcessor();
+        }
+        return this.cssCompressorProcessor;
+    }
 
-	@Override
-	public InputStream getInputStream() throws ResourceStreamNotFoundException {
-		try {
-			boolean rebuild = false;
-			String fullPath = this.getResourcePath() + '/' + this.getSassName();
-			if (this.css == null) {
-				logger.info("building " + fullPath + " because is new");
-				rebuild = true;
-			} else if (this.lastModified == null) {
-				logger.info("building " + fullPath + " because out of date");
-				rebuild = true;
-			} else if (WebApplication.get().usesDevelopmentConfig()) {
-				try {
-					if (this.lastModified.getMilliseconds() < this.getClass().getClassLoader().getResource(fullPath)
-							.openConnection().getLastModified()) {
-						logger.info("building " + fullPath + " because template out of date");
-						rebuild = true;
-					}
-				} catch (IOException ex1) {
-					logger.info("building " + fullPath + " because of exception: " + ex1);
-					rebuild = true;
-				}
-			}
-			if (rebuild) {
-				try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-					this.getSassCssProcessor().process(null, new InputStreamReader(this.read()),
-							new OutputStreamWriter(out));
-					byte[] byteArray = out.toByteArray();
-					this.length = Bytes.bytes(byteArray.length);
-					this.css = new String(byteArray, this.charset);
-					// System.out.println(new String( this.css));
-				} catch (IOException ex) {
-					throw new ResourceStreamNotFoundException(ex);
-				}
-				if (getCssCompressorProcessor() != null) {
-					try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-						getCssCompressorProcessor().process(null,
-								new InputStreamReader(new ByteArrayInputStream(this.css.getBytes("utf-8"))),
-								new OutputStreamWriter(out));
-						byte[] byteArray = out.toByteArray();
-						this.length = Bytes.bytes(byteArray.length);
-						this.css = new String(byteArray, this.charset);
-						// System.out.println(new String( this.css));
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-				this.lastModified = Time.now();
-			}
-			return new ByteArrayInputStream(this.css.getBytes(this.charset));
-		} catch (UnsupportedEncodingException ex) {
-			throw new ResourceStreamNotFoundException(ex);
-		}
-	}
+    @Override
+    public InputStream getInputStream() throws ResourceStreamNotFoundException {
+        try {
+            boolean rebuild = false;
+            String fullPath = this.getResourcePath() + '/' + this.getSassName();
+            if (this.css == null) {
+                logger.info("building " + fullPath + " because is new");
+                rebuild = true;
+            } else if (this.lastModified == null) {
+                logger.info("building " + fullPath + " because out of date");
+                rebuild = true;
+            } else if (WebApplication.get().usesDevelopmentConfig()) {
+                try {
+                    if (this.lastModified
+                            .getMilliseconds() < this.getClass().getClassLoader().getResource(fullPath).openConnection().getLastModified()) {
+                        logger.info("building " + fullPath + " because template out of date");
+                        rebuild = true;
+                    }
+                } catch (IOException ex1) {
+                    logger.info("building " + fullPath + " because of exception: " + ex1);
+                    rebuild = true;
+                }
+            }
+            if (rebuild) {
+                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    this.getSassCssProcessor().process(null, new InputStreamReader(this.read()), new OutputStreamWriter(out));
+                    byte[] byteArray = out.toByteArray();
+                    this.length = Bytes.bytes(byteArray.length);
+                    this.css = new String(byteArray, this.charset);
+                    // System.out.println(new String( this.css));
+                } catch (IOException ex) {
+                    throw new ResourceStreamNotFoundException(ex);
+                }
+                if (getCssCompressorProcessor() != null) {
+                    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                        getCssCompressorProcessor().process(null, new InputStreamReader(new ByteArrayInputStream(this.css.getBytes("utf-8"))),
+                                new OutputStreamWriter(out));
+                        byte[] byteArray = out.toByteArray();
+                        this.length = Bytes.bytes(byteArray.length);
+                        this.css = new String(byteArray, this.charset);
+                        // System.out.println(new String( this.css));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                this.lastModified = Time.now();
+            }
+            return new ByteArrayInputStream(this.css.getBytes(this.charset));
+        } catch (UnsupportedEncodingException ex) {
+            throw new ResourceStreamNotFoundException(ex);
+        }
+    }
 
-	@Override
-	public PackageResource getResource() {
-		return super.getResource();
-	}
+    @Override
+    public PackageResource getResource() {
+        return super.getResource();
+    }
 
-	protected String getResourcePath() {
-		return this.getScope().getPackage().getName().replace('.', '/');
-	}
+    protected String getResourcePath() {
+        return this.getScope().getPackage().getName().replace('.', '/');
+    }
 
-	@Override
-	public IResourceStream getResourceStream() {
-		return this;
-	}
+    @Override
+    public IResourceStream getResourceStream() {
+        return this;
+    }
 
-	public ResourcePreProcessor getSassCssProcessor() {
-		if (this.sassCssProcessor == null) {
-			this.sassCssProcessor = new RubySassCssProcessor();
-		}
-		return this.sassCssProcessor;
-	}
+    public ResourcePreProcessor getSassCssProcessor() {
+        if (this.sassCssProcessor == null) {
+            this.sassCssProcessor = new RubySassCssProcessor();
+        }
+        return this.sassCssProcessor;
+    }
 
-	protected String getSassName() {
-		return this.getName().replace(".css", ".scss");
-	}
+    protected String getSassName() {
+        return this.getName().replace(".css", ".scss");
+    }
 
-	@Override
-	public Time lastModifiedTime() {
-		if (this.lastModified == null) {
-			try {
-				this.getInputStream();
-			} catch (ResourceStreamNotFoundException ex) {
-				throw new RuntimeException(ex);
-			}
-		}
-		logger.trace(this.getSassName() + " - sending lastModifiedTime: " + this.lastModified);
-		return this.lastModified;
-	}
+    @Override
+    public Time lastModifiedTime() {
+        if (this.lastModified == null) {
+            try {
+                this.getInputStream();
+            } catch (ResourceStreamNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        logger.trace(this.getSassName() + " - sending lastModifiedTime: " + this.lastModified);
+        return this.lastModified;
+    }
 
-	@Override
-	public Bytes length() {
-		return this.length;
-	}
+    @Override
+    public Bytes length() {
+        return this.length;
+    }
 
-	protected InputStream read() throws IOException {
-		return this.getClass().getClassLoader().getResourceAsStream(this.getResourcePath() + '/' + this.getSassName());
-	}
+    protected InputStream read() throws IOException {
+        return this.getClass().getClassLoader().getResourceAsStream(this.getResourcePath() + '/' + this.getSassName());
+    }
 
-	public void setCssCompressorProcessor(ResourcePreProcessor cssCompressorProcessor) {
-		this.cssCompressorProcessor = cssCompressorProcessor;
-	}
+    public void setCssCompressorProcessor(ResourcePreProcessor cssCompressorProcessor) {
+        this.cssCompressorProcessor = cssCompressorProcessor;
+    }
 
-	@Override
-	public void setLocale(Locale locale) {
-		//
-	}
+    @Override
+    public void setLocale(Locale locale) {
+        //
+    }
 
-	@Override
-	public void setStyle(String style) {
-		//
-	}
+    @Override
+    public void setStyle(String style) {
+        //
+    }
 
-	@Override
-	public void setVariation(String variation) {
-		//
-	}
+    @Override
+    public void setVariation(String variation) {
+        //
+    }
 
-	public static void main(String[] args) {
-		try {
-			RubySassCssProcessor scss = new RubySassCssProcessor();
-			InputStream in = SassResourceReference.class.getClassLoader()
-					.getResourceAsStream("org/tools/hqlbuilder/webservice/css/form.scss");
-			ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
-			IOUtils.copy(in, outtmp);
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			scss.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
-					new BufferedWriter(new OutputStreamWriter(out)));
-			out.close();
-			System.out.println(new String(out.toByteArray()));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            // ro.isdc.wro.extensions.processor.css.SassCssProcessor
+            // ro.isdc.wro.extensions.processor.css.RubySassCssProcessor
+            RubySassCssProcessor scss = new RubySassCssProcessor();
+            InputStream in = SassResourceReference.class.getClassLoader().getResourceAsStream("org/tools/hqlbuilder/webservice/css/form.scss");
+            ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
+            IOUtils.copy(in, outtmp);
+            // System.out.println(new String(outtmp.toByteArray()));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            scss.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
+                    new BufferedWriter(new OutputStreamWriter(out)));
+            out.close();
+            System.out.println(new String(out.toByteArray()));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
