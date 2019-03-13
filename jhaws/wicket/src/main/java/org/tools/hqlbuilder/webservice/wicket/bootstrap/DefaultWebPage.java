4512cf9a90101444b11215fbfdeee943da3432ea
jurgendl <jurgen.de.landsheer@gmail.com>
2019-03-13 01:26:58 +0100

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage.java
@@ -475,8 +475,7 @@ public abstract class DefaultWebPage extends WebPage {
 		// FIXME response.render(CssHeaderItem.forReference(new
 		// LessResourceReference(DefaultWebPage.class,
 		// "org/tools/hqlbuilder/webservice/bootstrap4/social/brand.less")));
-		response.render(CssHeaderItem.forReference(new CssResourceReference(DefaultWebPage.class,
-				"org/tools/hqlbuilder/webservice/bootstrap4/social/brand.css")));
+		response.render(CssHeaderItem.forReference(new CssResourceReference(Bootstrap4.class, "social/brand.css")));
 
 		response.render(CssHeaderItem.forReference(BLazy.CSS));
 		response.render(JavaScriptHeaderItem.forReference(BLazy.JS));
