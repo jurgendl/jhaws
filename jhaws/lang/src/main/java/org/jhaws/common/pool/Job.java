8b98ab984ea7879575b091045bdbe71756f4832a
jurgendl <jurgen.de.landsheer@gmail.com>
2019-03-23 15:43:44 +0100

    *

--- jhaws/lang/src/main/java/org/jhaws/common/pool/Job.java
+++ jhaws/lang/src/main/java/org/jhaws/common/pool/Job.java
@@ -3,88 +3,90 @@ package org.jhaws.common.pool;
 import java.util.concurrent.Callable;
 
 public class Job<M> implements Callable<M> {
-    private JobState state;
+	private JobState state;
 
-    private M meta;
+	private M meta;
 
-    private Runnable runnable;
+	private Runnable runnable;
 
-    private Long queued;
+	private Long queued;
 
-    private Long started;
+	private Long started;
 
-    private Long finished;
+	private Long finished;
 
-    private Throwable error;
+	private Throwable error;
 
-    public Job(Runnable runnable, M meta) {
-        this.runnable = runnable;
-        this.meta = meta;
-    }
+	public Job(Runnable runnable, M meta) {
+		this.runnable = runnable;
+		this.meta = meta;
+	}
 
-    @Override
-    public M call() throws Exception {
-        runnable.run();
-        return meta;
-    }
+	@Override
+	public M call() throws Exception {
+		runnable.run();
+		return meta;
+	}
 
-    @Override
-    public String toString() {
-        if (finished != null) return "Job:meta[" + (finished - started) + "][" + meta + "]";
-        if (started != null) return "Job:meta[~" + (System.currentTimeMillis() - started) + "][" + meta + "]";
-        return "Job:meta[" + meta + "]";
-    }
+	@Override
+	public String toString() {
+		if (finished != null)
+			return "Job:meta[" + (finished - started) + "][" + meta + "]";
+		if (started != null)
+			return "Job:meta[~" + (System.currentTimeMillis() - started) + "][" + meta + "]";
+		return "Job:meta[" + meta + "]";
+	}
 
-    public M getMeta() {
-        return meta;
-    }
+	public M getMeta() {
+		return meta;
+	}
 
-    protected void setMeta(M meta) {
-        this.meta = meta;
-    }
+	protected void setMeta(M meta) {
+		this.meta = meta;
+	}
 
-    public Long getStarted() {
-        return this.started;
-    }
+	public Long getStarted() {
+		return this.started;
+	}
 
-    protected void setStarted(Long started) {
-        this.started = started;
-    }
+	protected void setStarted(Long started) {
+		this.started = started;
+	}
 
-    public Long getFinished() {
-        return this.finished;
-    }
+	public Long getFinished() {
+		return this.finished;
+	}
 
-    protected void setFinished(Long finished) {
-        this.finished = finished;
-    }
+	protected void setFinished(Long finished) {
+		this.finished = finished;
+	}
 
-    public Long getQueued() {
-        return this.queued;
-    }
+	public Long getQueued() {
+		return this.queued;
+	}
 
-    protected void setQueued(Long queued) {
-        this.queued = queued;
-    }
+	protected void setQueued(Long queued) {
+		this.queued = queued;
+	}
 
-    public Throwable getError() {
-        return this.error;
-    }
+	public Throwable getError() {
+		return this.error;
+	}
 
-    protected void setError(Throwable error) {
-        this.error = error;
-    }
+	protected void setError(Throwable error) {
+		this.error = error;
+	}
 
-    public JobState getState() {
-        return this.state;
-    }
+	public JobState getState() {
+		return this.state;
+	}
 
-    protected void setState(JobState newstate) {
-        if (!JobState.accepts(state, newstate)) {
-            System.out.println(this + " :: " + state + " !!!> " + newstate);
-            return;
-        }
-        System.out.println(this + " :: " + state + " > " + newstate);
-        this.state = newstate;
-    }
+	protected void setState(JobState newstate) {
+		if (!JobState.accepts(state, newstate)) {
+			// System.out.println(this + " :: " + state + " !!!> " + newstate);
+			return;
+		}
+		// System.out.println(this + " :: " + state + " > " + newstate);
+		this.state = newstate;
+	}
 }
