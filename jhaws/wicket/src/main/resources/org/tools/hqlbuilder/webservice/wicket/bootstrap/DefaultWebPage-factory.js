450bb5cfc59d5f7632a3efd40ef64f06e859d20f
jurgendl <jurgen.de.landsheer@gmail.com>
2019-04-18 13:00:00 +0200

    *

--- jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage-factory.js
+++ jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage-factory.js
@@ -1,16 +1,43 @@
 /* ============= DefaultWebPage-factory.js ============= */
 
-// https://www.bootply.com/130682
-//$(".hasclear").keyup(function() {
-//	var t = $(this);
-//	console.log(t.val()+'='+Boolean(t.val()));
-//	t.next('span').toggle(Boolean(t.val()));
-//});
+/* activate bootstrap popover icons */
 
-//$(".clearer").hide($(this).prev('input').val());
+$('[data-toggle="popover"]').popover();
 
-//$(".clearer").on('click',function() {
-//	console.log('4444');
-//	$(this).prev('input').val('').focus();
-//	$(this).hide();
-//});
+$('.popover-dismiss').popover({ trigger: 'focus' });
+
+/* activate bootstrap validation */
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+// activate with $('form').preventDoubleSubmission();
+//jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+  // Keep chainability
+  return this;
+};
