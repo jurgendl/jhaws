5cce98e211bdf4f757c5e49fca2382f374b0ec5d
jurgendl <jurgen.de.landsheer@gmail.com>
2019-04-24 17:42:18 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/toast/BootstrapToasts.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/toast/BootstrapToasts.java
@@ -1,5 +1,6 @@
 package org.tools.hqlbuilder.webservice.bootstrap4.toast;
 
+import org.apache.commons.lang3.StringUtils;
 import org.tools.hqlbuilder.webservice.bootstrap4.Bootstrap4;
 import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
 import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
@@ -11,6 +12,7 @@ import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 // type: 'info', 'success', 'warning', 'error'
 // delay: auto dismiss after this timeout
 // img: an object containging image information: {src class title alt}
+//
 public class BootstrapToasts {
     public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(BootstrapToasts.class, "toast.js");
 
@@ -19,5 +21,20 @@ public class BootstrapToasts {
     static {
         JS.addJavaScriptResourceReferenceDependency(Bootstrap4.JS);
         CSS.addCssResourceReferenceDependency(Bootstrap4.CSS);
+    }
+
+    public static enum ToastType {
+        info, success, warning, error;
+    }
+
+    public static String script(ToastType type, String title, String subtitle, String content, Long delay) {
+        if (type == null) throw new NullPointerException();
+        return ";$.toast({"//
+                + "type:'" + type + "'"//
+                + ",title:'" + title.toString() + "'"//
+                + ",content:'" + content.toString() + "'" //
+                + (StringUtils.isBlank(subtitle) ? "" : ",subtitle:'" + subtitle + "'")//
+                + (delay == null ? "" : ",delay:" + delay)//
+                + "});";
     }
 }
