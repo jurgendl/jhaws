fb6d22030613824ac829d8eb1829949ccce4cb63
jurgendl <jurgen.de.landsheer@gmail.com>
2019-04-30 18:01:55 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/sass/SassResourceReference.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/wicket/sass/SassResourceReference.java
@@ -1,19 +1,26 @@
 package org.tools.hqlbuilder.webservice.wicket.sass;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.request.resource.PackageResource;
 import org.apache.wicket.util.lang.Bytes;
 import org.apache.wicket.util.resource.IResourceStream;
 import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
@@ -34,11 +41,13 @@ import ro.isdc.wro.model.resource.processor.impl.css.JawrCssMinifierProcessor;
  */
 @SuppressWarnings("serial")
 public class SassResourceReference extends StreamResourceReference implements IResourceStream {
+    private static final String UTF_8 = "utf-8";
+
     protected static final Logger logger = LoggerFactory.getLogger(SassResourceReference.class);
 
-    protected final String charset = "utf-8";
+    protected String charset = UTF_8;
 
-    protected final String contentType = "text/css";
+    protected String contentType = "text/css";
 
     protected transient Bytes length = null;
 
@@ -46,7 +55,7 @@ public class SassResourceReference extends StreamResourceReference implements IR
 
     protected transient Time lastModified = null;
 
-    protected transient ResourcePreProcessor sassCssProcessor;
+    protected transient RubySassCssProcessor sassCssProcessor;
 
     protected transient ResourcePreProcessor cssCompressorProcessor;
 
@@ -95,39 +104,73 @@ public class SassResourceReference extends StreamResourceReference implements IR
                 }
             }
             if (rebuild) {
-                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-                    this.getSassCssProcessor().process(null, new InputStreamReader(this.read()), new OutputStreamWriter(out));
-                    byte[] byteArray = out.toByteArray();
-                    this.length = Bytes.bytes(byteArray.length);
-                    this.css = new String(byteArray, this.charset);
-                    // System.out.println(new String( this.css));
-                } catch (IOException ex) {
-                    throw new ResourceStreamNotFoundException(ex);
-                }
-                if (getCssCompressorProcessor() != null) {
-                    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-                        getCssCompressorProcessor().process(null, new InputStreamReader(new ByteArrayInputStream(this.css.getBytes("utf-8"))),
-                                new OutputStreamWriter(out));
-                        byte[] byteArray = out.toByteArray();
-                        this.length = Bytes.bytes(byteArray.length);
-                        this.css = new String(byteArray, this.charset);
-                        // System.out.println(new String( this.css));
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                this.lastModified = Time.now();
+                rebuild();
             }
             return new ByteArrayInputStream(this.css.getBytes(this.charset));
-        } catch (UnsupportedEncodingException ex) {
+        } catch (URISyntaxException | IOException ex) {
             throw new ResourceStreamNotFoundException(ex);
         }
     }
 
-    @Override
-    public PackageResource getResource() {
-        return super.getResource();
+    protected synchronized void rebuild() throws IOException, URISyntaxException {
+        URL resource = this.getClass().getClassLoader().getResource(this.getResourcePath() + '/' + this.getSassName());
+        String scss = new String(IOUtils.toByteArray(resource), UTF_8);
+
+        String name = getName();
+        if (name.contains("/")) name = name.substring(name.lastIndexOf("/") + 1);
+        if (name.contains("\\")) name = name.substring(name.lastIndexOf("\\") + 1);
+
+        // combineert imports 1 niveau diep
+        scss = replace(Paths.get(resource.toURI()).getParent(), scss);
+
+        // development
+        if (WebApplication.get().usesDevelopmentConfig()) {
+            File tmp = new File(System.getProperty("java.io.tmpdir"), name + ".combined." + System.currentTimeMillis() + ".css");
+            IOUtils.write(scss.getBytes(UTF_8), new FileOutputStream(tmp));
+            logger.info(tmp.getAbsolutePath());
+        }
+
+        // scss > css
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            this.getSassCssProcessor().process(null, new StringReader(scss), new OutputStreamWriter(out));
+            byte[] byteArray = out.toByteArray();
+            this.length = Bytes.bytes(byteArray.length);
+            this.css = new String(byteArray, this.charset);
+
+            // development
+            if (WebApplication.get().usesDevelopmentConfig()) {
+                File tmp = new File(System.getProperty("java.io.tmpdir"), name + ".uncompressed." + System.currentTimeMillis() + ".css");
+                IOUtils.write(byteArray, new FileOutputStream(tmp));
+                logger.info(tmp.getAbsolutePath());
+            }
+        }
+
+        // optionele post processor (bv die css verkleint)
+        if (getCssCompressorProcessor() != null) {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                getCssCompressorProcessor().process(null, new InputStreamReader(new ByteArrayInputStream(this.css.getBytes(charset))),
+                        new OutputStreamWriter(out));
+                byte[] byteArray = out.toByteArray();
+                this.length = Bytes.bytes(byteArray.length);
+                this.css = new String(byteArray, this.charset).trim();
+
+                // development
+                if (WebApplication.get().usesDevelopmentConfig()) {
+                    File tmp = new File(System.getProperty("java.io.tmpdir"), name + ".compressed." + System.currentTimeMillis() + ".css");
+                    IOUtils.write(byteArray, new FileOutputStream(tmp));
+                    logger.info(tmp.getAbsolutePath());
+                }
+            }
+        }
+
+        // zet tijdstip
+        this.lastModified = Time.now();
     }
+
+    // @Override
+    // public PackageResource getResource() {
+    // return super.getResource();
+    // }
 
     protected String getResourcePath() {
         return this.getScope().getPackage().getName().replace('.', '/');
@@ -138,7 +181,7 @@ public class SassResourceReference extends StreamResourceReference implements IR
         return this;
     }
 
-    public ResourcePreProcessor getSassCssProcessor() {
+    public RubySassCssProcessor getSassCssProcessor() {
         if (this.sassCssProcessor == null) {
             this.sassCssProcessor = new RubySassCssProcessor();
         }
@@ -190,22 +233,54 @@ public class SassResourceReference extends StreamResourceReference implements IR
         //
     }
 
-    public static void main(String[] args) {
-        try {
-            // ro.isdc.wro.extensions.processor.css.SassCssProcessor
-            // ro.isdc.wro.extensions.processor.css.RubySassCssProcessor
-            RubySassCssProcessor scss = new RubySassCssProcessor();
-            InputStream in = SassResourceReference.class.getClassLoader().getResourceAsStream("org/tools/hqlbuilder/webservice/css/form.scss");
-            ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
-            IOUtils.copy(in, outtmp);
-            // System.out.println(new String(outtmp.toByteArray()));
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            scss.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
-                    new BufferedWriter(new OutputStreamWriter(out)));
-            out.close();
-            System.out.println(new String(out.toByteArray()));
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    // protected void write(OutputStream out) throws IOException {
+    // this.getSassCssProcessor().process(new InputStreamReader(this.read()), new OutputStreamWriter(out));
+    // out.flush();
+    // }
+
+    // public static void main(String[] args) {
+    // try {
+    // RubySassCssProcessor scss = new RubySassCssProcessor();
+    // InputStream in = IOUtils.openFileOrResource("file.scss");
+    // ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
+    // IOUtils.copy(in, outtmp);
+    // // logger.info(new String(outtmp.toByteArray()));
+    // ByteArrayOutputStream out = new ByteArrayOutputStream();
+    // scss.process(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(outtmp.toByteArray()))),
+    // new BufferedWriter(new OutputStreamWriter(out)));
+    // out.close();
+    // // logger.info("=============");
+    // logger.info(new String(out.toByteArray()));
+    // } catch (Exception ex) {
+    // ex.printStackTrace();
+    // }
+    // }
+
+    protected static final Pattern PATTERN_IMPORT = Pattern.compile("^@import \"([^\"]++)\";", Pattern.MULTILINE);
+
+    protected static String replace(Path base, String css) throws IOException {
+        Matcher m = PATTERN_IMPORT.matcher(css);
+        if (m.find()) {
+            String relative = m.group(1);
+            Path imported = base.resolve(relative + ".scss");
+            if (imported.toFile().exists()) {
+                //
+            } else {
+                imported = base.resolve(relative + ".css");
+                if (imported.toFile().exists()) {
+                    //
+                } else {
+                    throw new FileNotFoundException(String.valueOf(imported));
+                }
+            }
+            String importing = new String(Files.readAllBytes(imported), UTF_8);
+            Matcher mi = PATTERN_IMPORT.matcher(importing);
+            if (mi.find()) {
+                throw new IOException("no 2 level imports: " + imported + " >> " + mi.group(1));
+            }
+            css = css.substring(0, m.start()) + importing + css.substring(m.end());
+            replace(base, css);
         }
+        return css;
     }
 }
