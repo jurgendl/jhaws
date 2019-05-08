36b63e311adb42627b6fd04ee430e182d5a0dcd6
jurgendl <jurgen.de.landsheer@gmail.com>
2019-05-09 01:36:50 +0200

    *

--- jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage.js
+++ jhaws/wicket/src/main/resources/org/tools/hqlbuilder/webservice/wicket/bootstrap/DefaultWebPage.js
@@ -342,4 +342,35 @@ var LinkGrabber = {
 	}
 };
 
+
+// https://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+/*
+.string { color: green; }
+.number { color: darkorange; }
+.boolean { color: blue; }
+.null { color: magenta; }
+.key { color: red; }
+*/
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 4);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 /* ======================================================= */
