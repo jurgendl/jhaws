6ed04d937c3234a870e094e7ba10de9c2d31cf39
jurgendl <jurgen.de.landsheer@gmail.com>
2019-05-09 18:03:57 +0200

    *

---
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/highlightjs/HighlightJs.java
@@ -0,0 +1,16 @@
+package org.tools.hqlbuilder.webservice.highlightjs;
+
+import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
+import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
+
+// https://highlightjs.org/download/
+// https://highlightjs.readthedocs.io/en/latest/css-classes-reference.html
+// https://highlightjs.org/static/demo/
+// npm install highlight.js
+//
+// alternative https://prismjs.com/
+public class HighlightJs {
+    public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(HighlightJs.class, "highlight.pack.js");
+
+    public static final CssResourceReference CSS = new CssResourceReference(HighlightJs.class, "styles/default.css");
+}
