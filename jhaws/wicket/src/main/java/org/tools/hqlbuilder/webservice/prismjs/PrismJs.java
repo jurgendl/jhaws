d03a707e86b8aa07f3caa91f2254b4a7cc9424b3
jurgendl <jurgen.de.landsheer@gmail.com>
2019-05-10 16:01:53 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/prismjs/PrismJs.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/prismjs/PrismJs.java
@@ -3,9 +3,14 @@ package org.tools.hqlbuilder.webservice.prismjs;
 import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
 import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 
+// v1.16.0
 // <pre><code class="language-xxx">...</code></pre>
 public class PrismJs {
-    public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(PrismJs.class, "prism.js");
+    public static final JavaScriptResourceReference JS_ALL_LANGUAGES_ALL_PLUGINS = new JavaScriptResourceReference(PrismJs.class,
+            "dist/prism-all.js");
 
-    public static final CssResourceReference CSS = new CssResourceReference(PrismJs.class, "prism.css");
+    public static final JavaScriptResourceReference JS_ALL_LANGUAGES_NO_PLUGINS = new JavaScriptResourceReference(PrismJs.class,
+            "dist/prism-limited.js");
+
+    public static final CssResourceReference CSS = new CssResourceReference(PrismJs.class, "dist/prism.css");
 }
