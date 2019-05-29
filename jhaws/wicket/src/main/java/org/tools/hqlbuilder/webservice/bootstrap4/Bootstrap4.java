98be6a388318600cd114aef58251b3752c321d33
jurgendl <jurgen.de.landsheer@gmail.com>
2019-05-29 16:51:40 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/Bootstrap4.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/Bootstrap4.java
@@ -31,33 +31,37 @@ import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 //
 // https://codexui.com/components/
 public class Bootstrap4 {
-	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/bootstrap.js");
+    public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(Bootstrap4.class, "js/bootstrap.js");
 
-	// IE10 viewport hack for Surface/desktop Windows 8 bug
-	public static final JavaScriptResourceReference JS_IE10FIX = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/ie10-viewport-bug-workaround.js");
+    // IE10 viewport hack for Surface/desktop Windows 8 bug
+    public static final JavaScriptResourceReference JS_IE10FIX = new JavaScriptResourceReference(Bootstrap4.class,
+            "js/ie10-viewport-bug-workaround.js");
 
-	public static final JavaScriptResourceReference JS_POPPER = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/popper.min.js");
+    public static final JavaScriptResourceReference JS_POPPER = new JavaScriptResourceReference(Bootstrap4.class, "js/popper.min.js");
 
-	public static CssResourceReference CSS = new CssResourceReference(Bootstrap4.class, "css/bootstrap.css");
+    public static CssResourceReference CSS = new CssResourceReference(Bootstrap4.class, "css/bootstrap.css");
 
-	// public static final CssResourceReference CSS_GRID = new
-	// CssResourceReference(Bootstrap4.class,
-	// "css/bootstrap-grid.css");
-	//
-	// public static final CssResourceReference CSS_REBOOT = new
-	// CssResourceReference(Bootstrap4.class,
-	// "css/bootstrap-reboot.css");
+    // public static final CssResourceReference CSS_GRID = new
+    // CssResourceReference(Bootstrap4.class,
+    // "css/bootstrap-grid.css");
+    //
+    // public static final CssResourceReference CSS_REBOOT = new
+    // CssResourceReference(Bootstrap4.class,
+    // "css/bootstrap-reboot.css");
 
-	public static String FACTORY = new FilePath(Bootstrap4.class, "js/bootstrap.factory.js").readAll();
+    public static String FACTORY = new FilePath(Bootstrap4.class, "js/bootstrap.factory.js").readAll();
 
-	static {
-		JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
-		JS_IE10FIX.addJavaScriptResourceReferenceDependency(JS);
-		JS.addJavaScriptResourceReferenceDependency(JS_POPPER);
-		// CSS_GRID.addCssResourceReferenceDependency(CSS);
-		// CSS_REBOOT.addCssResourceReferenceDependency(CSS);
-	}
+    static {
+        JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
+        JS_IE10FIX.addJavaScriptResourceReferenceDependency(JS);
+        JS.addJavaScriptResourceReferenceDependency(JS_POPPER);
+        // CSS_GRID.addCssResourceReferenceDependency(CSS);
+        // CSS_REBOOT.addCssResourceReferenceDependency(CSS);
+    }
+
+    public static final CssResourceReference MENLO = new CssResourceReference(Bootstrap4.class, "theme/menlo.css");
+
+    public static CssResourceReference theme(String name) {
+        return new CssResourceReference(Bootstrap4.class, "theme/" + name + "/css/" + name + ".css");
+    }
 }
