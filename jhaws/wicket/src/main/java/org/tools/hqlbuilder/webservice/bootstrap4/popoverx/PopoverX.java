607fda3b76b6bb6162683b74bde0e1fadd7327ed
jurgendl <jurgen.de.landsheer@gmail.com>
2019-06-03 16:51:07 +0200

    popoverx

---
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/popoverx/PopoverX.java
@@ -0,0 +1,11 @@
+package org.tools.hqlbuilder.webservice.bootstrap4.popoverx;
+
+import org.tools.hqlbuilder.webservice.wicket.CssResourceReference;
+import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
+
+public class PopoverX {
+	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(PopoverX.class, "js/bootstrap-popover-x.js");
+
+	public static CssResourceReference CSS = new CssResourceReference(PopoverX.class, "css/bootstrap-popover-x.css");
+
+}
