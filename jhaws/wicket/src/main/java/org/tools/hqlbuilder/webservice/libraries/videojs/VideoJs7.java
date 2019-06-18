1e38829e84ed2b6964eaef8b2a9260f34810e63d
jurgendl <jurgen.de.landsheer@gmail.com>
2019-06-18 20:27:41 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/libraries/videojs/VideoJs7.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/libraries/videojs/VideoJs7.java
@@ -7,19 +7,22 @@ import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 // http://videojs.com/getting-started/
 // http://videojs.com/plugins/
 // http://docs.videojs.com/tutorial-options.html#loop
-// 7.0.2
+// https://videojs.com/plugins/
+// 7.5.5
 public class VideoJs7 {
-    public static JavaScriptResourceReference JS = new JavaScriptResourceReference(VideoJs7.class, "video-js-7.0.2/video.js");
+	public static JavaScriptResourceReference JS = new JavaScriptResourceReference(VideoJs7.class,
+			"video-js-7.5.5/video.js");
 
-    static {
-        JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
-    }
+	static {
+		JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
+	}
 
-    public static JavaScriptResourceReference JS_PREVENT_MULTIPLE = new JavaScriptResourceReference(VideoJs7.class, "videojs-prevent-multiple.js");
+	public static JavaScriptResourceReference JS_PREVENT_MULTIPLE = new JavaScriptResourceReference(VideoJs7.class,
+			"videojs-prevent-multiple.js");
 
-    static {
-        JS_PREVENT_MULTIPLE.addJavaScriptResourceReferenceDependency(JS);
-    }
+	static {
+		JS_PREVENT_MULTIPLE.addJavaScriptResourceReferenceDependency(JS);
+	}
 
-    public static CssResourceReference CSS = new CssResourceReference(VideoJs7.class, "video-js-7.0.2/video-js.css");
+	public static CssResourceReference CSS = new CssResourceReference(VideoJs7.class, "video-js-7.5.5/video-js.css");
 }
