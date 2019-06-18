6eb18c043dd9494a8c62e75c7d19a9d0c8126c26
jurgendl <jurgen.de.landsheer@gmail.com>
2019-06-18 11:00:55 +0200

    *

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/Bootstrap4.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/bootstrap4/Bootstrap4.java
@@ -30,37 +30,22 @@ import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 //
 // https://codexui.com/components/
 public class Bootstrap4 {
-	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/bootstrap.js");
+    public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(Bootstrap4.class, "js/bootstrap.bundle.js");
 
-	// IE10 viewport hack for Surface/desktop Windows 8 bug
-	public static final JavaScriptResourceReference JS_IE10FIX = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/ie10-viewport-bug-workaround.js");
+    // IE10 viewport hack for Surface/desktop Windows 8 bug
+    public static final JavaScriptResourceReference JS_IE10FIX = new JavaScriptResourceReference(Bootstrap4.class,
+            "js/ie10-viewport-bug-workaround.js");
 
-	public static final JavaScriptResourceReference JS_POPPER = new JavaScriptResourceReference(Bootstrap4.class,
-			"js/popper.min.js");
+    public static CssResourceReference CSS = new CssResourceReference(Bootstrap4.class, "css/bootstrap.css");
 
-	public static CssResourceReference CSS = new CssResourceReference(Bootstrap4.class, "css/bootstrap.css");
+    static {
+        JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
+        JS_IE10FIX.addJavaScriptResourceReferenceDependency(JS);
+    }
 
-	// public static final CssResourceReference CSS_GRID = new
-	// CssResourceReference(Bootstrap4.class,
-	// "css/bootstrap-grid.css");
-	//
-	// public static final CssResourceReference CSS_REBOOT = new
-	// CssResourceReference(Bootstrap4.class,
-	// "css/bootstrap-reboot.css");
+    public static final CssResourceReference MENLO = new CssResourceReference(Bootstrap4.class, "theme/menlo.css");
 
-	static {
-		JS.addJavaScriptResourceReferenceDependency(JQuery.getJQueryReference());
-		JS_IE10FIX.addJavaScriptResourceReferenceDependency(JS);
-		JS.addJavaScriptResourceReferenceDependency(JS_POPPER);
-		// CSS_GRID.addCssResourceReferenceDependency(CSS);
-		// CSS_REBOOT.addCssResourceReferenceDependency(CSS);
-	}
-
-	public static final CssResourceReference MENLO = new CssResourceReference(Bootstrap4.class, "theme/menlo.css");
-
-	public static CssResourceReference theme(String name) {
-		return new CssResourceReference(Bootstrap4.class, "theme/" + name + "/bootstrap.css");
-	}
+    public static CssResourceReference theme(String name) {
+        return new CssResourceReference(Bootstrap4.class, "theme/" + name + "/bootstrap.css");
+    }
 }
