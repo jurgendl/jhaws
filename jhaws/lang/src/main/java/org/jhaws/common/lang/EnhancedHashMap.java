9e8c4a4e08a4c7d416c82a8ee7bf174f660a22a1
jurgendl <jurgen.de.landsheer@gmail.com>
2019-07-13 14:11:06 +0200

    *

--- jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedHashMap.java
+++ jhaws/lang/src/main/java/org/jhaws/common/lang/EnhancedHashMap.java
@@ -1,7 +1,24 @@
 package org.jhaws.common.lang;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class EnhancedHashMap<K, V> extends HashMap<K, V> implements EnhancedMap<K, V> {
-    private static final long serialVersionUID = 6198137611588663728L;
+	private static final long serialVersionUID = 2612641800194703157L;
+
+	public EnhancedHashMap() {
+		super();
+	}
+
+	public EnhancedHashMap(int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	public EnhancedHashMap(int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	public EnhancedHashMap(Map<? extends K, ? extends V> m) {
+		super(m);
+	}
 }
