1435d5f5e3b95395ebbd0fbbbd2eb1022e5e6ccd
jurgendl <jurgen.de.landsheer@gmail.com>
2019-10-03 00:19:04 +0200

    *

--- jhaws/documents/src/test/java/org/jhaws/common/lucene/DocExtractTest.java
+++ jhaws/documents/src/test/java/org/jhaws/common/lucene/DocExtractTest.java
@@ -1,19 +1,76 @@
 package org.jhaws.common.lucene;
 
-import org.jhaws.common.documents.XmlTextExtracter;
-import org.jhaws.common.io.FilePath;
+import java.io.InputStream;
+
+import org.apache.tika.Tika;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DocExtractTest {
-    @Test
-    public void testXml() {
-        try {
-            XmlTextExtracter.SPLIT = " ";
-            XmlTextExtracter x = new XmlTextExtracter();
-            Assert.assertEquals("UTF-8 ${pattern} false ", x.extract(FilePath.of(XmlTextExtracter.class, "logback.xml")));
-        } catch (Exception ex) {
-            Assert.fail("" + ex);
-        }
-    }
+	@Test
+	public void testXml() {
+		test("../logback.xml");
+	}
+
+	@Test
+	public void testOdp() {
+		test("file_example_ODP_1MB.odp");
+	}
+
+	@Test
+	public void testOds() {
+		test("file_example_ODS_5000.ods");
+	}
+
+	@Test
+	public void testPpt() {
+		test("file_example_PPT_1MB.ppt");
+	}
+
+	@Test
+	public void testXls() {
+		test("file_example_XLS_5000.xls");
+	}
+
+	@Test
+	public void testXlsx() {
+		test("file_example_XLSX_5000.xlsx");
+	}
+
+	@Test
+	public void testPdf() {
+		test("file-example_PDF_1MB.pdf");
+	}
+
+	@Test
+	public void testDoc() {
+		test("file-sample_1MB.doc");
+	}
+
+	@Test
+	public void testDocx() {
+		test("file-sample_1MB.docx");
+	}
+
+	@Test
+	public void testOdf() {
+		test("file-sample_1MB.odt");
+	}
+
+	@Test
+	public void testRtf() {
+		test("file-sample_1MB.rtf");
+	}
+
+	private void test(String docName) {
+		try {
+			System.out.println(docName);
+			InputStream doc = getClass().getClassLoader().getResourceAsStream("docs/" + docName);
+			Tika tika = new Tika();
+			System.out.println(tika.parseToString(doc).length());
+		} catch (Exception ex) {
+			ex.printStackTrace(System.out);
+			Assert.fail("" + ex);
+		}
+	}
 }
