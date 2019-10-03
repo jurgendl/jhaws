e7c79534b3dbd2b28c98f5247574a1c04a1be8f3
jurgendl <jurgen.de.landsheer@gmail.com>
2019-10-03 13:02:44 +0200

    *

--- jhaws/documents/src/test/java/org/jhaws/common/lucene/DocExtractTest.java
+++ jhaws/documents/src/test/java/org/jhaws/common/lucene/DocExtractTest.java
@@ -7,70 +7,80 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DocExtractTest {
-	@Test
-	public void testXml() {
-		test("../logback.xml");
-	}
+    @Test
+    public void testXml() {
+        test("../logback.xml");
+    }
 
-	@Test
-	public void testOdp() {
-		test("file_example_ODP_1MB.odp");
-	}
+    @Test
+    public void testHtml() {
+        test("html.html");
+    }
 
-	@Test
-	public void testOds() {
-		test("file_example_ODS_5000.ods");
-	}
+    @Test
+    public void testOdp() {
+        test("file_example_ODP_1MB.odp");
+    }
 
-	@Test
-	public void testPpt() {
-		test("file_example_PPT_1MB.ppt");
-	}
+    @Test
+    public void testOds() {
+        test("file_example_ODS_5000.ods");
+    }
 
-	@Test
-	public void testXls() {
-		test("file_example_XLS_5000.xls");
-	}
+    @Test
+    public void testPpt() {
+        test("file_example_PPT_1MB.ppt");
+    }
 
-	@Test
-	public void testXlsx() {
-		test("file_example_XLSX_5000.xlsx");
-	}
+    @Test
+    public void testPptx() {
+        test("file_example_PPT_1MB.pptx");
+    }
 
-	@Test
-	public void testPdf() {
-		test("file-example_PDF_1MB.pdf");
-	}
+    @Test
+    public void testXls() {
+        test("file_example_XLS_5000.xls");
+    }
 
-	@Test
-	public void testDoc() {
-		test("file-sample_1MB.doc");
-	}
+    @Test
+    public void testXlsx() {
+        test("file_example_XLSX_5000.xlsx");
+    }
 
-	@Test
-	public void testDocx() {
-		test("file-sample_1MB.docx");
-	}
+    @Test
+    public void testPdf() {
+        test("file-example_PDF_1MB.pdf");
+    }
 
-	@Test
-	public void testOdf() {
-		test("file-sample_1MB.odt");
-	}
+    @Test
+    public void testDoc() {
+        test("file-sample_1MB.doc");
+    }
 
-	@Test
-	public void testRtf() {
-		test("file-sample_1MB.rtf");
-	}
+    @Test
+    public void testDocx() {
+        test("file-sample_1MB.docx");
+    }
 
-	private void test(String docName) {
-		try {
-			System.out.println(docName);
-			InputStream doc = getClass().getClassLoader().getResourceAsStream("docs/" + docName);
-			Tika tika = new Tika();
-			System.out.println(tika.parseToString(doc).length());
-		} catch (Exception ex) {
-			ex.printStackTrace(System.out);
-			Assert.fail("" + ex);
-		}
-	}
+    @Test
+    public void testOdf() {
+        test("file-sample_1MB.odt");
+    }
+
+    @Test
+    public void testRtf() {
+        test("file-sample_1MB.rtf");
+    }
+
+    private void test(String docName) {
+        try {
+            System.out.println(docName);
+            InputStream doc = getClass().getClassLoader().getResourceAsStream("docs/" + docName);
+            Tika tika = new Tika();
+            System.out.println(tika.parseToString(doc).length());
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            Assert.fail("" + ex);
+        }
+    }
 }
