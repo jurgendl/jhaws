72360b5ad1daefa85ac1d98323d4bcbd087c21c9
jurgendl <jurgen.de.landsheer@gmail.com>
2019-11-01 00:27:43 +0100

    *

--- jhaws/net/src/test/java/org/jhaws/common/net/client5/Http2Test.java
+++ jhaws/net/src/test/java/org/jhaws/common/net/client5/Http2Test.java
@@ -1,6 +1,5 @@
 package org.jhaws.common.net.client5;
 
-import java.text.ParseException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -12,12 +11,8 @@ import java.util.stream.Stream;
 
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLException;
-import javax.swing.text.NumberFormatter;
 
-import org.apache.hc.core5.concurrent.FutureCallback;
 import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.Message;
 import org.apache.hc.core5.http.Methods;
 import org.apache.hc.core5.http.impl.bootstrap.HttpAsyncRequester;
 import org.apache.hc.core5.http.nio.AsyncClientEndpoint;
@@ -40,192 +35,143 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Http2Test {
-    static long total;
+	public static void main(String[] args) throws Exception {
+		siteDownload("www.youtube.com", HttpVersionPolicy.FORCE_HTTP_2);
+		siteDownload("www.youtube.com", HttpVersionPolicy.FORCE_HTTP_1);
+	}
 
-    private static class HttpResponseCallback<T> implements FutureCallback<Message<HttpResponse, T>> {
-        public HttpResponseCallback(CountDownLatch latch, AsyncClientEndpoint clientEndpoint, String requestUri) {
-            this.requestUri = requestUri;
-            this.latch = latch;
-            this.clientEndpoint = clientEndpoint;
-        }
+	protected static void siteDownload(String site, HttpVersionPolicy httpVersionPolicy)
+			throws InterruptedException, ExecutionException {
+		IOReactorConfig ioReactorConfig = IOReactorConfig.custom()//
+				.setSoTimeout(5, TimeUnit.SECONDS)//
+				// .setTcpNoDelay(true)//
+				.build();
 
-        T body;
+		H2Config h2Config = H2Config.custom()//
+				.setPushEnabled(false)//
+				.setMaxConcurrentStreams(50)//
+				.build();
 
-        final CountDownLatch latch;
+		HttpAsyncRequester requester = H2RequesterBootstrap//
+				.bootstrap()//
+				.setIOReactorConfig(ioReactorConfig)//
+				.setVersionPolicy(httpVersionPolicy)//
+				.setH2Config(h2Config)//
+				.setTlsStrategy(new H2ClientTlsStrategy(SSLContexts.createSystemDefault(), new SSLSessionVerifier() {
+					@Override
+					public TlsDetails verify(NamedEndpoint endpoint, SSLEngine sslEngine) throws SSLException {
+						// IMPORTANT uncomment the following line when running
+						// Java 9 or older
+						// in order to avoid the illegal reflective access
+						// operation warning
+						// ====
+						// return new TlsDetails(sslEngine.getSession(),
+						// sslEngine.getApplicationProtocol());
+						// ====
+						return null;
+					}
+				}))
+				// .setStreamListener(new H2StreamListener() {
+				// @Override
+				// public void onHeaderInput(HttpConnection connection, int
+				// streamId, List<? extends Header> headers) {
+				// for (int i = 0; i < headers.size(); i++) {
+				// System.out.println(
+				// connection.getRemoteAddress() + " (" + streamId + ") << " +
+				// headers.get(i));
+				// }
+				// }
+				//
+				// @Override
+				// public void onHeaderOutput(HttpConnection connection, int
+				// streamId,
+				// List<? extends Header> headers) {
+				// for (int i = 0; i < headers.size(); i++) {
+				// System.out.println(
+				// connection.getRemoteAddress() + " (" + streamId + ") >> " +
+				// headers.get(i));
+				// }
+				// }
+				//
+				// @Override
+				// public void onFrameInput(HttpConnection connection, int
+				// streamId, RawFrame frame) {
+				// }
+				//
+				// @Override
+				// public void onFrameOutput(HttpConnection connection, int
+				// streamId, RawFrame frame) {
+				// }
+				//
+				// @Override
+				// public void onInputFlowControl(HttpConnection connection, int
+				// streamId, int delta, int actualSize) {
+				// }
+				//
+				// @Override
+				// public void onOutputFlowControl(HttpConnection connection,
+				// int streamId, int delta,
+				// int actualSize) {
+				// }
+				// })
+				.create();
 
-        final AsyncClientEndpoint clientEndpoint;
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				System.out.println("HTTP requester shutting down");
+				requester.close(CloseMode.GRACEFUL);
+			}
+		});
+		requester.start();
 
-        final String requestUri;
+		HttpHost target = new HttpHost("https", site, 443);
 
-        @Override
-        public void completed(Message<HttpResponse, T> message) {
-            clientEndpoint.releaseAndReuse();
-            HttpResponse response = message.getHead();
-            body = message.getBody();
-            int len = 0;
-            try {
-                len = Integer.parseInt(response.getHeader("content-length").getValue());
-            } catch (Exception ex) {
-                //
-            }
-            total += len;
-            System.out.println(Thread.currentThread().getName() + " :: " + requestUri + " -> " + response.getCode() + " :: " + response.getVersion()
-                    + " :: " + len);
-            latch.countDown();
-        }
+		String body;
+		{
+			CountDownLatch latch = new CountDownLatch(1);
+			Future<AsyncClientEndpoint> future = requester.connect(target, Timeout.ofSeconds(10));
+			AsyncClientEndpoint clientEndpoint = future.get();
+			String requestUri = "/";
+			HttpResponseCallback<String> callback = new HttpResponseCallback<>(latch, clientEndpoint, requestUri);
+			clientEndpoint.execute(//
+					new BasicRequestProducer(Methods.GET, target, requestUri)//
+					, new BasicResponseConsumer<>(new StringAsyncEntityConsumer())//
+					, callback//
+			);
+			latch.await();
+			body = callback.body;
+		}
 
-        @Override
-        public void failed(Exception ex) {
-            clientEndpoint.releaseAndReuse();
-            latch.countDown();
-            System.out.println(requestUri + "->" + ex);
-        }
+		Set<String> requestUris = new LinkedHashSet<>();
 
-        @Override
-        public void cancelled() {
-            clientEndpoint.releaseAndReuse();
-            latch.countDown();
-            System.out.println(requestUri + " cancelled");
-        }
-    }
+		Consumer<Stream<String>> ssv = ss -> ss//
+				.filter(href -> href != null)//
+				.filter(href -> !href.startsWith("//"))//
+				.filter(href -> href.startsWith("/") || href.startsWith("https://" + site))//
+				.map(href -> href.replaceFirst("https://" + site, ""))//
+				.filter(href -> !href.equals("/"))//
+				.forEach(href -> requestUris.add(href));
+		Document parsed = Jsoup.parse(body);
+		ssv.accept(parsed.select("link").stream().map(el -> el.attr("href")));
+		ssv.accept(parsed.select("img").stream().map(el -> el.attr("src")));
+		requestUris.forEach(System.out::println);
 
-    public static void main(String[] args) throws Exception {
-        siteDownload("www.youtube.com", HttpVersionPolicy.FORCE_HTTP_2);
-        siteDownload("www.youtube.com", HttpVersionPolicy.FORCE_HTTP_1);
-    }
-
-    protected static void siteDownload(String site, HttpVersionPolicy httpVersionPolicy) throws InterruptedException, ExecutionException {
-        total = 0;
-
-        long start = System.currentTimeMillis();
-
-        IOReactorConfig ioReactorConfig = IOReactorConfig.custom()//
-                .setSoTimeout(5, TimeUnit.SECONDS)//
-                // .setTcpNoDelay(true)//
-                .build();
-
-        H2Config h2Config = H2Config.custom()//
-                .setPushEnabled(false)//
-                .setMaxConcurrentStreams(50)//
-                .build();
-
-        HttpAsyncRequester requester = H2RequesterBootstrap//
-                .bootstrap()//
-                .setIOReactorConfig(ioReactorConfig)//
-                .setVersionPolicy(httpVersionPolicy)//
-                .setH2Config(h2Config)//
-                .setTlsStrategy(new H2ClientTlsStrategy(SSLContexts.createSystemDefault(), new SSLSessionVerifier() {
-                    @Override
-                    public TlsDetails verify(NamedEndpoint endpoint, SSLEngine sslEngine) throws SSLException {
-                        // IMPORTANT uncomment the following line when running
-                        // Java 9 or older
-                        // in order to avoid the illegal reflective access
-                        // operation warning
-                        // ====
-                        // return new TlsDetails(sslEngine.getSession(),
-                        // sslEngine.getApplicationProtocol());
-                        // ====
-                        return null;
-                    }
-                }))
-                // .setStreamListener(new H2StreamListener() {
-                // @Override
-                // public void onHeaderInput(HttpConnection connection, int streamId, List<? extends Header> headers) {
-                // for (int i = 0; i < headers.size(); i++) {
-                // System.out.println(
-                // connection.getRemoteAddress() + " (" + streamId + ") << " + headers.get(i));
-                // }
-                // }
-                //
-                // @Override
-                // public void onHeaderOutput(HttpConnection connection, int streamId,
-                // List<? extends Header> headers) {
-                // for (int i = 0; i < headers.size(); i++) {
-                // System.out.println(
-                // connection.getRemoteAddress() + " (" + streamId + ") >> " + headers.get(i));
-                // }
-                // }
-                //
-                // @Override
-                // public void onFrameInput(HttpConnection connection, int streamId, RawFrame frame) {
-                // }
-                //
-                // @Override
-                // public void onFrameOutput(HttpConnection connection, int streamId, RawFrame frame) {
-                // }
-                //
-                // @Override
-                // public void onInputFlowControl(HttpConnection connection, int streamId, int delta, int actualSize) {
-                // }
-                //
-                // @Override
-                // public void onOutputFlowControl(HttpConnection connection, int streamId, int delta,
-                // int actualSize) {
-                // }
-                // })
-                .create();
-
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.println("HTTP requester shutting down");
-                requester.close(CloseMode.GRACEFUL);
-            }
-        });
-        requester.start();
-
-        HttpHost target = new HttpHost("https", site, 443);
-
-        String body;
-        {
-            CountDownLatch latch = new CountDownLatch(1);
-            Future<AsyncClientEndpoint> future = requester.connect(target, Timeout.ofSeconds(10));
-            AsyncClientEndpoint clientEndpoint = future.get();
-            String requestUri = "/";
-            HttpResponseCallback<String> callback = new HttpResponseCallback<>(latch, clientEndpoint, requestUri);
-            clientEndpoint.execute(//
-                    new BasicRequestProducer(Methods.GET, target, requestUri)//
-                    , new BasicResponseConsumer<>(new StringAsyncEntityConsumer())//
-                    , callback//
-            );
-            latch.await();
-            body = callback.body;
-        }
-
-        Set<String> requestUris = new LinkedHashSet<>();
-
-        Consumer<Stream<String>> ssv = ss -> ss//
-                .filter(href -> href != null)//
-                .filter(href -> !href.startsWith("//"))//
-                .filter(href -> href.startsWith("/") || href.startsWith("https://" + site))//
-                .map(href -> href.replaceFirst("https://" + site, ""))//
-                .filter(href -> !href.equals("/"))//
-                .forEach(href -> requestUris.add(href));
-        Document parsed = Jsoup.parse(body);
-        ssv.accept(parsed.select("link").stream().map(el -> el.attr("href")));
-        ssv.accept(parsed.select("img").stream().map(el -> el.attr("src")));
-        requestUris.forEach(System.out::println);
-
-        {
-            CountDownLatch latch = new CountDownLatch(requestUris.size());
-            for (String requestUri : requestUris) {
-                Future<AsyncClientEndpoint> future = requester.connect(target, Timeout.ofSeconds(10));
-                AsyncClientEndpoint clientEndpoint = future.get();
-                HttpResponseCallback<byte[]> callback = new HttpResponseCallback<>(latch, clientEndpoint, requestUri);
-                clientEndpoint.execute(//
-                        new BasicRequestProducer(Methods.GET, target, requestUri)//
-                        , new BasicResponseConsumer<>(new BasicAsyncEntityConsumer())//
-                        , callback//
-                );
-            }
-            latch.await();
-        }
-        System.out.println("Shutting down I/O reactor");
-        requester.initiateShutdown();
-        try {
-            System.out.println(new NumberFormatter().valueToString(total) + " in " + (System.currentTimeMillis() - start) + "ms");
-        } catch (ParseException ex) {
-            //
-        }
-    }
+		{
+			CountDownLatch latch = new CountDownLatch(requestUris.size());
+			for (String requestUri : requestUris) {
+				Future<AsyncClientEndpoint> future = requester.connect(target, Timeout.ofSeconds(10));
+				AsyncClientEndpoint clientEndpoint = future.get();
+				HttpResponseCallback<byte[]> callback = new HttpResponseCallback<>(latch, clientEndpoint, requestUri);
+				clientEndpoint.execute(//
+						new BasicRequestProducer(Methods.GET, target, requestUri)//
+						, new BasicResponseConsumer<>(new BasicAsyncEntityConsumer())//
+						, callback//
+				);
+			}
+			latch.await();
+		}
+		System.out.println("Shutting down I/O reactor");
+		requester.initiateShutdown();
+	}
 }
