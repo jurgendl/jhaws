8113cbce656ced6ba3a189c26b6031deaef120c2
jurgendl <jurgen.de.landsheer@gmail.com>
2019-11-13 17:30:13 +0100

    waypoints

--- jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/waypoints/Waypoints.java
+++ jhaws/wicket/src/main/java/org/tools/hqlbuilder/webservice/jquery/ui/waypoints/Waypoints.java
@@ -3,6 +3,12 @@ package org.tools.hqlbuilder.webservice.jquery.ui.waypoints;
 import org.tools.hqlbuilder.webservice.jquery.ui.jquery.JQuery;
 import org.tools.hqlbuilder.webservice.wicket.JavaScriptResourceReference;
 
+// var waypoint = new Waypoint({
+// element: document.getElementById('basic-waypoint'),
+// handler: function() {
+// notify('Basic waypoint triggered')
+// }
+// })
 public class Waypoints {
     public static JavaScriptResourceReference JS = new JavaScriptResourceReference(Waypoints.class, "jquery.waypoints.js");
 
