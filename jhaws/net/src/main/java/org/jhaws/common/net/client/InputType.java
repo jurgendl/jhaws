b74cfc982489a3bc4dde7ae6dfc839fecde115c3
jurgendl <jurgen.de.landsheer@gmail.com>
2020-05-12 17:09:40 +0200

    *

--- jhaws/net/src/main/java/org/jhaws/common/net/client/InputType.java
+++ jhaws/net/src/main/java/org/jhaws/common/net/client/InputType.java
@@ -1,5 +1,5 @@
 package org.jhaws.common.net.client;
 
 public enum InputType {
-    select, textarea, button, checkbox, file, hidden, image, password, radio, reset, submit, text;
+	select, textarea, button, checkbox, file, hidden, image, password, radio, reset, submit, text, email;
 }
