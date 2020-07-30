2f82d04c5e8df2fa1d29916cd4ef3f3b7a8dfcc8
jurgendl <jurgen.de.landsheer@gmail.com>
2020-07-30 18:38:41 +0200

    *

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
@@ -1,11 +1,15 @@
 package org.jhaws.common.web.wicket.jquery;
 
+import org.jhaws.common.web.wicket.JavaScriptResourceReference;
 import org.jhaws.common.web.wicket.WicketApplication;
 
 /**
  * version 1.12.4
  */
 public class JQuery {
+	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class,
+			"jquery-2.2.4.js");
+
 	public static org.apache.wicket.request.resource.JavaScriptResourceReference getJQueryReference() {
 		return (org.apache.wicket.request.resource.JavaScriptResourceReference) WicketApplication.get()
 				.getJavaScriptLibrarySettings().getJQueryReference();
