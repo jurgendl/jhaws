dcecc5cd48b19ca7db703a59e3e7ae9eccf1db67
jdlandsh <jurgen.delandsheer@ugent.be>
2020-08-18 17:27:58 +0200

    *

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
@@ -4,11 +4,13 @@ import org.jhaws.common.web.wicket.JavaScriptResourceReference;
 import org.jhaws.common.web.wicket.WicketApplication;
 
 /**
- * version 1.12.4
+ * version 3.5.1 (before: 2.2.4)
  */
 public class JQuery {
-	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class,
-			"jquery-2.2.4.js");
+//	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class,
+//			"jquery-2.2.4.js");
+
+	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class, "jquery.js");
 
 	public static org.apache.wicket.request.resource.JavaScriptResourceReference getJQueryReference() {
 		return (org.apache.wicket.request.resource.JavaScriptResourceReference) WicketApplication.get()
