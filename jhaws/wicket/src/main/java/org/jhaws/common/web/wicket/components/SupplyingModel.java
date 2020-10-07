92980e96ff5a877c74f40e66f15f9d826e6305a9
jdlandsh <jurgen.delandsheer@ugent.be>
2020-10-07 16:04:35 +0200

    *

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/components/SupplyingModel.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/components/SupplyingModel.java
@@ -1,26 +1,30 @@
 package org.jhaws.common.web.wicket.components;
 
 import java.io.Serializable;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 
 @SuppressWarnings("serial")
 public class SupplyingModel<T> extends LoadableDetachableModel<T> {
-	private Supplier<T> supplier;
+    private Supplier<T> supplier;
 
-	public <S extends Supplier<T> & Serializable> SupplyingModel(S supplier) {
-		this.supplier = supplier;
-	}
+    public <S extends Supplier<T> & Serializable> SupplyingModel(S supplier) {
+        setSupplier(supplier);
+    }
 
-	@Override
-	protected T load() {
-		return supplier.get();
-	}
+    public <M, S extends Function<M, T> & Serializable> SupplyingModel(IModel<? extends M> parentModel, S supplier) {
+        setSupplier(() -> supplier.apply(parentModel.getObject()));
+    }
 
-	@Override
-	public void detach() {
-		super.detach();
-		setObject(null);
-	}
+    protected <S extends Supplier<T> & Serializable> void setSupplier(S supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    protected T load() {
+        return supplier.get();
+    }
 }
