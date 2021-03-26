0c2189d6436eda83302032ec04f2cb77cb9b6b02
jurgendl <jurgen.de.landsheer@gmail.com>
2021-03-26 01:29:07 +0100

    *

--- jhaws/wicket/src/main/resources/org/jhaws/common/web/wicket/videojs/videojs-prevent-multiple.js
+++ jhaws/wicket/src/main/resources/org/jhaws/common/web/wicket/videojs/videojs-prevent-multiple.js
@@ -1,11 +1,13 @@
 // https://coderwall.com/p/9loihg/video-js-disable-simultaneous-video-playback-html5-amp-flash
 //html5 - prevent simultaneous video playback - pauses other playing videos upon play
-$('audio,video').bind('play', function() {
-        activated = this;
-        $('audio,video').each(function() {
-            if(this != activated) this.pause();
-        });
-});
+document.addEventListener('DOMContentLoaded', function(event) {
+	$('audio,video').bind('play', function() {
+	        activated = this;
+	        $('audio,video').each(function() {
+	            if(this != activated) this.pause();
+	        });
+	});
+})
 
 //https://stackoverflow.com/questions/14256894/video-js-v-is-not-defined
 //flash - prevent simultaneous video playback - pauses other playing videos upon play
