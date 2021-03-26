82d269ce9e7a3ee5c357c63e4adddd59c2f3073c
jurgendl <jurgen.de.landsheer@gmail.com>
2021-03-26 02:03:19 +0100

    *

--- jhaws/wicket/src/main/resources/org/jhaws/common/web/wicket/videojs/videojs-prevent-multiple.js
+++ jhaws/wicket/src/main/resources/org/jhaws/common/web/wicket/videojs/videojs-prevent-multiple.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	            if(this != activated) this.pause();
 	        });
 	});
-})
+});
 
 //https://stackoverflow.com/questions/14256894/video-js-v-is-not-defined
 //flash - prevent simultaneous video playback - pauses other playing videos upon play
