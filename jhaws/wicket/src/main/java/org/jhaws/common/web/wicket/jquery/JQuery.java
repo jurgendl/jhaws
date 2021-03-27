9d33db28e7a5b84a73640a31f317caf0e163e5fc
jurgendl <jurgen.de.landsheer@gmail.com>
2021-03-27 21:35:06 +0100

    wicket upgrade

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/jquery/JQuery.java
@@ -1,16 +1,16 @@
 package org.jhaws.common.web.wicket.jquery;
 
-import org.jhaws.common.web.wicket.JavaScriptResourceReference;
+import org.jhaws.common.web.wicket.WicketApplication;
 
 public class JQuery {
-	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class, "jquery.js");
+//	public static final JavaScriptResourceReference JS = new JavaScriptResourceReference(JQuery.class, "jquery.js");
+//
+//	public static org.apache.wicket.request.resource.JavaScriptResourceReference getJQueryReference() {
+//		return JS;
+//	}
 
 	public static org.apache.wicket.request.resource.JavaScriptResourceReference getJQueryReference() {
-		return JS;
+		return (org.apache.wicket.request.resource.JavaScriptResourceReference) WicketApplication.get()
+				.getJavaScriptLibrarySettings().getJQueryReference();
 	}
-
-//	public static org.apache.wicket.request.resource.JavaScriptResourceReference getJQueryReference() {
-//		return (org.apache.wicket.request.resource.JavaScriptResourceReference) WicketApplication.get()
-//				.getJavaScriptLibrarySettings().getJQueryReference();
-//	}
 }
