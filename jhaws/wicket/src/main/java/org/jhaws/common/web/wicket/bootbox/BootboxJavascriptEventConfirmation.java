a4920c66e632f7b1c3a54a2c639acc53f1e390df
jdlandsh <jurgen.delandsheer@ugent.be>
2021-04-12 15:26:33 +0200

    wicket 9

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/bootbox/BootboxJavascriptEventConfirmation.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/bootbox/BootboxJavascriptEventConfirmation.java
@@ -4,6 +4,9 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
+// WICKET 6/7
+//
+//
 // http://bootboxjs.com/
 // public void renderHead(IHeaderResponse response) {
 // response.render(JavaScriptHeaderItem.forReference(BootBox.JS_LOCALE));
@@ -15,79 +18,83 @@ import org.apache.wicket.model.Model;
 // f=document.getElementById('form5');var ff=document.getElementById('form7');if (typeof ff.onsubmit === 'function') { if (ff.onsubmit()==false)
 // return false; }f.submit();e.value='';e.name='';return false;}}});
 @SuppressWarnings("serial")
+@Deprecated
 public class BootboxJavascriptEventConfirmation extends AttributeModifier {
-    private BootBoxHelper model;
+	private BootBoxHelper helper;
 
-    public BootboxJavascriptEventConfirmation() {
-        super("onclick", Model.of(""));
-        model = new BootBoxHelper();
-    }
+	public BootboxJavascriptEventConfirmation() {
+		super("onclick", Model.of("confirmation.box.cancel.knop.label"));
+		helper = new BootBoxHelper();
+	}
 
-    public BootboxJavascriptEventConfirmation setOkButton(IModel<String> okButton) {
-        this.model.setOkButton(okButton);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setOkButton(IModel<String> okButton) {
+		this.helper.setOkButton(okButton);
+		return this;
+	}
 
-    public BootboxJavascriptEventConfirmation setCancelButton(IModel<String> cancelButton) {
-        this.model.setCancelButton(cancelButton);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setCancelButton(IModel<String> cancelButton) {
+		this.helper.setCancelButton(cancelButton);
+		return this;
+	}
 
-    public BootboxJavascriptEventConfirmation setOkButtonIcon(IModel<String> okButtonIcon) {
-        this.model.setOkButtonIcon(okButtonIcon);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setOkButtonIcon(IModel<String> okButtonIcon) {
+		this.helper.setOkButtonIcon(okButtonIcon);
+		return this;
+	}
 
-    public BootboxJavascriptEventConfirmation setCancelButtonIcon(IModel<String> cancelButtonIcon) {
-        this.model.setCancelButtonIcon(cancelButtonIcon);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setCancelButtonIcon(IModel<String> cancelButtonIcon) {
+		this.helper.setCancelButtonIcon(cancelButtonIcon);
+		return this;
+	}
 
-    public BootboxJavascriptEventConfirmation setTitle(IModel<String> title) {
-        this.model.setTitle(title);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setTitle(IModel<String> title) {
+		this.helper.setTitle(title);
+		return this;
+	}
 
-    public BootboxJavascriptEventConfirmation setMessage(IModel<String> message) {
-        this.model.setMessage(message);
-        return this;
-    }
+	public BootboxJavascriptEventConfirmation setMessage(IModel<String> message) {
+		this.helper.setMessage(message);
+		return this;
+	}
 
-    public IModel<String> getOkButton() {
-        return this.model.getOkButton();
-    }
+	public IModel<String> getOkButton() {
+		return this.helper.getOkButton();
+	}
 
-    public IModel<String> getCancelButton() {
-        return this.model.getCancelButton();
-    }
+	public IModel<String> getCancelButton() {
+		return this.helper.getCancelButton();
+	}
 
-    public IModel<String> getOkButtonIcon() {
-        return this.model.getOkButtonIcon();
-    }
+	public IModel<String> getOkButtonIcon() {
+		return this.helper.getOkButtonIcon();
+	}
 
-    public IModel<String> getCancelButtonIcon() {
-        return this.model.getCancelButtonIcon();
-    }
+	public IModel<String> getCancelButtonIcon() {
+		return this.helper.getCancelButtonIcon();
+	}
 
-    public IModel<String> getTitle() {
-        return this.model.getTitle();
-    }
+	public IModel<String> getTitle() {
+		return this.helper.getTitle();
+	}
 
-    public IModel<String> getMessage() {
-        return this.model.getMessage();
-    }
+	public IModel<String> getMessage() {
+		return this.helper.getMessage();
+	}
 
-    @Override
-    protected String newValue(final String currentValue, final String replacementValue) {
-        String jsonResp = model.getScript();
-        // TODO is dit ook nodig? e.stopImmediatePropagation(); // stopPropagation
-        // https://medium.com/@jacobwarduk/how-to-correctly-use-preventdefault-stoppropagation-or-return-false-on-events-6c4e3f31aedb
-        //
-        // ||FUNC|| anders is function niet callable door double quote aan het begin en einde
-        // event.preventDefault() is belangrijk voor button en input[type=submit] want anders submit deze toch zonder confirmation
-        jsonResp = "event.preventDefault();bootbox.confirm("
-                + jsonResp.replace(BootBoxHelper.QFUNC, "function(result){console.log(result);if(result){" + currentValue + "}}") + ");";
-        // System.out.println(jsonResp);
-        return jsonResp;
-    }
+	@Override
+	protected String newValue(final String currentValue, final String replacementValue) {
+		String jsonResp = helper.getScript();
+		// TODO is dit ook nodig? e.stopImmediatePropagation(); //
+		// stopPropagation
+		// https://medium.com/@jacobwarduk/how-to-correctly-use-preventdefault-stoppropagation-or-return-false-on-events-6c4e3f31aedb
+		//
+		// ||FUNC|| anders is function niet callable door double quote aan het
+		// begin en einde
+		// event.preventDefault() is belangrijk voor button en
+		// input[type=submit] want anders submit deze toch zonder confirmation
+		jsonResp = "event.preventDefault();bootbox.confirm(" + jsonResp.replace(BootBoxHelper.QFUNC,
+				"function(result){console.log(result);if(result){" + currentValue.toString() + "}}") + ");";
+		// System.out.println(jsonResp);
+		return jsonResp;
+	}
 }
--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/bootbox/ConfirmationSubmitLink.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/bootbox/ConfirmationSubmitLink.java
@@ -1,102 +1,127 @@
 package org.jhaws.common.web.wicket.bootbox;
 
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
 
+// <a class="btn btn-primary" wicket:id="submit">submit</a>
+// form.add(new ConfirmationSubmitLink("submit", Model.of("confirmationmessage")).addConfirmation());
+// werkt in forms en geneste forms
+// werkt op "a href", "input type=submit", "button"
 @SuppressWarnings("serial")
 public class ConfirmationSubmitLink extends SubmitLink {
-    private BootBoxHelper builder = new BootBoxHelper();
+	private BootBoxHelper builder = new BootBoxHelper();
 
-    public ConfirmationSubmitLink(String id, Form<?> form) {
-        super(id, form);
-    }
+	public ConfirmationSubmitLink(String id, Form<?> form) {
+		super(id, form);
+		builder.setCancelButton(new ResourceModel("confirmation.box.cancel.knop.label", (IModel<String>) null));
+	}
 
-    public ConfirmationSubmitLink(String id, IModel<?> model, Form<?> form) {
-        super(id, model, form);
-    }
+	public ConfirmationSubmitLink(String id, IModel<?> model, Form<?> form) {
+		super(id, model, form);
+		builder.setCancelButton(new ResourceModel("confirmation.box.cancel.knop.label", (IModel<String>) null));
+	}
 
-    public ConfirmationSubmitLink(String id, IModel<?> model) {
-        super(id, model);
-    }
+	public ConfirmationSubmitLink(String id, IModel<?> model) {
+		super(id, model);
+		builder.setCancelButton(new ResourceModel("confirmation.box.cancel.knop.label", (IModel<String>) null));
+	}
 
-    public ConfirmationSubmitLink(String id) {
-        super(id);
-    }
+	public ConfirmationSubmitLink(String id) {
+		super(id);
+		builder.setCancelButton(new ResourceModel("confirmation.box.cancel.knop.label", (IModel<String>) null));
+	}
 
-    @Override
-    protected void onInitialize() {
-        super.onInitialize();
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
 
-        BootboxJavascriptEventConfirmation handler = new BootboxJavascriptEventConfirmation();
-        handler.setMessage(builder.getMessage());
-        handler.setTitle(builder.getTitle());
-        handler.setOkButton(builder.getOkButton());
-        handler.setOkButtonIcon(builder.getOkButtonIcon());
-        handler.setCancelButton(builder.getCancelButton());
-        handler.setCancelButtonIcon(builder.getCancelButtonIcon());
+		// WICKET 6/7
+		// BootboxJavascriptEventConfirmation handler = new
+		// BootboxJavascriptEventConfirmation();
+		// handler.setMessage(builder.getMessage());
+		// handler.setTitle(builder.getTitle());
+		// handler.setOkButton(builder.getOkButton());
+		// handler.setOkButtonIcon(builder.getOkButtonIcon());
+		// handler.setCancelButton(builder.getCancelButton());
+		// handler.setCancelButtonIcon(builder.getCancelButtonIcon());
+		// add(handler);
+	}
 
-        add(handler);
-    }
+	@Override
+	protected CharSequence getTriggerJavaScript() {
+		CharSequence triggerJavaScript = super.getTriggerJavaScript();
+		// WICKET 9
+		if (triggerJavaScript != null) {
+			triggerJavaScript = "event.preventDefault();bootbox.confirm("
+					+ builder.getScript().replace(BootBoxHelper.QFUNC,
+							"function(result){console.log(result);if(result){" + triggerJavaScript + "}}")
+					+ ");";
+		}
+		return triggerJavaScript;
+	}
 
-    // @Override
-    // public void renderHead(IHeaderResponse response) {
-    // super.renderHead(response);
-    // response.render(JavaScriptHeaderItem.forReference(BootBox.JS));
-    // response.render(JavaScriptHeaderItem.forReference(BootBox.JS_LOCALE));
-    // }
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(JavaScriptHeaderItem.forReference(BootBox.JS));
+		response.render(JavaScriptHeaderItem.forReference(BootBox.JS_LOCALE));
+	}
 
-    public IModel<String> getOkButton() {
-        return this.builder.getOkButton();
-    }
+	public IModel<String> getOkButton() {
+		return this.builder.getOkButton();
+	}
 
-    public ConfirmationSubmitLink setOkButton(IModel<String> okButton) {
-        this.builder.setOkButton(okButton);
-        return this;
-    }
+	public ConfirmationSubmitLink setOkButton(IModel<String> okButton) {
+		this.builder.setOkButton(okButton);
+		return this;
+	}
 
-    public IModel<String> getCancelButton() {
-        return this.builder.getCancelButton();
-    }
+	public IModel<String> getCancelButton() {
+		return this.builder.getCancelButton();
+	}
 
-    public ConfirmationSubmitLink setCancelButton(IModel<String> cancelButton) {
-        this.builder.setCancelButton(cancelButton);
-        return this;
-    }
+	public ConfirmationSubmitLink setCancelButton(IModel<String> cancelButton) {
+		this.builder.setCancelButton(cancelButton);
+		return this;
+	}
 
-    public IModel<String> getOkButtonIcon() {
-        return this.builder.getOkButtonIcon();
-    }
+	public IModel<String> getOkButtonIcon() {
+		return this.builder.getOkButtonIcon();
+	}
 
-    public ConfirmationSubmitLink setOkButtonIcon(IModel<String> okButtonIcon) {
-        this.builder.setOkButtonIcon(okButtonIcon);
-        return this;
-    }
+	public ConfirmationSubmitLink setOkButtonIcon(IModel<String> okButtonIcon) {
+		this.builder.setOkButtonIcon(okButtonIcon);
+		return this;
+	}
 
-    public IModel<String> getCancelButtonIcon() {
-        return this.builder.getCancelButtonIcon();
-    }
+	public IModel<String> getCancelButtonIcon() {
+		return this.builder.getCancelButtonIcon();
+	}
 
-    public ConfirmationSubmitLink setCancelButtonIcon(IModel<String> cancelButtonIcon) {
-        this.builder.setCancelButtonIcon(cancelButtonIcon);
-        return this;
-    }
+	public ConfirmationSubmitLink setCancelButtonIcon(IModel<String> cancelButtonIcon) {
+		this.builder.setCancelButtonIcon(cancelButtonIcon);
+		return this;
+	}
 
-    public IModel<String> getTitle() {
-        return this.builder.getTitle();
-    }
+	public IModel<String> getTitle() {
+		return this.builder.getTitle();
+	}
 
-    public ConfirmationSubmitLink setTitle(IModel<String> title) {
-        this.builder.setTitle(title);
-        return this;
-    }
+	public ConfirmationSubmitLink setTitle(IModel<String> title) {
+		this.builder.setTitle(title);
+		return this;
+	}
 
-    public IModel<String> getMessage() {
-        return this.builder.getMessage();
-    }
+	public IModel<String> getMessage() {
+		return this.builder.getMessage();
+	}
 
-    public ConfirmationSubmitLink setMessage(IModel<String> message) {
-        this.builder.setMessage(message);
-        return this;
-    }
+	public ConfirmationSubmitLink setMessage(IModel<String> message) {
+		this.builder.setMessage(message);
+		return this;
+	}
 }
