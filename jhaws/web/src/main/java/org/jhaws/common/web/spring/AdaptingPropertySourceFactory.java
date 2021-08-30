80d958eff6d5a6d3bf6e4712344413c4d99c7679
jdlandsh <jurgen.delandsheer@ugent.be>
2021-08-30 15:59:41 +0200

    org.springframework.context.annotation.PropertySource.ignoreResourceNotFound

--- jhaws/web/src/main/java/org/jhaws/common/web/spring/AdaptingPropertySourceFactory.java
+++ jhaws/web/src/main/java/org/jhaws/common/web/spring/AdaptingPropertySourceFactory.java
@@ -18,6 +18,7 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
 import org.springframework.core.io.support.ResourcePropertySource;
+import org.springframework.lang.Nullable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -35,60 +36,65 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 // https://www.baeldung.com/spring-yaml-propertysource
 // https://www.baeldung.com/spring-boot-json-properties
 public class AdaptingPropertySourceFactory implements PropertySourceFactory {
-	protected ObjectMapper objectMapper = new ObjectMapper();
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
-	@Override
-	public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) throws IOException {
-		String filename = encodedResource.getResource().getFilename();
-		if (filename.toLowerCase().endsWith(".properties")) {
-			return (name != null ? new ResourcePropertySource(name, encodedResource)
-					: new ResourcePropertySource(encodedResource));
-		}
-		if (filename.toLowerCase().endsWith(".xml")) {
-			return (name != null ? new ResourcePropertySource(name, encodedResource)
-					: new ResourcePropertySource(encodedResource));
-		}
-		if (filename.toLowerCase().endsWith(".yml")) {
-			YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-			factory.setResources(encodedResource.getResource());
-			Properties properties = factory.getObject();
-			return new PropertiesPropertySource(filename, properties);
-		}
-		if (filename.toLowerCase().endsWith(".json")) {
-			@SuppressWarnings("unchecked")
-			Map<String, Object> readValue = objectMapper.readValue(encodedResource.getInputStream(), Map.class);
-			Set<Map.Entry<String, Object>> set = readValue.entrySet();
-			List<MapPropertySource> propertySources = convertEntrySet(set, Optional.empty());
-			Map<String, Object> merged = new LinkedHashMap<>();
-			propertySources.forEach(t -> {
-				merged.putAll(t.getSource());
-			});
-			return new MapPropertySource(filename, merged);
-		}
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource encodedResource) throws IOException {
+        String filename = encodedResource.getResource().getFilename();
+        if (filename.toLowerCase().endsWith(".properties")) {
+            return (name != null ? new ResourcePropertySource(name, encodedResource) : new ResourcePropertySource(encodedResource));
+        }
+        if (filename.toLowerCase().endsWith(".xml")) {
+            return (name != null ? new ResourcePropertySource(name, encodedResource) : new ResourcePropertySource(encodedResource));
+        }
+        if (filename.toLowerCase().endsWith(".yml")) {
+            try {
+                YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+                factory.setResources(encodedResource.getResource());
+                Properties properties = factory.getObject();
+                return new PropertiesPropertySource(filename, properties);
+            } catch (java.lang.IllegalStateException ex) {
+                if (ex.getCause() instanceof IOException) {
+                    throw (IOException) ex.getCause();
+                }
+                throw ex;
+            }
+        }
+        if (filename.toLowerCase().endsWith(".json")) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> readValue = objectMapper.readValue(encodedResource.getInputStream(), Map.class);
+            Set<Map.Entry<String, Object>> set = readValue.entrySet();
+            List<MapPropertySource> propertySources = convertEntrySet(set, Optional.empty());
+            Map<String, Object> merged = new LinkedHashMap<>();
+            propertySources.forEach(t -> {
+                merged.putAll(t.getSource());
+            });
+            return new MapPropertySource(filename, merged);
+        }
+        throw new UnsupportedOperationException();
+    }
 
-	protected List<MapPropertySource> convertEntrySet(Set<Map.Entry<String, Object>> entrySet,
-			Optional<String> parentKey) {
-		return entrySet.stream().map((Map.Entry<String, Object> e) -> convertToPropertySourceList(e, parentKey))
-				.flatMap(Collection::stream).collect(Collectors.toList());
-	}
+    protected List<MapPropertySource> convertEntrySet(Set<Map.Entry<String, Object>> entrySet, Optional<String> parentKey) {
+        return entrySet.stream()
+                .map((Map.Entry<String, Object> e) -> convertToPropertySourceList(e, parentKey))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
 
-	protected List<MapPropertySource> convertToPropertySourceList(Map.Entry<String, Object> e,
-			Optional<String> parentKey) {
-		String key = parentKey.map(s -> s + ".").orElse("") + e.getKey();
-		Object value = e.getValue();
-		return covertToPropertySourceList(key, value);
-	}
+    protected List<MapPropertySource> convertToPropertySourceList(Map.Entry<String, Object> e, Optional<String> parentKey) {
+        String key = parentKey.map(s -> s + ".").orElse("") + e.getKey();
+        Object value = e.getValue();
+        return covertToPropertySourceList(key, value);
+    }
 
-	protected List<MapPropertySource> covertToPropertySourceList(String key, Object value) {
-		if (value instanceof Map) {
-			@SuppressWarnings("unchecked")
-			Map<String, Object> map = (Map<String, Object>) value;
-			Set<Map.Entry<String, Object>> entrySet = map.entrySet();
-			return convertEntrySet(entrySet, Optional.ofNullable(key));
-		}
-		String finalKey = key;
-		return Collections.singletonList(new MapPropertySource(finalKey, Collections.singletonMap(finalKey, value)));
-	}
+    protected List<MapPropertySource> covertToPropertySourceList(String key, Object value) {
+        if (value instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> map = (Map<String, Object>) value;
+            Set<Map.Entry<String, Object>> entrySet = map.entrySet();
+            return convertEntrySet(entrySet, Optional.ofNullable(key));
+        }
+        String finalKey = key;
+        return Collections.singletonList(new MapPropertySource(finalKey, Collections.singletonMap(finalKey, value)));
+    }
 }
