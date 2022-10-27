63a7c18c7381685f68254b435588cc39dd3cf05a
jurgendl <jurgen.de.landsheer@gmail.com>
2022-10-27 20:55:40 +0200

    wicket supplying model

--- jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/components/SupplyingListModel.java
+++ jhaws/wicket/src/main/java/org/jhaws/common/web/wicket/components/SupplyingListModel.java
@@ -8,29 +8,41 @@ import java.util.function.Supplier;
 import org.apache.wicket.model.IModel;
 
 /**
- * {@link IModel} van {@link List}, opgevuld met een {@link Serializable} {@link Supplier} (ps {@link Supplier} is niet {@link Serializable} by default en geeft normaal problemen
- * in Wicket maar deze class dus niet), je hoeft dus ook niet telkens een subclass te maken<br>
+ * {@link IModel} van {@link List}, opgevuld met een {@link Serializable}
+ * {@link Supplier} (ps {@link Supplier} is niet {@link Serializable} by default
+ * en geeft normaal problemen in Wicket maar deze class dus niet), je hoeft dus
+ * ook niet telkens een subclass te maken<br>
  * <br>
- * bv. IModel<List<String>> listModel = new ListModel<>(() -> new ArrayList<>());
+ * bv. IModel<List<String>> listModel = new ListModel<>(() -> new
+ * ArrayList<>());
  */
 @SuppressWarnings("serial")
 public class SupplyingListModel<T> extends org.apache.wicket.model.util.ListModel<T> {
-    private Supplier<List<T>> supplier;
+	private Supplier<List<T>> supplier;
 
-    public <S extends Supplier<List<T>> & Serializable> SupplyingListModel(S supplier) {
-        setSupplier(supplier);
-    }
+	public SupplyingListModel(List<T> list) {
+		setSupplier(() -> list);
+	}
 
-    public <M, S extends Function<M, List<T>> & Serializable> SupplyingListModel(IModel<? extends M> parentModel, S supplier) {
-        setSupplier(() -> supplier.apply(parentModel.getObject()));
-    }
+	public SupplyingListModel(IModel<List<T>> listModel) {
+		setSupplier(() -> listModel.getObject());
+	}
 
-    protected <S extends Supplier<List<T>> & Serializable> void setSupplier(S supplier) {
-        this.supplier = supplier;
-    }
+	public <S extends Supplier<List<T>> & Serializable> SupplyingListModel(S supplier) {
+		setSupplier(supplier);
+	}
 
-    @Override
-    public List<T> getObject() {
-        return supplier.get();
-    }
+	public <M, S extends Function<M, List<T>> & Serializable> SupplyingListModel(IModel<? extends M> parentModel,
+			S supplier) {
+		setSupplier(() -> supplier.apply(parentModel.getObject()));
+	}
+
+	protected <S extends Supplier<List<T>> & Serializable> void setSupplier(S supplier) {
+		this.supplier = supplier;
+	}
+
+	@Override
+	public List<T> getObject() {
+		return supplier.get();
+	}
 }
