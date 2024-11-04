df34cac7c46a6737bf8d5d6e7d6d57727a7a5081
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2024-11-04 10:13:54 +0100

    json cookie store

---
+++ jhaws/net/src/main/java/org/jhaws/common/net/client5/JsonCookieStore.java
@@ -0,0 +1,46 @@
+package org.jhaws.common.net.client5;
+
+import org.apache.hc.client5.http.cookie.Cookie;
+import org.apache.hc.client5.http.cookie.CookieStore;
+import org.jhaws.common.io.FilePath;
+import org.jhaws.common.net.client.JsonCookieStoreBase;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JsonCookieStore extends JsonCookieStoreBase implements CookieStore {
+    public JsonCookieStore() {
+        super();
+    }
+
+    public JsonCookieStore(FilePath cookieStore) {
+        super(cookieStore);
+    }
+
+    @Override
+    public void addCookie(Cookie cookie) {
+        if (cookies == null) cookies = new ArrayList<>();
+        cookies.add(cookie);
+    }
+
+    @Override
+    public boolean clearExpired(Date date) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        cookies = null;
+    }
+
+    protected List<Cookie> cookies;
+
+    @Override
+    public List<Cookie> getCookies() {
+        if (cookies == null)
+            cookies = getSerializableCookies().stream().map(SerializableCookie::new).collect(Collectors.toList());
+        return cookies;
+    }
+}
