205be88e42502e226ec4925cfb409666370bdde8
jurgendl <jurgen.de.landsheer@gmail.com>
2025-04-18 15:36:30 +0200

    dependencies

--- jhaws/dependencies.sh
+++ jhaws/dependencies.sh
@@ -19,7 +19,20 @@ xidel -s pom.xml --xquery '
 #props[$1]=$2 stores the version variables.
 #gsub(/\$\{[^}]+\}/, ...) replaces ${...} with the correct version.
 #substr($3, 3, length($3)-3) extracts the version key inside ${...}.
-awk -F: 'NR==FNR { props[$1]=$2; next } { gsub(/\$\{[^}]+\}/, v=props[substr($3, 3, length($3)-3)]); print $1 ":" $2 ":" v }' props.txt FS=':' deps.txt
+awk -F: 'NR==FNR { props[$1]=$2; next } { gsub(/\$\{[^}]+\}/, v=props[substr($3, 3, length($3)-3)]); print $1 ":" $2 ":" v }' props.txt FS=':' deps.txt > dp.txt
 
 rm props.txt
 rm deps.txt
+
+#https://unix.stackexchange.com/questions/86321/how-can-i-display-the-contents-of-a-text-file-on-the-command-line
+#https://www.geeksforgeeks.org/sort-command-linuxunix-examples/
+sort dp.txt > tmp.txt
+cat tmp.txt
+
+rm dp.txt
+
+awk -F: '{printf "<a href=\"https://search.maven.org/search?q=g:%s%%20AND%%20a:%s\">%s:%s:%s</a><br>\n", $1, $2, $1, $2, $3}' tmp.txt > deps.html
+
+rm tmp.txt
+
+read -p "..." x
